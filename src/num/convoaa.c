79ed8c05f148778826e52fead01cbee391fa6333
Martin Uecker <uecker@eecs.berkeley.edu>
2015-07-13 22:26:36 -0700

    add copyright

--- src/num/convoaa.c
+++ src/num/convoaa.c
@@ -1,3 +1,10 @@
+/* Copyright 2015. The Regents of the University of California.
+ * All rights reserved. Use of this source code is governed by
+ * a BSD-style license which can be found in the LICENSE file.
+ *
+ * Authors:
+ * 2012 Martin Uecker uecker@eecs.berkeley.edu
+ */
 
 #include <assert.h>
 
