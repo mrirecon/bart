44887f8705c9dd08ecec5988843f14b8250469c9
Martin Uecker <muecker@gwdg.de>
2015-10-12 15:54:09 +0200

    simplify

--- src/misc/debug.c
+++ src/misc/debug.c
@@ -3,7 +3,7 @@
  * a BSD-style license which can be found in the LICENSE file.
  *
  * Authors:
- * 2013	Martin Uecker <uecker@eecs.berkeley.edu>
+ * 2013,2015	Martin Uecker <uecker@eecs.berkeley.edu>
  * 2013,2015	Jonathan Tamir <jtamir@eecs.berkeley.edu>
  * 2013 Dara Bahri <dbahri123@gmail.com
  */
@@ -26,6 +26,7 @@
 #include "num/multind.h"
 #include "misc/io.h"
 #include "misc/mmio.h"
+#include "misc/cppmap.h"
 
 #include "debug.h"
 
@@ -53,58 +54,25 @@ void dump_cfl(const char* name, int D, const long dimensions[D], const complex f
 int debug_level = -1;
 bool debug_logging = false;
 
-static char* get_level_str(int level, char* level_str)
+static const char* level_strings[] = {
+#define LSTR(x) [DP_ ## x] = # x,
+	MAP(LSTR, ERROR, WARN, INFO, DEBUG1, DEBUG2, DEBUG3, DEBUG4, ())
+#undef  LSTR
+};
+
+static const char* get_level_str(int level)
 {
 	assert(level >= 0);
 
-	switch (level) {
-
-		case DP_ERROR:
-			strcpy(level_str, "ERROR");
-			break;
-
-		case DP_WARN:
-			strcpy(level_str, "WARN");
-			break;
-
-		case DP_INFO:
-			strcpy(level_str, "INFO");
-			break;
-
-		case DP_DEBUG1:
-			strcpy(level_str, "DEBUG1");
-			break;
-
-		case DP_DEBUG2:
-			strcpy(level_str, "DEBUG2");
-			break;
-
-		case DP_DEBUG3:
-			strcpy(level_str, "DEBUG3");
-			break;
-
-		case DP_DEBUG4:
-			strcpy(level_str, "DEBUG4");
-			break;
-
-		default:
-			strcpy(level_str, "ALL");
-			break;
-
-	}
-
-	return level_str;
+	return (level <= DP_DEBUG4) ? level_strings[level] : "ALL";
 }
 
-static char* get_datetime_str(char* datetime_str)
+static void get_datetime_str(int len, char* datetime_str)
 {
-
 	time_t tv = time(NULL);
 	struct tm* dt = gmtime(&tv);
 
-	strftime(datetime_str, STRSIZE, "%F %T", dt);
-
-	return datetime_str;
+	strftime(datetime_str, len, "%F %T", dt);
 }
 
 void debug_vprintf(int level, const char* fmt, va_list ap)
@@ -121,11 +89,14 @@ void debug_vprintf(int level, const char* fmt, va_list ap)
 
 		if (true == debug_logging) {
 
-			char level_str[STRSIZE];
 			char dt_str[STRSIZE];
+			get_datetime_str(STRSIZE, dt_str);
 
-			fprintf(ofp, "[%s] [%s] - ", get_datetime_str(dt_str), get_level_str(level, level_str));
-		}
+			fprintf(ofp, "[%s] [%s] - ", dt_str, get_level_str(level));
+
+		} else
+		if (debug_level < DP_INFO)
+			fprintf(ofp, "%s: ", get_level_str(level));
 
 		vfprintf(ofp, fmt, ap);
 		fflush(ofp);
