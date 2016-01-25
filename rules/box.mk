560cc4c540d92243ad4f353c8f1e6e60ef20245d
Martin Uecker <muecker@gwdg.de>
2016-01-25 19:57:29 +0100

    simplify makefile (remove bbox)

--- rules/box.mk
+++ rules/box.mk
@@ -1,21 +1,12 @@
 
 
 
-boxsrcs := $(BTARGETS:%=src/%.c)
+boxsrcs := $(XTARGETS:%=src/%.c)
 boxobjs := $(boxsrcs:.c=.o)
 
 .INTERMEDIATE: $(boxobjs)
 
 lib/libbox.a: libbox.a($(boxobjs))
-
-
-box2srcs := $(XTARGETS:%=src/%.c)
-box2objs := $(box2srcs:.c=.o)
-
-.INTERMEDIATE: $(box2objs)
-
-lib/libbox2.a: libbox2.a($(box2objs))
-
 
 
 
