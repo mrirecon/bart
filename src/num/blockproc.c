96150f1c1a34f0e4b384210144fad2c43697afb6
Martin Uecker <ma.uecker@gmail.com>
2016-02-09 15:32:31 +0100

    update comments which (incorrectly) refer to former license

--- src/num/blockproc.c
+++ src/num/blockproc.c
@@ -1,7 +1,6 @@
 /* Copyright 2015. The Regents of the University of California.
  * All rights reserved. Use of this source code is governed by
- * a educational/research license which can be found in the
- * LICENSE file.
+ * a BSD-style license which can be found in the LICENSE file.
  *
  * Authors: 
  * 2014 Frank Ong <frankong@berkeley.edu>
