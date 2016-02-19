718deec4503a5569317b5ca159daba5fb29a0db4
Martin Uecker <ma.uecker@gmail.com>
2016-02-19 19:09:01 +0100

    improve fftmod help, add -i option

--- src/fftmod.c
+++ src/fftmod.c
@@ -1,9 +1,10 @@
 /* Copyright 2013. The Regents of the University of California.
- * All rights reserved. Use of this source code is governed by 
+ * Copyright 2016. Martin Uecker.
+ * All rights reserved. Use of this source code is governed by
  * a BSD-style license which can be found in the LICENSE file.
  *
- * Authors: 
- * 2012 Martin Uecker <uecker@eecs.berkeley.edu>
+ * Authors:
+ * 2012, 2016 Martin Uecker <martin.uecker@med.uni-goettingen.de>
  */
 
 #include <stdlib.h>
@@ -17,6 +18,7 @@
 
 #include "misc/mmio.h"
 #include "misc/misc.h"
+#include "misc/opts.h"
 
 #ifndef DIMS
 #define DIMS 16
@@ -30,7 +32,16 @@ static const char help_str[] =	"Apply 1 -1 modulation along dimensions selected 
 
 int main_fftmod(int argc, char* argv[])
 {
-	bool b = mini_cmdline_bool(argc, argv, 'b', 3, usage_str, help_str);
+	bool inv = false;
+
+	const struct opt_s opts[] = {
+
+		OPT_SET('b', &inv, "(deprecated)"),
+		OPT_SET('i', &inv, "\tinverse"),
+	};
+
+	cmdline(&argc, argv, 3, 3, usage_str, help_str, ARRAY_SIZE(opts), opts);
+
 
 	unsigned long flags = labs(atol(argv[1]));
 
@@ -40,7 +51,7 @@ int main_fftmod(int argc, char* argv[])
 	complex float* idata = load_cfl(argv[2], N, dims);
 	complex float* odata = create_cfl(argv[3], N, dims);
 
-	(b ? ifftmod : fftmod)(N, dims, flags, odata, idata);
+	(inv ? ifftmod : fftmod)(N, dims, flags, odata, idata);
 
 	unmap_cfl(N, dims, idata);
 	unmap_cfl(N, dims, odata);
