1ce2eb7095f43a3188346232dcdc7a21b4fd50f7
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2016-03-12 21:40:23 +0100

    minor

--- tests/phantom.mk
+++ tests/phantom.mk
@@ -34,9 +34,8 @@ tests/test-phantom-coil: phantom fmac nrmse $(TESTS_OUT)/shepplogan.ra
 	touch $@
 
 
-tests/test-phantom-ksp-coil: phantom fmac fft nrmse $(TESTS_OUT)/shepplogan_coil.ra $(TESTS_OUT)/shepplogan.ra
-	$(TOOLDIR)/phantom -S8 $(TESTS_TMP)/coil.ra
-	$(TOOLDIR)/fmac $(TESTS_OUT)/shepplogan.ra $(TESTS_TMP)/coil.ra $(TESTS_TMP)/sl_coil2.ra
+tests/test-phantom-ksp-coil: phantom fft nrmse $(TESTS_OUT)/shepplogan_coil.ra $(TESTS_OUT)/shepplogan.ra
+	$(TOOLDIR)/phantom -s8 $(TESTS_TMP)/sl_coil2.ra
 	$(TOOLDIR)/fft -i 7 $(TESTS_OUT)/shepplogan_coil.ra $(TESTS_TMP)/shepplogan_cimg.ra
 	$(TOOLDIR)/nrmse -t 0.22 $(TESTS_TMP)/sl_coil2.ra $(TESTS_TMP)/shepplogan_cimg.ra
 	rm $(TESTS_TMP)/*.ra
