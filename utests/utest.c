2e7698a1a31823b016f4f929edfbf45f3d8ee30b
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2016-05-14 10:47:48 +0200

    utests: be more optimistic

--- utests/utest.c
+++ utests/utest.c
@@ -13,14 +13,14 @@ extern ut_test_f* _utests_end;
 int main()
 {
 	int num_tests_run = 0;
-	int num_tests_failed = 0;
+	int num_tests_pass = 0;
 
 	for (ut_test_f** ptr = &_utests_begin; ptr != &_utests_end; ptr++)
-		UNUSED((num_tests_run++, (**ptr)()) || num_tests_failed++);
+		UNUSED((num_tests_run++, (**ptr)()) && num_tests_pass++);
 
-	debug_printf(DP_INFO, "%d/%d failed.\n", num_tests_failed, num_tests_run);
+	debug_printf(DP_INFO, "%d/%d passed.\n", num_tests_pass, num_tests_run);
 
-	exit((0 == num_tests_failed) ? 0 : 1);
+	exit((num_tests_pass == num_tests_run) ? 0 : 1);
 }
 
 
