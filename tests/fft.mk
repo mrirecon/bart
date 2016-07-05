395cb7ea761da67a72133251c58e251151857c2c
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2016-07-05 22:19:19 +0200

    fix broken test

--- tests/fft.mk
+++ tests/fft.mk
@@ -21,7 +21,7 @@ $(TESTS_OUT)/shepplogan_fftu.ra: fft $(TESTS_OUT)/shepplogan.ra
 	$(TOOLDIR)/fft -u 7 $(TESTS_OUT)/shepplogan.ra $@
 
 tests/test-fft-unitary: fft nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_fftu.ra
-	set e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
 	$(TOOLDIR)/fft -u -i 7 $(TESTS_OUT)/shepplogan_fftu.ra shepplogan2u.ra		;\
 	$(TOOLDIR)/nrmse -t 0.000001 $(TESTS_OUT)/shepplogan.ra shepplogan2u.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
