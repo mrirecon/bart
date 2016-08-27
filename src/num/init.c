6a46b88537e18de08d9e52373bba0835cb2f9557
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2016-08-27 15:12:04 +0200

    mpi: initialization

--- src/num/init.c
+++ src/num/init.c
@@ -1,9 +1,10 @@
 /* Copyright 2013. The Regents of the University of California.
- * All rights reserved. Use of this source code is governed by 
+ * Copyright 2016. Martin Uecker.
+ * All rights reserved. Use of this source code is governed by
  * a BSD-style license which can be found in the LICENSE file.
  *
  * Authors:
- * 2013 Martin Uecker <uecker@eecs.berkeley.edu>
+ * 2013-2016 Martin Uecker <martin.uecker@med.uni-goettingen.de>
  *
  *
  * Initialization routines. 
@@ -20,6 +21,7 @@
 #endif
 
 #include "misc/debug.h"
+#include "misc/misc.h"
 #include "num/fft.h"
 
 #ifdef USE_CUDA
@@ -30,10 +32,25 @@
 #include <cula_lapack_device.h>
 #endif
 
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+
 #include "init.h"
 
 
-void num_init(void)
+#ifdef USE_MPI
+// for use in optimize.c
+#define MPI_MAX_DIMS (sizeof(mpi_flags) * 8)
+unsigned long mpi_flags = 0;
+int mpi_rank = 0;
+int mpi_size = 0;
+long mpi_dims[MPI_MAX_DIMS] = { [0 ... MPI_MAX_DIMS - 1] = 1 };
+long mpi_position[MPI_MAX_DIMS] = { 0 };
+#endif
+
+
+static void num_init2(void)
 {
 #ifdef __linux__
 //	feenableexcept(FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW|FE_UNDERFLOW);
@@ -59,6 +76,52 @@ void num_init(void)
 #endif
 #ifdef FFTWTHREADS
 	fft_set_num_threads(p);
+#endif
+}
+
+static void mpi_exit(void)
+{
+	MPI_Finalize();
+}
+
+bool num_root_node_p(void)
+{
+#ifdef USE_MPI
+	return (0 == mpi_rank);
+#else
+	return true;
+#endif	
+}
+
+void num_init(void)
+{
+	num_init2();
+#ifdef USE_MPI
+	const char* mpi = getenv("BART_MPI_FLAGS");
+
+	if (NULL != mpi) {
+
+		MPI_Init(NULL, NULL);
+		atexit(mpi_exit);
+
+		mpi_flags = atoi(mpi);
+		
+		assert(0u == (mpi_flags & (mpi_flags - 1)));
+
+		MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+		MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+
+		debug_printf(DP_INFO, "MPI Rank: %d/%d\n", mpi_rank, mpi_size);
+
+		for (unsigned int i = 0; i < ARRAY_SIZE(mpi_position); i++) {
+
+			if (mpi_flags & (1 << i)) {
+
+				mpi_position[i] = mpi_rank;
+				mpi_dims[i] = mpi_size;
+			}
+		}
+	}
 #endif
 }
 
--- src/num/init.h
+++ src/num/init.h
@@ -1,10 +1,14 @@
 /* Copyright 2013. The Regents of the University of California.
- * All rights reserved. Use of this source code is governed by 
+ * Copyright 2016. Martin Uecker.
+ * All rights reserved. Use of this source code is governed by
  * a BSD-style license which can be found in the LICENSE file.
  */ 
 
 extern void num_init(void);
 extern void num_init_gpu(void);
+
+extern _Bool num_root_node_p(void);
+
 /**
  * selects the GPU with the maximum available memory
  *   (if there are more than one on the system)
