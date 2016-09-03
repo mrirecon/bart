2b97c62eb87c7e6ffab9944f8532cb0b8713f6b6
Siddharth <sid8795@gmail.com>
2016-09-02 17:05:48 -0700

    Added auto-espirit test in ecalib.mk

--- tests/ecalib.mk
+++ tests/ecalib.mk
@@ -9,6 +9,13 @@ tests/test-ecalib: ecalib pocsense nrmse $(TESTS_OUT)/shepplogan_coil.ra
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+tests/test-ecalib-auto: ecalib pocsense nrmse noise $(TESTS_OUT)/shepplogan_coil.ra
+	set -e ; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP) ;\
+	$(TOOLDIR)/noise -n 100 $(TESTS_OUT)/shepplogan_coil.ra shepplogan_noise.ra ;\
+	$(TOOLDIR)/ecalib -m 1 -a -v 100 shepplogan_noise.ra coils.ra ;\
+	$(TOOLDIR)/pocsense -i 1 shepplogan_noise.ra coils.ra proj.ra ;\
+	$(TOOLDIR)/nrmse -t 0.05 proj.ra $(TESTS_OUT)/shepplogan_coil.ra ;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 
-TESTS += tests/test-ecalib
-
+TESTS += tests/test-ecalib tests/test-ecalib-auto
