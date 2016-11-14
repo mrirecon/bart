27be58addf6d61b524aaaba959187eecf31c98e5
Jon Tamir <jtamir@eecs.berkeley.edu>
2016-11-14 11:05:24 -0800

    add squeeze tool

---
+++ src/squeeze.c
@@ -0,0 +1,55 @@
+/* Copyright 2016. The Regents of the University of California.
+ * All rights reserved. Use of this source code is governed by 
+ * a BSD-style license which can be found in the LICENSE file.
+ * 
+ * Authors:
+ * 2016 Jon Tamir <jtamir@eecs.berkeley.edu>
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <complex.h>
+#include <stdio.h>
+
+#include "num/multind.h"
+#include "num/flpmath.h"
+
+#include "misc/mmio.h"
+#include "misc/misc.h"
+
+#ifndef DIMS
+#define DIMS 16
+#endif
+
+
+static const char usage_str[] = "<input> <output>";
+static const char help_str[] = "Remove singleton dimensions of array.\n";
+
+
+int main_squeeze(int argc, char* argv[])
+{
+	mini_cmdline(argc, argv, 2, usage_str, help_str);
+
+	long idims[DIMS];
+	long odims[DIMS] = MD_INIT_ARRAY(DIMS, 1);
+
+	complex float* idata = load_cfl(argv[1], DIMS, idims);
+		
+	unsigned int j = 0;
+	for (unsigned int i = 0; i < DIMS; i++) {
+
+		if (1 < idims[i])
+			odims[j++] = idims[i];
+	}
+
+	complex float* odata = create_cfl(argv[2], DIMS, odims);
+
+	md_copy(DIMS, idims, odata, idata, CFL_SIZE);
+
+	unmap_cfl(DIMS, idims, idata);
+	unmap_cfl(DIMS, odims, odata);
+
+	exit(0);
+}
+
+
---
+++ tests/squeeze.mk
@@ -0,0 +1,16 @@
+
+tests/test-squeeze: ones squeeze noise nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/ones 6 1 2 1 3 1 4 a0.ra								;\
+	$(TOOLDIR)/ones 3 2 3 4 b0.ra								;\
+	$(TOOLDIR)/noise a0.ra a1.ra							;\
+	$(TOOLDIR)/noise b0.ra b1.ra							;\
+	$(TOOLDIR)/squeeze a1.ra a2.ra							;\
+	$(TOOLDIR)/nrmse -t 0. b1.ra a2.ra							;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+
+TESTS += tests/test-squeeze
+
