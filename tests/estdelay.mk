039fb072a483bad515d85b3fccbc8adc90a784a7
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2017-03-25 11:36:40 +0100

    add test for transverse delays

--- tests/estdelay.mk
+++ tests/estdelay.mk
@@ -8,6 +8,15 @@ tests/test-estdelay: estdelay traj phantom nrmse
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+tests/test-estdelay-transverse: estdelay traj phantom nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/traj -D -O -q1.5:1:-0.5 -r -y8 t.ra						;\
+	$(TOOLDIR)/phantom -k -t t.ra k.ra							;\
+	$(TOOLDIR)/traj -D -O -q`$(TOOLDIR)/estdelay t.ra k.ra` -r -y8 t2.ra			;\
+	$(TOOLDIR)/nrmse -t 0.001 t.ra t2.ra							;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
 tests/test-estdelay-coils: estdelay scale traj phantom nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
 	$(TOOLDIR)/traj -G -q0.3:-0.1:0.2 -y5 t.ra						;\
@@ -44,6 +53,6 @@ tests/test-estdelay-scale: estdelay scale traj phantom nrmse
 	touch $@
 
 
-
-TESTS += tests/test-estdelay tests/test-estdelay-ring tests/test-estdelay-coils tests/test-estdelay-scale
+TESTS += tests/test-estdelay tests/test-estdelay-transverse
+TESTS += tests/test-estdelay-ring tests/test-estdelay-coils tests/test-estdelay-scale
 
