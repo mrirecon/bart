876bcbf185b3851018a7129e20813f4f038be7a1
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2017-05-12 22:41:45 +0200

    use defaults in nlinv

--- src/noir/recon.c
+++ src/noir/recon.c
@@ -111,8 +111,10 @@ void noir_recon(const struct noir_conf_s* conf, const long dims[DIMS], complex f
 	struct noir_data* ndata = noir_init(dims, mask, psf, conf->rvc, conf->usegpu);
 	struct data data = { { &TYPEID(data) }, ndata };
 
-	struct iter3_irgnm_conf irgnm_conf = { .iter = conf->iter, .alpha = conf->alpha, .redu = conf->redu };
-	SET_TYPEID(iter3_irgnm_conf, &irgnm_conf);
+	struct iter3_irgnm_conf irgnm_conf = iter3_irgnm_defaults;
+	irgnm_conf.iter = conf->iter;
+	irgnm_conf.alpha = conf->alpha;
+	irgnm_conf.redu = conf->redu;
 
 	iter3_irgnm(CAST_UP(&irgnm_conf),
 			(struct iter_op_s){ frw, CAST_UP(&data) },
