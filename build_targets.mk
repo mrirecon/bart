702bf8b76656787a7291c7c95d88b40a8ef9166e
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2017-05-18 10:44:06 +0200

    move casorati to base

--- build_targets.mk
+++ build_targets.mk
@@ -1,8 +1,8 @@
 # Main build targets
 #
-TBASE=show slice crop resize join transpose squeeze flatten zeros ones flip circshift extract repmat bitmask reshape version delta copy
+TBASE=show slice crop resize join transpose squeeze flatten zeros ones flip circshift extract repmat bitmask reshape version delta copy casorati
 TFLP=scale invert conj fmac saxpy sdot spow cpyphs creal carg normalize cdf97 pattern nrmse mip avg cabs zexpj
-TNUM=fft fftmod fftshift noise bench threshold conv rss filter mandelbrot casorati
+TNUM=fft fftmod fftshift noise bench threshold conv rss filter mandelbrot
 TRECO=pics pocsense sqpics bpsense itsense nlinv nufft rof sake wave lrmatrix estdims estshift estdelay
 TCALIB=ecalib ecaltwo caldir walsh cc ccapply calmat svd estvar
 TMRI=homodyne poisson twixread fakeksp
