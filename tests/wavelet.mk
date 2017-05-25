af0ef6f225831adc4b507aabf6550b54f5b9f9f1
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2017-05-25 23:51:04 +0200

    improve precision of wavelet filter

--- tests/wavelet.mk
+++ tests/wavelet.mk
@@ -3,7 +3,7 @@ tests/test-wavelet: wavelet nrmse $(TESTS_OUT)/shepplogan.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
 	$(TOOLDIR)/wavelet 3 $(TESTS_OUT)/shepplogan.ra w.ra				;\
 	$(TOOLDIR)/wavelet -a 3 128 128 w.ra a.ra					;\
-	$(TOOLDIR)/nrmse -t 0.000005 $(TESTS_OUT)/shepplogan.ra a.ra			;\
+	$(TOOLDIR)/nrmse -t 0.000001 $(TESTS_OUT)/shepplogan.ra a.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
