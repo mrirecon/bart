8f38ce8e4e87260c46a3db578bb248eb2c852a2d
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2017-05-26 14:03:56 +0200

    tests: remove redundancy

--- tests/ccapply.mk
+++ tests/ccapply.mk
@@ -1,74 +1,68 @@
 
 
 
-tests/test-ccapply-forward: phantom cc ccapply nrmse
+tests/test-ccapply-forward: cc ccapply nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -s 8 -k ksp.ra						;\
-	$(TOOLDIR)/cc -S -p 4 ksp.ra ksp-cc.ra						;\
-	$(TOOLDIR)/cc -M -S ksp.ra sccmat.ra						;\
-	$(TOOLDIR)/ccapply -S -p 4 ksp.ra sccmat.ra ksp-cc-2.ra				;\
+	$(TOOLDIR)/cc -S -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra ksp-cc.ra		;\
+	$(TOOLDIR)/cc -M -S $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra		;\
+	$(TOOLDIR)/ccapply -S -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra ksp-cc-2.ra	;\
 	$(TOOLDIR)/nrmse -t 0.001 ksp-cc.ra ksp-cc-2.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-tests/test-ccapply-backward: phantom cc ccapply extract fmac transpose nrmse
+tests/test-ccapply-backward: cc ccapply extract fmac transpose nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -s 8 -k ksp.ra						;\
-	$(TOOLDIR)/cc -M -S ksp.ra sccmat.ra						;\
-	$(TOOLDIR)/ccapply -S -p 4 ksp.ra sccmat.ra ksp-cc.ra				;\
+	$(TOOLDIR)/cc -M -S $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra		;\
+	$(TOOLDIR)/ccapply -S -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra ksp-cc.ra	;\
 	$(TOOLDIR)/ccapply -S -u ksp-cc.ra sccmat.ra ksp-2.ra				;\
 	$(TOOLDIR)/extract 4 0 3 sccmat.ra sccmat-4.ra					;\
 	$(TOOLDIR)/transpose 3 4 ksp-cc.ra ksp-ccT.ra					;\
 	$(TOOLDIR)/fmac -s 16 ksp-ccT.ra sccmat-4.ra ksp-3.ra				;\
-	$(TOOLDIR)/nrmse -t 0.08 ksp.ra ksp-2.ra					;\
+	$(TOOLDIR)/nrmse -t 0.08 $(TESTS_OUT)/shepplogan_coil_ksp.ra ksp-2.ra		;\
 	$(TOOLDIR)/nrmse -t 0.0001 ksp-2.ra ksp-3.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-tests/test-ccapply-geom-forward: phantom cc ccapply extract nrmse
+tests/test-ccapply-geom-forward: cc ccapply extract nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -s 8 -k ksp.ra						;\
-	$(TOOLDIR)/cc -G -p 4 ksp.ra ksp-cc.ra						;\
-	$(TOOLDIR)/cc -M -p 4 -G ksp.ra sccmat.ra					;\
-	$(TOOLDIR)/ccapply -G -p 4 ksp.ra sccmat.ra ksp-cc-2.ra				;\
+	$(TOOLDIR)/cc -G -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra ksp-cc.ra		;\
+	$(TOOLDIR)/cc -M -p 4 -G $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra		;\
+	$(TOOLDIR)/ccapply -G -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra ksp-cc-2.ra	;\
 	$(TOOLDIR)/extract 4 0 3 sccmat.ra sccmat-4.ra					;\
 	$(TOOLDIR)/nrmse -t 0.001 ksp-cc.ra ksp-cc-2.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-tests/test-ccapply-geom-backward: phantom cc ccapply nrmse
+tests/test-ccapply-geom-backward: cc ccapply nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -s 8 -k ksp.ra						;\
-	$(TOOLDIR)/cc -M -p4 -G ksp.ra sccmat.ra					;\
-	$(TOOLDIR)/ccapply -G -p 4 ksp.ra sccmat.ra ksp-cc.ra				;\
+	$(TOOLDIR)/cc -M -p4 -G $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra		;\
+	$(TOOLDIR)/ccapply -G -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra ksp-cc.ra	;\
 	$(TOOLDIR)/ccapply -G -u ksp-cc.ra sccmat.ra ksp-2.ra				;\
-	$(TOOLDIR)/nrmse -t 0.08 ksp.ra ksp-2.ra					;\
+	$(TOOLDIR)/nrmse -t 0.08 $(TESTS_OUT)/shepplogan_coil_ksp.ra ksp-2.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-tests/test-ccapply-esp-forward: phantom cc ccapply extract nrmse
+tests/test-ccapply-esp-forward: cc ccapply extract nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -s 8 -k ksp.ra						;\
-	$(TOOLDIR)/cc -E -p 4 ksp.ra ksp-cc.ra						;\
-	$(TOOLDIR)/cc -M -p 4 -E ksp.ra sccmat.ra					;\
-	$(TOOLDIR)/ccapply -E -p 4 ksp.ra sccmat.ra ksp-cc-2.ra				;\
+	$(TOOLDIR)/cc -E -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra ksp-cc.ra		;\
+	$(TOOLDIR)/cc -M -p 4 -E $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra		;\
+	$(TOOLDIR)/ccapply -E -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra ksp-cc-2.ra	;\
 	$(TOOLDIR)/extract 4 0 3 sccmat.ra sccmat-4.ra					;\
 	$(TOOLDIR)/nrmse -t 0.001 ksp-cc.ra ksp-cc-2.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-tests/test-ccapply-esp-backward: phantom cc ccapply nrmse
+tests/test-ccapply-esp-backward: cc ccapply nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -s 8 -k ksp.ra						;\
-	$(TOOLDIR)/cc -M -p4 -E ksp.ra sccmat.ra					;\
-	$(TOOLDIR)/ccapply -E -p 4 ksp.ra sccmat.ra ksp-cc.ra				;\
+	$(TOOLDIR)/cc -M -p4 -E $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra		;\
+	$(TOOLDIR)/ccapply -E -p 4 $(TESTS_OUT)/shepplogan_coil_ksp.ra sccmat.ra ksp-cc.ra	;\
 	$(TOOLDIR)/ccapply -E -u ksp-cc.ra sccmat.ra ksp-2.ra				;\
-	$(TOOLDIR)/nrmse -t 0.08 ksp.ra ksp-2.ra					;\
+	$(TOOLDIR)/nrmse -t 0.08 $(TESTS_OUT)/shepplogan_coil_ksp.ra ksp-2.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
--- tests/phantom.mk
+++ tests/phantom.mk
@@ -5,6 +5,9 @@ $(TESTS_OUT)/shepplogan.ra: phantom
 $(TESTS_OUT)/shepplogan_ksp.ra: phantom
 	$(TOOLDIR)/phantom -k $@
 
+$(TESTS_OUT)/shepplogan_coil.ra: phantom
+	$(TOOLDIR)/phantom -s8 $@
+
 $(TESTS_OUT)/shepplogan_coil_ksp.ra: phantom
 	$(TOOLDIR)/phantom -s8 -k $@
 
@@ -30,21 +33,18 @@ tests/test-phantom-noncart: traj phantom reshape nrmse $(TESTS_OUT)/shepplogan_k
 	touch $@
 
 
-tests/test-phantom-coil: phantom fmac nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/coils.ra
+tests/test-phantom-coil: fmac nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/phantom -s8 shepplogan_coil.ra						;\
-	$(TOOLDIR)/phantom -S8 coil.ra                                                          ;\
-	$(TOOLDIR)/fmac $(TESTS_OUT)/shepplogan.ra coil.ra sl_coil2.ra                          ;\
-	$(TOOLDIR)/nrmse -t 0. shepplogan_coil.ra sl_coil2.ra					;\
+	$(TOOLDIR)/fmac $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/coils.ra sl_coil2.ra		;\
+	$(TOOLDIR)/nrmse -t 0. $(TESTS_OUT)/shepplogan_coil.ra sl_coil2.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-tests/test-phantom-ksp-coil: phantom fft nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
+tests/test-phantom-ksp-coil: fft nrmse $(TESTS_OUT)/shepplogan_coil.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/phantom -s8 sl_coil2.ra							;\
 	$(TOOLDIR)/fft -i 7 $(TESTS_OUT)/shepplogan_coil_ksp.ra shepplogan_cimg.ra		;\
-	$(TOOLDIR)/nrmse -t 0.22 sl_coil2.ra shepplogan_cimg.ra					;\
+	$(TOOLDIR)/nrmse -t 0.22 $(TESTS_OUT)/shepplogan_coil.ra shepplogan_cimg.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
