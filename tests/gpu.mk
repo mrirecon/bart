b54620294266fe69394ef08712bc6ff230d84ead
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2017-05-26 14:11:53 +0200

    tests: remove more redundancy

--- tests/gpu.mk
+++ tests/gpu.mk
@@ -2,11 +2,10 @@
 
 # 
 
-tests/test-pics-gpu: phantom pics nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
+tests/test-pics-gpu: pics nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
-	$(TOOLDIR)/pics -g -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco1.ra	;\
-	$(TOOLDIR)/pics    -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco2.ra	;\
+	$(TOOLDIR)/pics -g -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco1.ra	;\
+	$(TOOLDIR)/pics    -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco2.ra	;\
 	$(TOOLDIR)/nrmse -t 0.000001 reco1.ra reco2.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
@@ -26,29 +25,27 @@ tests/test-pics-gpu-noncart: traj scale phantom ones pics nrmse
 
 
 
-tests/test-pics-gpu-weights: phantom pics scale ones nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra
+tests/test-pics-gpu-weights: pics scale ones nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
 	$(TOOLDIR)/ones 2 128 128 weights.ra						;\
 	$(TOOLDIR)/scale 0.1 weights.ra weights2.ra					;\
-	$(TOOLDIR)/pics    -S -r0.001 -p weights2.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco1.ra	;\
-	$(TOOLDIR)/pics -g -S -r0.001 -p weights2.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco2.ra	;\
+	$(TOOLDIR)/pics    -S -r0.001 -p weights2.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco1.ra	;\
+	$(TOOLDIR)/pics -g -S -r0.001 -p weights2.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco2.ra	;\
 	$(TOOLDIR)/nrmse -t 0.000001 reco2.ra reco1.ra				 	;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
 
-tests/test-pics-gpu-noncart-weights: traj scale ones phantom pics nrmse
+tests/test-pics-gpu-noncart-weights: traj scale ones phantom pics nrmse $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
 	$(TOOLDIR)/traj -r -x256 -y32 traj.ra						;\
 	$(TOOLDIR)/scale 0.5 traj.ra traj2.ra						;\
 	$(TOOLDIR)/phantom -s8 -t traj2.ra ksp.ra					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
 	$(TOOLDIR)/ones 4 1 256 32 1 weights.ra						;\
 	$(TOOLDIR)/scale 0.1 weights.ra weights2.ra					;\
-	$(TOOLDIR)/pics    -S -r0.001 -p weights2.ra -t traj2.ra ksp.ra coils.ra reco1.ra ;\
-	$(TOOLDIR)/pics -g -S -r0.001 -p weights2.ra -t traj2.ra ksp.ra coils.ra reco2.ra ;\
+	$(TOOLDIR)/pics    -S -r0.001 -p weights2.ra -t traj2.ra ksp.ra $(TESTS_OUT)/coils.ra reco1.ra ;\
+	$(TOOLDIR)/pics -g -S -r0.001 -p weights2.ra -t traj2.ra ksp.ra $(TESTS_OUT)/coils.ra reco2.ra ;\
 	$(TOOLDIR)/nrmse -t 0.005 reco1.ra reco2.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
--- tests/pics.mk
+++ tests/pics.mk
@@ -1,10 +1,9 @@
 
 
 
-tests/test-pics-pi: phantom pics scale nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra
+tests/test-pics-pi: pics scale nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
-	$(TOOLDIR)/pics -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco.ra	;\
+	$(TOOLDIR)/pics -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco.ra	;\
 	$(TOOLDIR)/scale 128. reco.ra reco2.ra						;\
 	$(TOOLDIR)/nrmse -t 0.23 reco2.ra $(TESTS_OUT)/shepplogan.ra	;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
@@ -40,13 +39,12 @@ tests/test-pics-cs: traj scale phantom ones pics nrmse $(TESTS_OUT)/shepplogan.r
 
 
 
-tests/test-pics-pics: traj scale phantom pics nrmse $(TESTS_OUT)/shepplogan.ra
+tests/test-pics-pics: traj scale phantom pics nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
 	$(TOOLDIR)/traj -r -x256 -y32 traj.ra						;\
 	$(TOOLDIR)/scale 0.5 traj.ra traj2.ra						;\
 	$(TOOLDIR)/phantom -s8 -t traj2.ra ksp.ra					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
-	$(TOOLDIR)/pics -S -RT:7:0:0.001 -u1000000000. -e -t traj2.ra ksp.ra coils.ra reco.ra	;\
+	$(TOOLDIR)/pics -S -RT:7:0:0.001 -u1000000000. -e -t traj2.ra ksp.ra $(TESTS_OUT)/coils.ra reco.ra	;\
 	$(TOOLDIR)/scale 128. reco.ra reco2.ra						;\
 	$(TOOLDIR)/nrmse -t 0.22 reco2.ra $(TESTS_OUT)/shepplogan.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
@@ -55,13 +53,12 @@ tests/test-pics-pics: traj scale phantom pics nrmse $(TESTS_OUT)/shepplogan.ra
 
 
 # test that weights =0.5 have no effect
-tests/test-pics-weights: phantom pics ones nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra
+tests/test-pics-weights: pics ones nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
 	$(TOOLDIR)/ones 2 128 128 weights.ra						;\
 	$(TOOLDIR)/scale 0.5 weights.ra	weights2.ra					;\
-	$(TOOLDIR)/pics -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco1.ra	;\
-	$(TOOLDIR)/pics -S -r0.001 -p weights2.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco2.ra	;\
+	$(TOOLDIR)/pics -S -r0.001 $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco1.ra	;\
+	$(TOOLDIR)/pics -S -r0.001 -p weights2.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco2.ra	;\
 	$(TOOLDIR)/nrmse -t 0.000001 reco2.ra reco1.ra				 	;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
@@ -70,39 +67,36 @@ tests/test-pics-weights: phantom pics ones nrmse $(TESTS_OUT)/shepplogan.ra $(TE
 
 # test that weights =0.5 have no effect
 # FIXME: this was 0.005 before but fails on travis
-tests/test-pics-noncart-weights: traj scale ones phantom pics nrmse
+tests/test-pics-noncart-weights: traj scale ones phantom pics nrmse $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
 	$(TOOLDIR)/traj -r -x256 -y32 traj.ra						;\
 	$(TOOLDIR)/scale 0.5 traj.ra traj2.ra						;\
 	$(TOOLDIR)/phantom -s8 -t traj2.ra ksp.ra					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
 	$(TOOLDIR)/ones 4 1 256 32 1 weights.ra						;\
 	$(TOOLDIR)/scale 0.5 weights.ra	weights2.ra					;\
-	$(TOOLDIR)/pics -S -r0.001 -p weights2.ra -t traj2.ra ksp.ra coils.ra reco1.ra	;\
-	$(TOOLDIR)/pics -S -r0.001                -t traj2.ra ksp.ra coils.ra reco2.ra	;\
+	$(TOOLDIR)/pics -S -r0.001 -p weights2.ra -t traj2.ra ksp.ra $(TESTS_OUT)/coils.ra reco1.ra	;\
+	$(TOOLDIR)/pics -S -r0.001                -t traj2.ra ksp.ra $(TESTS_OUT)/coils.ra reco2.ra	;\
 	$(TOOLDIR)/nrmse -t 0.010 reco1.ra reco2.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
 
-tests/test-pics-warmstart: phantom pics scale nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
+tests/test-pics-warmstart: pics scale nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
 	$(TOOLDIR)/ones 3 128 128 1 o.ra						;\
-	$(TOOLDIR)/pics -i0 -Wo.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra coils.ra reco.ra	;\
+	$(TOOLDIR)/pics -i0 -Wo.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra reco.ra	;\
 	$(TOOLDIR)/nrmse -t 0. o.ra reco.ra 						;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
 
-tests/test-pics-batch: phantom pics repmat nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra
+tests/test-pics-batch: pics repmat nrmse $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/phantom -S8 coils.ra							;\
 	$(TOOLDIR)/repmat 5 32 $(TESTS_OUT)/shepplogan_coil_ksp.ra kspaces.ra		;\
-	$(TOOLDIR)/pics -r0.01 -B32 kspaces.ra coils.ra reco1.ra			;\
-	$(TOOLDIR)/pics -r0.01      kspaces.ra coils.ra reco2.ra			;\
+	$(TOOLDIR)/pics -r0.01 -B32 kspaces.ra $(TESTS_OUT)/coils.ra reco1.ra		;\
+	$(TOOLDIR)/pics -r0.01      kspaces.ra $(TESTS_OUT)/coils.ra reco2.ra		;\
 	$(TOOLDIR)/nrmse -t 0.00001 reco1.ra reco2.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
