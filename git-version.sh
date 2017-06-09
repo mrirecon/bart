2a9dd2f94d657dbda8803da9698d99e93cdfa78e
Nguyen Damien <ngn.damien@gmail.com>
2017-06-09 22:05:39 +0200

    Enable CUDA compilation when using cmake (#83)
    enable cuda compilation when using cmake

--- git-version.sh
+++ git-version.sh
@@ -2,9 +2,14 @@
 
 if test -d ${GIT_DIR:-.git} -o -f .git
 then
-	git describe --match "v*" --dirty
+    git describe --match "v*" --dirty
+    if [[ $? -eq 0 ]]; then
 	git describe --match "v*" | cut -f1 -d'-' > version.txt
+    else
+	var=`cat version.txt`
+	echo ${var}-dirty
+    fi
 else
-	cat version.txt
+    cat version.txt
 fi
 
