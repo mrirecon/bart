ef9f3b8c87bcf02a744fae647e2e4ad62792d088
Siddharth Iyer <ssi@mit.edu>
2017-11-21 16:28:14 -0500

    Added wavepsf
    - Generates point spread function used in wave-caipi based on gradient parameters
      such as max gradient amplitude and gradient slew rate.
    - Added test file `wave.mk` to test both wave and wavepsf. This uses the automatic
      scaling option in nrmse.

---
+++ tests/wave.mk
@@ -0,0 +1,14 @@
+tests/test-wave: wave wavepsf scale nrmse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra $(TESTS_OUT)/coils.ra
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)                          ;\
+	$(TOOLDIR)/wavepsf -x 640 -y 128 -z 1 wave_psf.ra                     ;\
+	$(TOOLDIR)/fft -iu 7 $(TESTS_OUT)/shepplogan_coil_ksp.ra img.ra       ;\
+	$(TOOLDIR)/resize -c 0 640 img.ra wave_zpad.ra                        ;\
+	$(TOOLDIR)/fft -u 1 wave_zpad.ra wave_hyb.ra                          ;\
+	$(TOOLDIR)/fmac wave_hyb.ra wave_psf.ra wave_acq.ra                   ;\
+	$(TOOLDIR)/fft -u 6 wave_acq.ra wave_ksp.ra                           ;\
+	$(TOOLDIR)/wave wave_ksp.ra $(TESTS_OUT)/coils.ra wave_psf.ra reco.ra ;\
+	$(TOOLDIR)/nrmse -t 0.23 -s reco.ra $(TESTS_OUT)/shepplogan.ra        ;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+TESTS += tests/test-wave
