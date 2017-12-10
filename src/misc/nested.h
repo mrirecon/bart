47acbcb124e92aef2a9edaf94ff92222dd13d31b
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2017-12-10 13:31:26 +0100

    compiler check for cuda

--- src/misc/nested.h
+++ src/misc/nested.h
@@ -1,13 +1,13 @@
 
-#ifndef __clang__
+#if defined(__clang__) && !defined(__CUDACC__)
+#define NESTED(RET, NAME, ARGS) \
+	RET (^NAME)ARGS = ^ARGS
+#define CLOSURE_TYPE(x) (^x)
+#else
 #define NESTED(RET, NAME, ARGS) \
 	RET NAME ARGS
 #define CLOSURE_TYPE(x) x
 #define __block
-#else
-#define NESTED(RET, NAME, ARGS) \
-	RET (^NAME)ARGS = ^ARGS
-#define CLOSURE_TYPE(x) (^x)
 #endif
 
 
