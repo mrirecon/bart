a55b95265db53ecbe4a6c0cd656aae2bfb1ff5ee
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2018-06-30 21:50:42 +0200

    move italgo init into its own file

---
+++ src/grecon/italgo.h
@@ -0,0 +1,22 @@
+
+struct admm_conf {
+
+	bool dynamic_rho;
+	bool dynamic_tau;
+	bool relative_norm;
+	float rho;
+	unsigned int maxitercg;
+};
+
+
+struct iter {
+
+	italgo_fun2_t italgo;
+	iter_conf* iconf;
+};
+
+struct reg_s;
+enum algo_t;
+
+extern struct iter configure_italgo(enum algo_t algo, int nr_penalties, const struct reg_s* regs, unsigned int maxiter, float step, bool hogwild, bool fast, const struct admm_conf admm, float scaling, bool warm_start);
+
