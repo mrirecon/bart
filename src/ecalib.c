1cba6ca8a5187138e999377a970f3d640adab6bd
Nguyen Damien <damien.nguyen@unibas.ch>
2018-07-16 13:49:01 +0200

    ecalib: add debug_level argument

--- src/ecalib.c
+++ src/ecalib.c
@@ -75,6 +75,7 @@ int main_ecalib(int argc, char* argv[])
 		OPT_INT('n', &conf.numsv, "", "()"),
 		OPT_FLOAT('v', &conf.var, "variance", "Variance of noise in data."),
 		OPT_SET('a', &conf.automate, "Automatically pick thresholds."),
+		OPT_INT('d', &debug_level, "level", "Debug level"),
 	};
 
 	cmdline(&argc, argv, 2, 3, usage_str, help_str, ARRAY_SIZE(opts), opts);
