d6d46a228101ff815d7018e4c57c70aeb6caa3c6
Christian Holme <christian.holme@med.uni-goettingen.de>
2018-07-31 14:04:13 +0200

    iter/iter4: add iter4_altmin interface

--- src/iter/iter4.h
+++ src/iter/iter4.h
@@ -7,6 +7,7 @@
 
 struct iter3_conf_s;
 struct iter_op_s;
+struct iter_nlop_s;
 struct nlop_s;
 
 typedef void iter4_fun_f(iter3_conf* _conf,
@@ -20,7 +21,14 @@ typedef void iter4_irgnm_f(iter3_conf* _conf,
 			   long M, const float* src,
 			   struct iter_op_s cb);
 
+typedef void iter4_altmin_f(iter3_conf* _conf,
+			struct nlop_s* nlop,
+			long NI, float* dst[NI],
+			long M, const float* src,
+			struct iter_nlop_s cb);
+
 iter4_irgnm_f iter4_irgnm;
+iter4_altmin_f iter4_altmin;
 iter4_fun_f iter4_landweber;
 
 
