9e1d9312ce024da7e7d9b6c1ca5d3addc33933d6
soumickOVGU <45432762+soumickOVGU@users.noreply.github.com>
2018-11-28 19:01:54 +0100

    Helper Function for BART with WSL

---
+++ matlab/WSLPathCorrection.m
@@ -0,0 +1,10 @@
+%Soumick Chatterjee <soumick.chatterjee@ovgu.de>
+
+function [outData] = WSLPathCorrection(inData)
+    outData=inData;
+    for i = 'a':'z' %Replace drive letters with /mnt/
+        outData=strrep(outData,[i,':'],['/mnt/',i]); %if drive letter is supplied in lowercase
+        outData=strrep(outData,[upper(i),':'],['/mnt/',i]); %if drive letter is supplied as uppercase
+    end
+    outData = strrep(outData, '\', '/'); %Change windows filesep to linux filesep
+end
