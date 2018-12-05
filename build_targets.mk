dcfe39bb2309b4ea05b1098a43b3b1f19a97151c
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2018-12-05 08:19:54 +0100

    replace zexpj with zexp

--- build_targets.mk
+++ build_targets.mk
@@ -1,7 +1,7 @@
 # Main build targets
 #
 TBASE=show slice crop resize join transpose squeeze flatten zeros ones flip circshift extract repmat bitmask reshape version delta copy casorati vec poly
-TFLP=scale invert conj fmac saxpy sdot spow cpyphs creal carg normalize cdf97 pattern nrmse mip avg cabs zexpj
+TFLP=scale invert conj fmac saxpy sdot spow cpyphs creal carg normalize cdf97 pattern nrmse mip avg cabs zexp
 TNUM=fft fftmod fftshift noise bench threshold conv rss filter mandelbrot wavelet window var std
 TRECO=pics pocsense sqpics itsense nlinv nufft rof sake wave lrmatrix estdims estshift estdelay wavepsf wshfl
 TCALIB=ecalib ecaltwo caldir walsh cc ccapply calmat svd estvar whiten
--- src/zexp.c
+++ src/zexp.c
@@ -1,9 +1,11 @@
 /* Copyright 2016. The Regents of the University of California.
+ * Copyright 2018. Martin Uecker.
  * All rights reserved. Use of this source code is governed by 
  * a BSD-style license which can be found in the LICENSE file.
  * 
  * Authors:
  * 2016 Jon Tamir <jtamir@eecs.berkeley.edu>
+ * 2018 Martin Uecker <martin.uecker@med.uni-goettingen.de>
  */
 
 #include <stdlib.h>
@@ -17,6 +19,7 @@
 
 #include "misc/mmio.h"
 #include "misc/misc.h"
+#include "misc/opts.h"
 
 
 
@@ -26,13 +29,21 @@
 
 
 static const char usage_str[] = "<input> <output>";
-static const char help_str[] = "Complex exponential of array, exp(1j * <input>).\n";
+static const char help_str[] = "Point-wise complex exponential.\n";
 
 
-int main_zexpj(int argc, char* argv[argc])
+int main_zexp(int argc, char* argv[argc])
 {
-	mini_cmdline(&argc, argv, 2, usage_str, help_str);
+	bool img = false;
 
+	const struct opt_s opts[] = {
+
+		OPT_SET('i', &img, "imaginary"),
+	};
+
+	cmdline(&argc, argv, 2, 2, usage_str, help_str, ARRAY_SIZE(opts), opts);
+
+	
 	num_init();
 
 	long dims[DIMS];
@@ -40,7 +51,7 @@ int main_zexpj(int argc, char* argv[argc])
 	complex float* in_data = load_cfl(argv[1], DIMS, dims);
 	complex float* out_data = create_cfl(argv[2], DIMS, dims);
 
-	md_zexpj(DIMS, dims, out_data, in_data);
+	(img ? md_zexpj : md_zexp)(DIMS, dims, out_data, in_data);
 
 	unmap_cfl(DIMS, dims, in_data);
 	unmap_cfl(DIMS, dims, out_data);
