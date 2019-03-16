ac48b2dd14c95d345a956405e85659259a0d695e
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2019-03-17 00:35:28 +0100

    estdelay tests

--- tests/estdelay.mk
+++ tests/estdelay.mk
@@ -8,6 +8,42 @@ tests/test-estdelay: estdelay traj phantom nrmse
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+tests/test-estdelay-coils: estdelay traj phantom nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/traj -G -q0.3:-0.1:0.2 -y5 t.ra						;\
+	$(TOOLDIR)/traj -G -r -y5 n.ra								;\
+	$(TOOLDIR)/scale 0.5 n.ra ns.ra								;\
+	$(TOOLDIR)/scale 0.5 t.ra ts.ra								;\
+	$(TOOLDIR)/phantom -s8 -k -t ts.ra k.ra							;\
+	$(TOOLDIR)/traj -G -q`$(TOOLDIR)/estdelay ns.ra k.ra` -y5 t2.ra				;\
+	$(TOOLDIR)/nrmse -t 0.004 t.ra t2.ra							;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 
-TESTS += tests/test-estdelay
+tests/test-estdelay-ring: estdelay traj phantom nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/traj -G -q0.3:-0.1:0.2 -c -y5 t.ra						;\
+	$(TOOLDIR)/traj -G -c -r -y5 n.ra							;\
+	$(TOOLDIR)/scale 0.5 n.ra ns.ra								;\
+	$(TOOLDIR)/scale 0.5 t.ra ts.ra								;\
+	$(TOOLDIR)/phantom -k -t ts.ra k.ra							;\
+	$(TOOLDIR)/traj -G -q`$(TOOLDIR)/estdelay -R ns.ra k.ra` -c -y5 t2.ra			;\
+	$(TOOLDIR)/nrmse -t 0.003 t.ra t2.ra							;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+tests/test-estdelay-scale: estdelay scale traj phantom nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/traj -D -q1.5:1:-0.5 -r -y8 t.ra						;\
+	$(TOOLDIR)/scale 0.5 t.ra ts.ra								;\
+	$(TOOLDIR)/phantom -k -t ts.ra k.ra							;\
+	$(TOOLDIR)/traj -D -q`$(TOOLDIR)/estdelay ts.ra k.ra` -r -y8 t2.ra			;\
+	$(TOOLDIR)/scale 0.5 t2.ra t2s.ra							;\
+	$(TOOLDIR)/nrmse -t 0.0001 ts.ra t2s.ra							;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+
+TESTS += tests/test-estdelay tests/test-estdelay-ring tests/test-estdelay-coils tests/test-estdelay-scale
 
