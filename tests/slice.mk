b4400c69a8e1fe1970d39c99269c4f133b368d0c
Christian Holme <christian.holme@med.uni-goettingen.de>
2019-05-29 16:33:21 +0200

    add test for new multidimensional slice

--- tests/slice.mk
+++ tests/slice.mk
@@ -10,6 +10,14 @@ tests/test-slice: ones slice resize nrmse
 	touch $@
 
 
+tests/test-slice-multidim: ones slice resize nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/ones 3 1 1 1 o.ra								;\
+	$(TOOLDIR)/resize -c 0 100 1 100 2 100 o.ra o0.ra						;\
+	$(TOOLDIR)/slice 0 50 1 50 2 50 o0.ra o1.ra							;\
+	$(TOOLDIR)/nrmse -t 0. o1.ra o.ra							;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 
-TESTS += tests/test-slice
+TESTS += tests/test-slice tests/test-slice-multidim
 
