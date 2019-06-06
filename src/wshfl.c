47fdbc8fbc9f7e0679e68f8296e7131ffcf46062
Siddharth Iyer <ssi@mit.edu>
2019-06-06 11:05:07 -0400

    calloc -> alloc_sameplace in kern_adj

--- src/wshfl.c
+++ src/wshfl.c
@@ -246,7 +246,8 @@ static void kern_adjoint(const linop_data_t* _data, complex float* dst, const co
 	long line_dims[]    = {wx, nc,  1,  1};
 	long vthrd_dims[]   = {wx, nc, tf,  1, num_threads};
 
-	complex float* vec = md_calloc(5, vthrd_dims, CFL_SIZE);
+	complex float* vec = md_alloc_sameplace(5, vthrd_dims, CFL_SIZE, dst);
+	md_clear(DIMS, vthrd_dims, vec, CFL_SIZE);
 
 	long vec_str[4];
 	md_calc_strides(4, vec_str, vec_dims, CFL_SIZE);
