efa2666af12dd69fa910d853462133a791561bcc
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-01-03 21:27:37 +0100

    missing __VLA

--- src/num/flpmath.h
+++ src/num/flpmath.h
@@ -247,7 +247,7 @@ extern float md_scalar(unsigned int D, const long dim[__VLA(D)], const float* pt
 extern void md_rss(unsigned int D, const long dims[__VLA(D)], unsigned int flags, float* dst, const float* src);
 extern void md_zrss(unsigned int D, const long dims[__VLA(D)], unsigned int flags, _Complex float* dst, const _Complex float* src);
 extern void md_zss(unsigned int D, const long dims[__VLA(D)], unsigned int flags, _Complex float* dst, const _Complex float* src);
-extern void md_zss2(unsigned int D, const long dims[D], unsigned int flags, const long str2[D], _Complex float* dst, const long str1[D], const _Complex float* src);
+extern void md_zss2(unsigned int D, const long dims[__VLA(D)], unsigned int flags, const long str2[__VLA(D)], _Complex float* dst, const long str1[__VLA(D)], const _Complex float* src);
 
 extern void md_zstd(unsigned int D, const long dims[__VLA(D)], unsigned int flags, _Complex float* optr, const _Complex float* iptr);
 extern void md_zstd2(unsigned int D, const long dims[__VLA(D)], unsigned int flags, const long ostr[__VLA(D)], _Complex float* optr, const long istr[__VLA(D)], const _Complex float* iptr);
@@ -255,8 +255,8 @@ extern void md_zstd2(unsigned int D, const long dims[__VLA(D)], unsigned int fla
 extern void md_zvar(unsigned int D, const long dims[__VLA(D)], unsigned int flags, _Complex float* optr, const _Complex float* iptr);
 extern void md_zvar2(unsigned int D, const long dims[__VLA(D)], unsigned int flags, const long ostr[__VLA(D)], _Complex float* optr, const long istr[__VLA(D)], const _Complex float* iptr);
 
-extern void md_zcovar(unsigned int D, const long dims[D], unsigned int flags, _Complex float* optr, const _Complex float* iptr1, const _Complex float* iptr2);
-extern void md_zcovar2(unsigned int D, const long dims[D], unsigned int flags, const long ostr[D], _Complex float* optr, const long istr1[D], const _Complex float* iptr1, const long istr2[D], const _Complex float* iptr2);
+extern void md_zcovar(unsigned int D, const long dims[__VLA(D)], unsigned int flags, _Complex float* optr, const _Complex float* iptr1, const _Complex float* iptr2);
+extern void md_zcovar2(unsigned int D, const long dims[__VLA(D)], unsigned int flags, const long ostr[__VLA(D)], _Complex float* optr, const long istr1[__VLA(D)], const _Complex float* iptr1, const long istr2[__VLA(D)], const _Complex float* iptr2);
 
 extern void md_zavg(unsigned int D, const long dims[__VLA(D)], unsigned int flags, _Complex float* optr, const _Complex float* iptr);
 extern void md_zavg2(unsigned int D, const long dims[__VLA(D)], unsigned int flags, const long ostr[__VLA(D)], _Complex float* optr, const long istr[__VLA(D)], const _Complex float* iptr);
