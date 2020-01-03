1870ceedd7c67bce162c964272a835d6a999479d
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-01-03 23:14:39 +0100

    better ring test

--- tests/estdelay.mk
+++ tests/estdelay.mk
@@ -31,13 +31,13 @@ tests/test-estdelay-coils: estdelay scale traj phantom nrmse
 
 tests/test-estdelay-ring: estdelay scale traj phantom nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/traj -G -q0.3:-0.1:0.2 -c -y5 t.ra						;\
-	$(TOOLDIR)/traj -G -c -r -y5 n.ra							;\
+	$(TOOLDIR)/traj -D -r -q0.3:0.1:0.2 -O -c -y5 t.ra					;\
+	$(TOOLDIR)/traj -D -c -r -y5 n.ra							;\
 	$(TOOLDIR)/scale 0.5 n.ra ns.ra								;\
 	$(TOOLDIR)/scale 0.5 t.ra ts.ra								;\
 	$(TOOLDIR)/phantom -k -t ts.ra k.ra							;\
-	$(TOOLDIR)/traj -G -q`$(TOOLDIR)/estdelay -R ns.ra k.ra` -c -y5 t2.ra			;\
-	$(TOOLDIR)/nrmse -t 0.003 t.ra t2.ra							;\
+	$(TOOLDIR)/traj -D -r -q`$(TOOLDIR)/estdelay -R ns.ra k.ra` -O -c -y5 t2.ra		;\
+	$(TOOLDIR)/nrmse -t 0.0001 t.ra t2.ra							;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
