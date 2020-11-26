0ef7b470a5069bc7a65d92a749ff91b979afee63
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2020-11-26 15:08:17 +0100

    Add projections

---
+++ src/iter/proj.h
@@ -0,0 +1,5 @@
+
+const struct operator_p_s* operator_project_pos_real_create(long N, const long dims[N]);
+const struct operator_p_s* operator_project_mean_free_create(long N, const long dims[N], unsigned long bflag);
+const struct operator_p_s* operator_project_sphere_create(long N, const long dims[N], unsigned long bflag, bool real);
+const struct operator_p_s* operator_project_mean_free_sphere_create(long N, const long dims[N], unsigned long bflag, bool real);
