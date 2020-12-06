0f06b3a9b647b68901c1af71e10d87479886aa1d
Jon Tamir <jtamir@utexas.edu>
2020-12-05 21:09:10 -0600

    relax test

--- utests/test_flpmath2.c
+++ utests/test_flpmath2.c
@@ -208,7 +208,7 @@ static bool test_optimized_md_fmac2_gemm(void) { UT_ASSERT(test_optimized_md_fma
 static bool test_optimized_md_fmac2_gemm2(void) { UT_ASSERT(test_optimized_md_fmac2_flags(11ul, 14ul, 13ul, true, 1.e-6));}
 static bool test_optimized_md_fmac2_ger(void) { UT_ASSERT(test_optimized_md_fmac2_flags(3ul, 1ul, 2ul, true, 2.e-6)); }
 static bool test_optimized_md_fmac2_ger2(void) { UT_ASSERT(test_optimized_md_fmac2_flags(7ul, 5ul, 6ul, true, 1.e-6)); }
-static bool test_optimized_md_fmac2_axpy(void) { UT_ASSERT(test_optimized_md_fmac2_flags(1ul, 1ul, 0ul, true, 2.e-6)); }
+static bool test_optimized_md_fmac2_axpy(void) { UT_ASSERT(test_optimized_md_fmac2_flags(1ul, 1ul, 0ul, true, 3.e-6)); }
 static bool test_optimized_md_fmac2_axpy2(void) { UT_ASSERT(test_optimized_md_fmac2_flags(3ul, 2ul, 3ul, true, 1.e-6));}
 
 UT_REGISTER_TEST(test_optimized_md_fmac2_dot);
