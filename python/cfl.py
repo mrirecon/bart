6ae13fca79ed59953b4218867a09f327cc2b42b6
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2020-12-14 17:12:52 +0100

    Update python cfl + multicfl

--- python/cfl.py
+++ python/cfl.py
@@ -6,16 +6,20 @@
 # 2013 Martin Uecker <uecker@eecs.berkeley.edu>
 # 2015 Jonathan Tamir <jtamir@eecs.berkeley.edu>
 
+from __future__ import print_function
+from __future__ import with_statement
 
 import numpy as np
+import mmap
+import os
+
 
 def readcfl(name):
     # get dims from .hdr
-    h = open(name + ".hdr", "r")
-    h.readline() # skip
-    l = h.readline()
-    h.close()
-    dims = [int(i) for i in l.split( )]
+    with open(name + ".hdr", "rt") as h:
+        h.readline() # skip
+        l = h.readline()
+    dims = [int(i) for i in l.split()]
 
     # remove singleton dimensions from the end
     n = np.prod(dims)
@@ -23,19 +27,83 @@ def readcfl(name):
     dims = dims[:np.searchsorted(dims_prod, n)+1]
 
     # load data and reshape into dims
-    d = open(name + ".cfl", "r")
-    a = np.fromfile(d, dtype=np.complex64, count=n);
-    d.close()
+    with open(name + ".cfl", "rb") as d:
+        a = np.fromfile(d, dtype=np.complex64, count=n);
     return a.reshape(dims, order='F') # column-major
+
+def readmulticfl(name):
+    # get dims from .hdr
+    with open(name + ".hdr", "rt") as h:
+        lines = h.read().splitlines()
+
+    index_dim = 1 + lines.index('# Dimensions')
+    total_size = int(lines[index_dim])
+    index_sizes = 1 + lines.index('# SizesDimensions')
+    sizes = [int(i) for i in lines[index_sizes].split()]
+    index_dims = 1 + lines.index('# MultiDimensions')
+
+    with open(name + ".cfl", "rb") as d:
+        a = np.fromfile(d, dtype=np.complex64, count=total_size)
+
+    offset = 0
+    result = []
+    for i in range(len(sizes)):
+        dims = ([int(i) for i in lines[index_dims + i].split()])
+        n = np.prod(dims)
+        result.append(a[offset:offset+n].reshape(dims, order='F'))
+        offset += n
+
+    if total_size != offset:
+        print("Error")
+
+    return result
 
 
 def writecfl(name, array):
-    h = open(name + ".hdr", "w")
-    h.write('# Dimensions\n')
-    for i in (array.shape):
-            h.write("%d " % i)
-    h.write('\n')
-    h.close()
-    d = open(name + ".cfl", "w")
-    array.T.astype(np.complex64).tofile(d) # tranpose for column-major order
-    d.close()
+    with open(name + ".hdr", "wt") as h:
+        h.write('# Dimensions\n')
+        for i in (array.shape):
+                h.write("%d " % i)
+        h.write('\n')
+
+    size = np.prod(array.shape) * np.dtype(np.complex64).itemsize
+
+    with open(name + ".cfl", "a+b") as d:
+        os.ftruncate(d.fileno(), size)
+        mm = mmap.mmap(d.fileno(), size, flags=mmap.MAP_SHARED, prot=mmap.PROT_WRITE)
+        mm.write(array.astype(np.complex64).tobytes(order='F'))
+        mm.close()
+        #with mmap.mmap(d.fileno(), size, flags=mmap.MAP_SHARED, prot=mmap.PROT_WRITE) as mm:
+        #    mm.write(array.astype(np.complex64).tobytes(order='F'))
+
+def writemulticfl(name, arrays):
+    size = 0
+    dims = []
+
+    for array in arrays:
+        size += array.size
+        dims.append(array.shape)
+
+    with open(name + ".hdr", "wt") as h:
+        h.write('# Dimensions\n')
+        h.write("%d\n" % size)
+
+        h.write('# SizesDimensions\n')
+        for dim in dims:
+            h.write("%d " % len(dim))
+        h.write('\n')
+
+        h.write('# MultiDimensions\n')
+        for dim in dims:
+            for i in dim:
+                h.write("%d " % i)
+            h.write('\n')
+            
+    size = size * np.dtype(np.complex64).itemsize
+
+    with open(name + ".cfl", "a+b") as d:
+        os.ftruncate(d.fileno(), size)
+        mm = mmap.mmap(d.fileno(), size, flags=mmap.MAP_SHARED, prot=mmap.PROT_WRITE)
+        for array in arrays:
+            mm.write(array.astype(np.complex64).tobytes(order='F'))
+        mm.close()
