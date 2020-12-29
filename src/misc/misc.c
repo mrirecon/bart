bb7d99dd14e7268894ce4ba9b9d54c6b3680ab62
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2020-12-29 11:32:23 +0100

    coding style

--- src/misc/misc.c
+++ src/misc/misc.c
@@ -308,47 +308,60 @@ void quicksort(int N, int ord[N], const void* data, quicksort_cmp_t cmp)
  *
  * @note In-place sort. The input array contents are not preserved in their original order. 
  */
-float quickselect(float *arr, unsigned int n, unsigned int k) {
-	unsigned long i,ir,j,l,mid;
+float quickselect(float *arr, unsigned int n, unsigned int k)
+{
+	unsigned long i, ir, j, l, mid;
 	float a;
    
-	l=0;
-	ir=n-1;
+	l = 0;
+	ir = n - 1;
+
 	for(;;) {
-		if (ir <= l+1) { 
-			if (ir == l+1 && arr[ir] > arr[l]) {
-				SWAP(arr[l],arr[ir]);
-			}
+
+		if (ir <= l + 1) {
+
+			if ((ir == l + 1) && (arr[ir] > arr[l]))
+				SWAP(arr[l], arr[ir]);
+
 			return arr[k];
 		}
-		else {
-			mid=(l+ir) >> 1; 
-			SWAP(arr[mid],arr[l+1]);
-			if (arr[l] < arr[ir]) {
-				SWAP(arr[l],arr[ir]);
-			}
-			if (arr[l+1] < arr[ir]) {
-				SWAP(arr[l+1],arr[ir]);
-			}
-			if (arr[l] < arr[l+1]) {
-				SWAP(arr[l],arr[l+1]);
-			}
-			i=l+1; 
-			j=ir;
-			a=arr[l+1];
 
-			for (;;) { 
-				do i++; while (arr[i] > a); 
-				do j--; while (arr[j] < a); 
-				if (j < i) break; 
-				SWAP(arr[i],arr[j]);
-			} 
-			arr[l+1]=arr[j]; 
-			arr[j]=a;
-      
-			if (j >= k) ir=j-1; 
-			if (j <= k) l=i;
+		mid = (l + ir) / 2;
+
+		SWAP(arr[mid], arr[l + 1]);
+
+		if (arr[l] < arr[ir])
+			SWAP(arr[l], arr[ir]);
+
+		if (arr[l + 1] < arr[ir])
+			SWAP(arr[l + 1], arr[ir]);
+
+		if (arr[l] < arr[l + 1])
+			SWAP(arr[l], arr[l + 1]);
+
+		i = l + 1;
+		j = ir;
+		a = arr[l + 1];
+
+		for (;;) {
+
+			do i++; while (arr[i] > a);
+			do j--; while (arr[j] < a);
+
+			if (j < i)
+				break;
+
+			SWAP(arr[i], arr[j]);
 		}
+
+		arr[l + 1] = arr[j];
+		arr[j] = a;
+
+		if (j >= k)
+			ir = j - 1;
+
+		if (j <= k)
+			l = i;
 	}
 }
 
@@ -359,48 +372,62 @@ float quickselect(float *arr, unsigned int n, unsigned int k) {
  * Possibly faster for application to complex arrays.
  *
  */
-float quickselect_complex(complex float *arr, unsigned int n, unsigned int k) {
-	unsigned long i,ir,j,l,mid;
+float quickselect_complex(complex float* arr, unsigned int n, unsigned int k)
+{
+	unsigned long i, ir, j, l, mid;
 	float a;
 	complex float ca;
    
-	l=0;
-	ir=n-1;
+	l = 0;
+	ir = n - 1;
+
 	for(;;) {
-		if (ir <= l+1) { 
-			if (ir == l+1 && cabsf(arr[ir]) > cabsf(arr[l])) {
-				SWAP(arr[l],arr[ir]);
-			}
+
+		if (ir <= l + 1) {
+
+			if ((ir == l + 1) && (cabsf(arr[ir]) > cabsf(arr[l])))
+				SWAP(arr[l], arr[ir]);
+
 			return cabsf(arr[k]);
 		}
-		else {
-			mid=(l+ir) >> 1; 
-			SWAP(arr[mid],arr[l+1]);
-			if (cabsf(arr[l]) < cabsf(arr[ir])) {
-				SWAP(arr[l],arr[ir]);
-			}
-			if (cabsf(arr[l+1]) < cabsf(arr[ir])) {
-				SWAP(arr[l+1],arr[ir]);
-			}
-			if (cabsf(arr[l]) < cabsf(arr[l+1])) {
-				SWAP(arr[l],arr[l+1]);
-			}
-			i=l+1; 
-			j=ir;
-			a=cabsf(arr[l+1]);
-			ca = arr[l+1];
-			for (;;) { 
-				do i++; while (cabsf(arr[i]) > a); 
-				do j--; while (cabsf(arr[j]) < a); 
-				if (j < i) break; 
-				SWAP(arr[i],arr[j]);
-			} 
-			arr[l+1]=arr[j]; 
-			arr[j]=ca;
-      
-			if (j >= k) ir=j-1; 
-			if (j <= k) l=i;
+
+		mid = (l + ir) / 2;
+
+		SWAP(arr[mid], arr[l + 1]);
+
+		if (cabsf(arr[l]) < cabsf(arr[ir]))
+			SWAP(arr[l], arr[ir]);
+
+		if (cabsf(arr[l + 1]) < cabsf(arr[ir]))
+			SWAP(arr[l + 1], arr[ir]);
+
+		if (cabsf(arr[l]) < cabsf(arr[l + 1]))
+			SWAP(arr[l], arr[l + 1]);
+
+		i = l + 1;
+		j = ir;
+		a = cabsf(arr[l + 1]);
+		ca = arr[l + 1];
+
+		for (;;) {
+
+			do i++; while (cabsf(arr[i]) > a);
+			do j--; while (cabsf(arr[j]) < a);
+
+			if (j < i)
+				break;
+
+			SWAP(arr[i], arr[j]);
 		}
+
+		arr[l + 1] = arr[j];
+		arr[j] = ca;
+
+		if (j >= k)
+			ir = j - 1;
+
+		if (j <= k)
+			l = i;
 	}
 }
 
