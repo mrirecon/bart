2b6a234f2d265d619349f0a4fe3b91f26cb6473a
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2021-04-15 00:05:16 +0200

    fix test

--- tests/traj.mk
+++ tests/traj.mk
@@ -4,7 +4,7 @@
 tests/test-traj-custom: traj poly nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
 	$(TOOLDIR)/traj -x128 -y128 -r traja.ra				;\
-	$(TOOLDIR)/bart poly 128 1 0 0.0245436926 angle.ra		;\
+	$(TOOLDIR)/poly 128 1 0 0.0245436926 angle.ra			;\
 	$(TOOLDIR)/traj -x128 -y128 -r -C angle.ra trajb.ra		;\
 	$(TOOLDIR)/nrmse -t 0.000001 traja.ra trajb.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
