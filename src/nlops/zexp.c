19df772dcac6fc704f2699de24c247e2a4a3fcf6
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2021-04-15 15:32:41 +0200

    Add some nlops

--- src/nlops/zexp.c
+++ src/nlops/zexp.c
@@ -16,69 +16,74 @@
 #include "num/flpmath.h"
 
 #include "nlops/nlop.h"
+#include "nlops/nlop_jacobian.h"
 
 #include "zexp.h"
-
 
 struct zexp_s {
 
 	INTERFACE(nlop_data_t);
-
-	int N;
-	const long* dims;
-	complex float* xn;
 };
 
 DEF_TYPEID(zexp_s);
 
-static void zexp_fun(const nlop_data_t* _data, complex float* dst, const complex float* src)
+static void zexp_free(const nlop_data_t* _data)
 {
-	const auto data = CAST_DOWN(zexp_s, _data);
-
-	md_zexp(data->N, data->dims, data->xn, src);
-	md_copy(data->N, data->dims, dst, data->xn, CFL_SIZE);
+	xfree(_data);
 }
 
-static void zexp_der(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
+static void zexp_apply(const nlop_data_t* _data, int N, const long dims[N], complex float* dst, const complex float* src, complex float* der)
 {
-	UNUSED(o);
-	UNUSED(i);
+	UNUSED(_data);
 
-	const auto data = CAST_DOWN(zexp_s, _data);
-	md_zmul(data->N, data->dims, dst, src, data->xn);
+	md_zexp(N, dims, dst, src);
+
+	if (NULL != der)
+		md_copy(N, dims, der, dst, CFL_SIZE);
 }
-
-static void zexp_adj(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
-{
-	UNUSED(o);
-	UNUSED(i);
-
-	const auto data = CAST_DOWN(zexp_s, _data);
-	md_zmulc(data->N, data->dims, dst, src, data->xn);
-}
-
-static void zexp_del(const nlop_data_t* _data)
-{
-	const auto data = CAST_DOWN(zexp_s, _data);
-
-	md_free(data->xn);
-	xfree(data->dims);
-	xfree(data);
-}
-
 
 struct nlop_s* nlop_zexp_create(int N, const long dims[N])
 {
 	PTR_ALLOC(struct zexp_s, data);
 	SET_TYPEID(zexp_s, data);
 
-	PTR_ALLOC(long[N], ndims);
-	md_copy_dims(N, *ndims, dims);
+	return nlop_zdiag_create(N, dims, CAST_UP(PTR_PASS(data)), zexp_apply, zexp_free);
+}
 
-	data->N = N;
-	data->dims = *PTR_PASS(ndims);
-	data->xn = md_alloc(N, dims, CFL_SIZE);
 
-	return nlop_create(N, dims, N, dims, CAST_UP(PTR_PASS(data)),
-		zexp_fun, zexp_der, zexp_adj, NULL, NULL, zexp_del);
+
+
+
+
+struct zlog_s {
+
+	INTERFACE(nlop_data_t);
+};
+
+DEF_TYPEID(zlog_s);
+
+static void zlog_free(const nlop_data_t* _data)
+{
+	xfree(_data);
+}
+
+static void zlog_apply(const nlop_data_t* _data, int N, const long dims[N], complex float* dst, const complex float* src, complex float* der)
+{
+	UNUSED(_data);
+
+	if (NULL != der) {
+
+		md_zfill(N, dims, der, 1);
+		md_zdiv(N, dims, der, der, src);
+	}
+
+	md_zlog(N, dims, dst, src);
+}
+
+struct nlop_s* nlop_zlog_create(int N, const long dims[N])
+{
+	PTR_ALLOC(struct zlog_s, data);
+	SET_TYPEID(zlog_s, data);
+
+	return nlop_zdiag_create(N, dims, CAST_UP(PTR_PASS(data)), zlog_apply, zlog_free);
 }
---
+++ src/nlops/zhyperbolic.h
@@ -0,0 +1,5 @@
+
+struct nlop_s;
+extern struct nlop_s* nlop_zsinh_create(int N, const long dims[N]);
+extern struct nlop_s* nlop_zcosh_create(int N, const long dims[N]);
+
