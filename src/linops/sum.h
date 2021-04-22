21fbbb1f88d2a88eb3cd6a3498c35b638498b2aa
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2021-04-22 16:47:40 +0200

    Add linop repmat (adjoing sum)

--- src/linops/sum.h
+++ src/linops/sum.h
@@ -3,3 +3,4 @@ struct linop_s;
 extern const struct linop_s* linop_avg_create(int N, const long imgd_dims[N], unsigned long flags);
 extern const struct linop_s* linop_sum_create(int N, const long imgd_dims[N], unsigned long flags);
 extern const struct linop_s* linop_scaled_sum_create(int N, const long imgd_dims[N], unsigned long flags);
+extern const struct linop_s* linop_repmat_create(int N, const long odims[N], unsigned long flags);
