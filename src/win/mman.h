eb97924934fd2306cb1a1f77c39ffa06d685610d
Tamás Hakkel <hakkelt@gmail.com>
2021-05-14 16:27:40 +0200

    minor correction in licence text

--- src/win/mman.h
+++ src/win/mman.h
@@ -3,7 +3,7 @@ MIT licence
 
 Copyright (c) 2021 Tamás Hakkel <hakkelt@gmail.com>
 Copyright (c) 2013-2019 Steven Lee
-Copyright (c) 2010-2012 Kutuzov Viktor
+Copyright (c) 2010-2012 Viktor Kutuzov
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
