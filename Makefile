07ed3a637bea9ea564095c7703cb02300831b3d6
Tamás Hakkel <hakkelt@gmail.com>
2021-06-02 14:53:55 +0200

    Og flag for debug mode + memcfl_exists

--- Makefile
+++ Makefile
@@ -125,7 +125,11 @@ ALLDEPS = $(shell find $(srcdir) utests -name ".*.d")
 
 # Compilation flags
 
+ifneq ($(DEBUG),1)
 OPT = -O2
+else
+OPT = -Og
+endif
 CPPFLAGS ?= -Wall -Wextra
 CFLAGS ?= $(OPT) -Wmissing-prototypes
 CXXFLAGS ?= $(OPT)
--- src/misc/memcfl.h
+++ src/misc/memcfl.h
@@ -3,6 +3,7 @@
 #include <complex.h>
 
 extern void memcfl_register(const char* name, int D, const long dims[D], complex float* data, bool managed);
+extern bool memcfl_exists(const char* name);
 extern complex float* memcfl_create(const char* name, int D, const long dims[D]);
 extern complex float* memcfl_load(const char* name, int D, long dims[D]);
 extern bool memcfl_unmap(const complex float* p);
