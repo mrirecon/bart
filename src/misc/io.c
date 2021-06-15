mrirecon/bart

commit 11b0a3c0ae2ee40f663f77418d0770160f8ed2c8

misc/io: io_unregister removes all IOs of that name

src/misc/io.c
@@ -125,7 +125,7 @@ void io_unregister(const char* name)
 			xfree(io->name);
 			xfree(io);
 
-			return;
+			continue;
 		}
 
 		iop = &io->prev;

commit f0e12f8ee5e3c30ea3e2c4e1a4bed2e1225ab191

misc/io.c: fix bug in io_unregister
introduced by me in 11b0a3c0

src/misc/io.c
@@ -181,7 +181,7 @@ void io_unregister(const char* name)
 			xfree(io->name);
 			xfree(io);
 
-			continue;
+			return;
 		}
 
 		iop = &io->prev;