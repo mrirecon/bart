a3f98500ad68fcf25a54e296b324cffd7b9ea1b0
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2021-10-29 16:28:00 +0200

    refactor mad loss

--- src/nn/losses.c
+++ src/nn/losses.c
@@ -174,7 +174,7 @@ const struct nlop_s* nlop_nrmse_create(int N, const long dims[N], unsigned long 
 }
 
 
-struct mad_s {
+struct zasum_s {
 
 	INTERFACE(nlop_data_t);
 
@@ -185,32 +185,21 @@ struct mad_s {
 	float* der;
 };
 
-DEF_TYPEID(mad_s);
+DEF_TYPEID(zasum_s);
 
-static void mad_fun(const nlop_data_t* _data, int D, complex float* args[D])
+static void zasum_fun(const nlop_data_t* _data, complex float* dst, const complex float* src)
 {
-	const auto data = CAST_DOWN(mad_s, _data);
-	assert(3 == D);
+	const auto data = CAST_DOWN(zasum_s, _data);
 
-	complex float* dst = args[0];
-	const float* src1 = (float*)args[1];
-	const float* src2 = (float*)args[2];
-
-#ifdef USE_CUDA
-	assert((cuda_ondevice(dst) == cuda_ondevice(src1)) && (cuda_ondevice(src1) == cuda_ondevice(src2)));
-#endif
 	if (NULL == data->der)
 		data->der = md_alloc_sameplace(data->N, data->rdims, FL_SIZE, dst);
 
-	md_sub(data->N, data->rdims, data->der, src1, src2);
+	complex float result = md_asum(data->N, data->rdims, (const float*)src) / data->scaling;
 
-	complex float result = md_asum(data->N, data->rdims, data->der) / data->scaling;
-
-	md_greatequal(data->N, data->rdims, data->der, src1, src2);
+	md_sgreatequal(data->N, data->rdims, data->der, (const float*)src, 0);
 
 	float* tmp = md_alloc_sameplace(data->N, data->rdims, FL_SIZE, dst);
-
-	md_greatequal(data->N, data->rdims, tmp, src2, src1);
+	md_slessequal(data->N, data->rdims, tmp, (const float*)src, 0);
 	md_sub(data->N, data->rdims, data->der, data->der, tmp);
 	md_free(tmp);
 
@@ -220,67 +209,42 @@ static void mad_fun(const nlop_data_t* _data, int D, complex float* args[D])
 }
 
 
-static void mad_der1(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
+static void zasum_der(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
 {
 	UNUSED(o);
 	UNUSED(i);
 
-	auto data = CAST_DOWN(mad_s, _data);
+	auto data = CAST_DOWN(zasum_s, _data);
 	assert(NULL != data->der);
 
 	md_clear(1, MD_DIMS(1), dst, CFL_SIZE);
 	md_tenmul(data->N, MD_SINGLETON_DIMS(data->N), (float*)dst, data->rdims, (float*)src, data->rdims, data->der);
 }
 
-static void mad_der2(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
+static void zasum_adj(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
 {
 	UNUSED(o);
 	UNUSED(i);
 
-	auto data = CAST_DOWN(mad_s, _data);
-	assert(NULL != data->der);
-
-	md_clear(1, MD_DIMS(1), dst, CFL_SIZE);
-	md_tenmul(data->N, MD_SINGLETON_DIMS(data->N), (float*)dst, data->rdims, (float*)src, data->rdims, data->der);
-	md_smul(1, MD_DIMS(1), (float*)dst, (float*)dst, -1);
-}
-
-static void mad_adj1(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
-{
-	UNUSED(o);
-	UNUSED(i);
-
-	auto data = CAST_DOWN(mad_s, _data);
+	auto data = CAST_DOWN(zasum_s, _data);
 	assert(NULL != data->der);
 
 	md_tenmul(data->N, data->rdims, (float*)dst, MD_SINGLETON_DIMS(data->N), (float*)src, data->rdims, data->der);
 }
 
-static void mad_adj2(const nlop_data_t* _data, unsigned int o, unsigned int i, complex float* dst, const complex float* src)
+static void zasum_del(const nlop_data_t* _data)
 {
-	UNUSED(o);
-	UNUSED(i);
-
-	auto data = CAST_DOWN(mad_s, _data);
-	assert(NULL != data->der);
-
-	md_tenmul(data->N, data->rdims, (float*)dst, MD_SINGLETON_DIMS(data->N), (float*)src, data->rdims, data->der);
-	md_smul(data->N, data->rdims, (float*)dst, (float*)dst, -1);
-}
-
-static void mad_del(const nlop_data_t* _data)
-{
-	const auto data = CAST_DOWN(mad_s, _data);
+	const auto data = CAST_DOWN(zasum_s, _data);
 
 	md_free(data->der);
 	xfree(data->rdims);
 	xfree(data);
 }
 
-const struct nlop_s* nlop_mad_create(int N, const long dims[N], unsigned long mean_dims)
+const struct nlop_s* nlop_zasum_create(int N, const long dims[N], unsigned long mean_dims)
 {
-	PTR_ALLOC(struct mad_s, data);
-	SET_TYPEID(mad_s, data);
+	PTR_ALLOC(struct zasum_s, data);
+	SET_TYPEID(zasum_s, data);
 
 	PTR_ALLOC(long[N + 1], rdims);
 	(*rdims[0] = 2);
@@ -294,13 +258,17 @@ const struct nlop_s* nlop_mad_create(int N, const long dims[N], unsigned long me
 	md_select_dims(N, mean_dims, tdims, dims);
 	data->scaling = (float)md_calc_size(N, tdims);
 
-	long nl_odims[1][1];
-	md_copy_dims(1, nl_odims[0], MD_SINGLETON_DIMS(1));
-	long nl_idims[2][N];
-	md_copy_dims(N, nl_idims[0], data->rdims + 1);
-	md_copy_dims(N, nl_idims[1], data->rdims + 1);
+	return nlop_create(1, MD_SINGLETON_DIMS(1), N, dims, CAST_UP(PTR_PASS(data)), zasum_fun, zasum_der, zasum_adj, NULL, NULL, zasum_del);
+}
 
-	return nlop_generic_create(1, 1, nl_odims, 2, N, nl_idims, CAST_UP(PTR_PASS(data)), mad_fun, (nlop_der_fun_t[2][1]){ { mad_der1 }, { mad_der2 } }, (nlop_der_fun_t[2][1]){ { mad_adj1 }, { mad_adj2 } }, NULL, NULL, mad_del);
+const struct nlop_s* nlop_z1norm_create(int N, const long dims[N], unsigned long mean_dims)
+{
+	return nlop_chain_FF(nlop_smo_abs_create(N, dims, 0), nlop_zasum_create(N, dims, mean_dims));
+}
+
+const struct nlop_s* nlop_mad_create(int N, const long dims[N], unsigned long mean_dims)
+{
+	return nlop_chain2_FF(nlop_zaxpbz_create(N, dims, 1, -1), 0, nlop_zasum_create(N, dims, mean_dims), 0);
 }
 
 
