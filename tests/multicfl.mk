8ba111bb61ecaa1662bedad36ad4c8d9d3f510d1
Christian Holme <christian.holme@med.uni-goettingen.de>
2021-11-08 14:46:33 +0100

    tests/multicfl.mk: adapt to interface changes
    As the input is now in the beginning, the test needs to be adapted

--- tests/multicfl.mk
+++ tests/multicfl.mk
@@ -3,7 +3,7 @@ tests/test-multicfl: multicfl ones nrmse $(TESTS_OUT)/shepplogan_coil.ra $(TESTS
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)								;\
 	$(TOOLDIR)/ones 7 3 5 1 1 2 1 5 o.ra									;\
 	$(TOOLDIR)/multicfl $(TESTS_OUT)/shepplogan_coil.ra $(TESTS_OUT)/shepplogan_coil_ksp.ra o.ra comb	;\
-	$(TOOLDIR)/multicfl -s sc sck o2 comb									;\
+	$(TOOLDIR)/multicfl -s comb sc sck o2									;\
 	$(TOOLDIR)/nrmse -t 0. $(TESTS_OUT)/shepplogan_coil.ra sc						;\
 	$(TOOLDIR)/nrmse -t 0. $(TESTS_OUT)/shepplogan_coil_ksp.ra sck						;\
 	$(TOOLDIR)/nrmse -t 0. o.ra o2										;\
