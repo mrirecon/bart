54f9c24e921625bf24cec2bfdca6f642f2602b5e
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2022-03-01 13:16:58 +0100

    add test ideal for measure tool

---
+++ tests/measure.mk
@@ -0,0 +1,29 @@
+tests/test-measure-ideal-mse: measure $(TESTS_OUT)/shepplogan.ra
+	set -e															;\
+	if [ "0.000000e+00" != $$($(TOOLDIR)/measure --mse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan.ra) ]		;\
+	then															 \
+		false														;\
+	fi															;\
+	touch $@
+
+tests/test-measure-ideal-mse-mag: measure $(TESTS_OUT)/shepplogan.ra
+	set -e															;\
+	if [ "0.000000e+00" != $$($(TOOLDIR)/measure --mse $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan.ra) ]		;\
+	then															 \
+		false														;\
+	fi															;\
+	touch $@
+
+tests/test-measure-ideal-ssim: measure $(TESTS_OUT)/shepplogan.ra
+	set -e															;\
+	if [ "1.000000e+00" != $$($(TOOLDIR)/measure --ssim $(TESTS_OUT)/shepplogan.ra $(TESTS_OUT)/shepplogan.ra) ]		;\
+	then															 \
+		false														;\
+	fi															;\
+	touch $@
+	
+
+
+TESTS += tests/test-measure-ideal-mse
+TESTS += tests/test-measure-ideal-mse-mag
+TESTS += tests/test-measure-ideal-ssim
