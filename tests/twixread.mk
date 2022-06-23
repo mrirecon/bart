3efad151297333006e7cdadccada39d63810b1bd
Christian Holme <holme@tugraz.at>
2022-06-23 12:07:16 +0200

    tests/twixread: better datasets, better paths
    and controlled by an environment variable, so that they are no longer
    random paths on our systems

--- tests/twixread.mk
+++ tests/twixread.mk
@@ -1,35 +1,37 @@
+TWIX_SingleRaid=$(AGUE_TWIX_REF)/TWIX_SingleRaid.dat
+TWIX_MultiRaid=$(AGUE_TWIX_REF)/TWIX_MultiRaid.dat
+TWIX_MPI=$(AGUE_TWIX_REF)/TWIX_MPI.dat
+TWIX_VE=$(AGUE_TWIX_REF)/TWIX_VE.dat
 
-TWIXFILE_SINGLE=/home/ague/archive/pha/2019/2019-03-25_Other-MU_Export/single_raid/meas_MID00311_FID65562_t1_fl2d.dat
-TWIXFILE_MPI=/home/ague/data/T1825/T1825.dat
-TWIXFILE_MULTI=/home/ague/archive/pha/2019/2019-03-25_Other-MU_Export/meas_MID00311_FID65562_t1_fl2d.dat
-TWIXFILE_VE=/home/ague/data/SCBI_2022/2022-05-13_SCBI/MultiRaid/meas_MID00052_FID03520_localizer.dat
-
-tests/test-twixread: twixread ${TWIXFILE_SINGLE}
+tests/test-twixread: twixread ${TIWX_SingleRaid}
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/twixread -A ${TWIXFILE_SINGLE} ksp.ra				;\
-	echo "c304bf3bb41c7571408e776ec1280870 ksp.ra" | md5sum -c			;\
+	$(TOOLDIR)/twixread -A ${TWIX_SingleRaid} ksp.ra				;\
+	echo "c304bf3bb41c7571408e776ec1280870 *ksp.ra" | md5sum -c			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
-tests/test-twixread-multiraid: twixread ${TWIXFILE_MULTI}
+tests/test-twixread-multiraid: twixread ${TWIX_MultiRaid}
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/twixread -A ${TWIXFILE_MULTI} ksp.ra					;\
-	echo "c304bf3bb41c7571408e776ec1280870 ksp.ra" | md5sum -c			;\
+	$(TOOLDIR)/twixread -A ${TWIX_MultiRaid} ksp.ra					;\
+	echo "c304bf3bb41c7571408e776ec1280870 *ksp.ra" | md5sum -c			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
-tests/test-twixread-mpi: twixread ${TWIXFILE_MPI}
+tests/test-twixread-mpi: twixread ${TWIX_MPI}
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/twixread -M -x320 -r11 -s3 -n217 -c34 ${TWIXFILE_MPI} ksp.ra		;\
-	echo "9ee480b68ed70a7388851277e209b38d ksp.ra" | md5sum -c			;\
+	$(TOOLDIR)/twixread -M -x320 -r11 -s3 -n217 -c34 ${TWIX_MPI} ksp.ra		;\
+	echo "9ee480b68ed70a7388851277e209b38d *ksp.ra" | md5sum -c			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
-tests/test-twixread-ve: twixread ${TWIXFILE_VE}
+tests/test-twixread-ve: twixread ${TWIX_VE}
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/twixread -A ${TWIXFILE_VE} ksp.ra		;\
-	echo "d6553079149dd6d0689828546681a4da  ksp.ra" | md5sum -c			;\
+	$(TOOLDIR)/twixread -A ${TWIX_VE} ksp.ra					;\
+	echo "ca5478fa82f1fc1052358c376c41d5e2  ksp.ra" | md5sum -c 			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
+
+
 
 TESTS_AGUE += tests/test-twixread tests/test-twixread-multiraid tests/test-twixread-mpi tests/test-twixread-ve
+
