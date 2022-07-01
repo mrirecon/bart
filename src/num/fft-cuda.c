a714b0560294a6349d2c410ef4b92e6e0d9c0069
Moritz Blumenthal <moritz.blumenthal@med.uni-goettingen.de>
2022-07-01 10:15:25 +0200

    multigpu and streams for cudafft

--- src/num/fft-cuda.c
+++ src/num/fft-cuda.c
@@ -30,15 +30,12 @@
 #define CFL_SIZE sizeof(complex float)
 #endif
 
-#define CUFFT_MEMCACHE
-
 struct fft_cuda_plan_s {
 
-	cufftHandle cufft;
-#ifdef CUFFT_MEMCACHE
-	size_t workspace_size;
-	void* workspace;
-#endif
+	cufftHandle cufft[MAX_CUDA_DEVICES];
+	bool cufft_initialized[MAX_CUDA_DEVICES];
+	size_t workspace_size[MAX_CUDA_DEVICES];
+
 	struct fft_cuda_plan_s* chain;
 
 	bool backwards;
@@ -55,6 +52,14 @@ struct iovec {
 	long os; 
 };
 
+static char* cufft_error_string(enum cufftResult_t err);
+static void cufft_error(const char* file, int line, enum cufftResult_t code)
+{
+	const char* err_str = cufft_error_string(code);
+	error("cuFFT Error: %s in %s:%d \n", err_str, file, line);
+}
+
+#define CUFFT_ERROR(x)	({ CUDA_ASYNC_ERROR_NOTE("before cuFFT call"); enum cufftResult_t errval = (x); if (CUFFT_SUCCESS != errval) cufft_error(__FILE__, __LINE__, errval); CUDA_ASYNC_ERROR_NOTE("after cuFFT call");})
 
 
 // detect if flags has blocks of 1's seperated by 0's
@@ -99,10 +104,11 @@ static struct fft_cuda_plan_s* fft_cuda_plan0(int D, const long dimensions[D], u
 	plan->backwards = backwards;
 	plan->chain = NULL;
 
-#ifdef CUFFT_MEMCACHE
-	plan->workspace_size = 0;
-	plan->workspace = NULL;
-#endif
+	for (int i = 0; i < MAX_CUDA_DEVICES; i++) {
+
+		plan->cufft_initialized[i] = false;
+		plan->workspace_size[i] = 0;
+	}
 
 	struct iovec dims[N];
 	struct iovec hmdims[N];
@@ -210,39 +216,24 @@ static struct fft_cuda_plan_s* fft_cuda_plan0(int D, const long dimensions[D], u
 
 	assert(k <= 3);
 
-#ifdef CUFFT_MEMCACHE
-	int err1;
-	int err2;
-	int err3;
+	int old_device = cuda_get_device();
 
-	#pragma omp critical
-	err1 = cufftCreate(&plan->cufft);
-	#pragma omp critical
-	err2 = cufftSetAutoAllocation(plan->cufft, 0);
-	#pragma omp critical
-	err3 = cufftMakePlanMany(plan->cufft, k,
-				cudims, cuiemb, istride, idist,
-				cuoemb, ostride, odist, CUFFT_C2C, cubs, &plan->workspace_size);
+	for (int device = 0; device < cuda_num_devices(); ++device) {
 
-	if ((CUFFT_SUCCESS != err1) || (CUFFT_SUCCESS != err2) || (CUFFT_SUCCESS != err3)) {
+		cuda_set_device(device);
 
-		debug_printf(DP_WARN, "CUFFT Plan error: %d %d %d\n", err1, err2, err3);
-		goto errout;
+
+		CUFFT_ERROR(cufftCreate(&plan->cufft[device]));
+		CUFFT_ERROR(cufftSetAutoAllocation(plan->cufft[device], 0));
+		CUFFT_ERROR(cufftMakePlanMany(plan->cufft[device], k,
+					cudims, cuiemb, istride, idist,
+					cuoemb, ostride, odist, CUFFT_C2C, cubs, &(plan->workspace_size[device])));
+
+
+		plan->cufft_initialized[device] = true;
 	}
+	cuda_set_device(old_device);
 
-#else
-	int err;
-	#pragma omp critical
-	err = cufftPlanMany(&plan->cufft, k,
-				cudims, cuiemb, istride, idist,
-				        cuoemb, ostride, odist, CUFFT_C2C, cubs);
-
-	if (CUFFT_SUCCESS != err) {
-
-		debug_printf(DP_WARN, "CUFFT Plan error: %d\n", err);
-		goto errout;
-	}
-#endif
 
 	return PTR_PASS(plan);
 
@@ -300,13 +291,51 @@ void fft_cuda_free_plan(struct fft_cuda_plan_s* cuplan)
 	if (NULL != cuplan->chain)
 		fft_cuda_free_plan(cuplan->chain);
 
-	cufftDestroy(cuplan->cufft);
-#ifdef CUFFT_MEMCACHE
-	md_free(cuplan->workspace);
-#endif
+	for (int device = 0; device < cuda_num_devices(); ++device) {
+
+		if (cuplan->cufft_initialized[device]) {
+
+			CUFFT_ERROR(cufftDestroy(cuplan->cufft[device]));
+			cuplan->cufft_initialized[device] = false;
+		}
+	}
+
 	xfree(cuplan);
 }
 
+#ifdef _OPENMP
+#include <omp.h>
+static bool gpulock_init = false;
+static omp_lock_t gpulock[MAX_CUDA_DEVICES];
+static void cufft_set_gpulock(void)
+{
+	#pragma omp critical (init_cufft_gpulock)
+	if (!gpulock_init) {
+
+		for (int i = 0; i < MAX_CUDA_DEVICES; i++)
+			omp_init_lock(&gpulock[i]);
+
+		gpulock_init = true;		
+	}
+
+	omp_set_lock(&gpulock[cuda_get_device()]);
+}
+
+static void cufft_unset_gpulock(void)
+{
+	omp_unset_lock(&gpulock[cuda_get_device()]);
+}
+#else
+static void cufft_set_gpulock(void)
+{
+	return;
+}
+
+static void cufft_unset_gpulock(void)
+{
+	return;
+}
+#endif
 
 void fft_cuda_exec(struct fft_cuda_plan_s* cuplan, complex float* dst, const complex float* src)
 {
@@ -314,29 +343,56 @@ void fft_cuda_exec(struct fft_cuda_plan_s* cuplan, complex float* dst, const com
 	assert(cuda_ondevice(dst));
 	assert(NULL != cuplan);
 
-	int err;
+	
+	assert(cuplan->cufft_initialized[cuda_get_device()]);
+	size_t workspace_size = cuplan->workspace_size[cuda_get_device()];
+	cufftHandle cufft = cuplan->cufft[cuda_get_device()];
 
-	for (int i = 0; i < cuplan->batch; i++) {
+	void* workspace = md_alloc_gpu(1, MAKE_ARRAY(1l), workspace_size);
+	CUDA_ERROR_PTR(dst, src, workspace);
 
-		#ifdef CUFFT_MEMCACHE
-			cuplan->workspace = md_alloc_gpu(1, MAKE_ARRAY(1l), cuplan->workspace_size);
-			cufftSetWorkArea(cuplan->cufft, cuplan->workspace);
-		#endif
+	cufft_set_gpulock();
 
-		if (CUFFT_SUCCESS != (err = cufftExecC2C(cuplan->cufft,
-							(cufftComplex*)src + i * cuplan->idist,
-							(cufftComplex*)dst + i * cuplan->odist,
-							(!cuplan->backwards) ? CUFFT_FORWARD : CUFFT_INVERSE)))
-			error("CUFFT: %d\n", err);
+	CUFFT_ERROR(cufftSetStream(cufft, cuda_get_stream()));
+	CUFFT_ERROR(cufftSetWorkArea(cufft, workspace));
+	for (int i = 0; i < cuplan->batch; i++)
+		CUFFT_ERROR(cufftExecC2C(cufft,
+					 (cufftComplex*)src + i * cuplan->idist,
+					 (cufftComplex*)dst + i * cuplan->odist,
+					 (!cuplan->backwards) ? CUFFT_FORWARD : CUFFT_INVERSE));
 
-		#ifdef CUFFT_MEMCACHE
-			md_free(cuplan->workspace);
-			cuplan->workspace = NULL;
-		#endif
-	}
+	cufft_unset_gpulock();
+	
+	md_free(workspace);
 
 	if (NULL != cuplan->chain)
 		fft_cuda_exec(cuplan->chain, dst, dst);
 }
-#endif
+
+
+static char* cufft_error_string(enum cufftResult_t err)
+{
+	switch (err) {
+		case CUFFT_SUCCESS: 			return "CUFFT_SUCCESS"; break;
+		case CUFFT_INVALID_PLAN: 		return "CUFFT_INVALID_PLAN"; break;
+		case CUFFT_ALLOC_FAILED: 		return "CUFFT_ALLOC_FAILED"; break;
+		case CUFFT_INVALID_TYPE: 		return "CUFFT_INVALID_TYPE"; break;
+		case CUFFT_INVALID_VALUE: 		return "CUFFT_INVALID_VALUE"; break;
+		case CUFFT_INTERNAL_ERROR: 		return "CUFFT_INTERNAL_ERROR"; break;
+		case CUFFT_EXEC_FAILED: 		return "CUFFT_EXEC_FAILED"; break;
+		case CUFFT_SETUP_FAILED: 		return "CUFFT_SETUP_FAILED"; break;
+		case CUFFT_INVALID_SIZE: 		return "CUFFT_INVALID_SIZE"; break;
+		case CUFFT_UNALIGNED_DATA: 		return "CUFFT_UNALIGNED_DATA"; break;
+		case CUFFT_INCOMPLETE_PARAMETER_LIST: 	return "CUFFT_INCOMPLETE_PARAMETER_LIST"; break;
+		case CUFFT_INVALID_DEVICE: 		return "CUFFT_INVALID_DEVICE"; break;
+		case CUFFT_PARSE_ERROR: 		return "CUFFT_PARSE_ERROR"; break;
+		case CUFFT_NO_WORKSPACE: 		return "CUFFT_NO_WORKSPACE"; break;
+		case CUFFT_NOT_IMPLEMENTED: 		return "CUFFT_NOT_IMPLEMENTED"; break;
+		case CUFFT_LICENSE_ERROR: 		return "CUFFT_LICENSE_ERROR"; break;
+		case CUFFT_NOT_SUPPORTED: 		return "CUFFT_NOT_SUPPORTED"; break;
+		default: return "Not a valid error string!\n"; break;
+	}
+}
+
+#endif // USE_CUDA
 
