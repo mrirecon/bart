f6df6e78c31c03c61c6766198ca15a5f9bbf8a55
Moritz Blumenthal <blumenthal@tugraz.at>
2022-12-04 00:35:14 +0100

    don't perform clear in resize if not necessary

--- src/num/multind.c
+++ src/num/multind.c
@@ -1129,7 +1129,13 @@ void md_resize(int D, const long odim[D], void* optr, const long idim[D], const 
 	long pos[D];
 	memset(pos, 0, D * sizeof(long));
 
-	md_clear(D, odim, optr, size);
+	for (int i = 0; i < D; i++)
+		if (odim[i] > idim[i]) {
+
+			md_clear(D, odim, optr, size);
+			break;
+		}
+
 	md_copy_block(D, pos, odim, optr, idim, iptr, size);
 }
 
@@ -1163,7 +1169,13 @@ void md_resize_center(int D, const long odim[D], void* optr, const long idim[D],
 	for (int i = 0; i < D; i++)
 		pos[i] = labs((odim[i] / 2) - (idim[i] / 2));
 
-	md_clear(D, odim, optr, size);
+	for (int i = 0; i < D; i++)
+		if (odim[i] > idim[i]) {
+
+			md_clear(D, odim, optr, size);
+			break;
+		}
+
 	md_copy_block(D, pos, odim, optr, idim, iptr, size);
 }
 
