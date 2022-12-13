62c5ee2b4bb0a0483520ceaa8a6e1c567220960f
Moritz Blumenthal <blumenthal@tugraz.at>
2022-12-13 10:50:15 +0100

    add function for preconditioning

--- src/grecon/optreg.h
+++ src/grecon/optreg.h
@@ -43,6 +43,7 @@ struct opt_reg_s {
 extern _Bool opt_reg_init(struct opt_reg_s* ropts);
 
 extern void opt_bpursuit_configure(struct opt_reg_s* ropts, const struct operator_p_s* prox_ops[NUM_REGS], const struct linop_s* trafos[NUM_REGS], const struct linop_s* model_op, const _Complex float* data, const float eps);
+extern void opt_precond_configure(struct opt_reg_s* ropts, const struct operator_p_s* prox_ops[NUM_REGS], const struct linop_s* trafos[NUM_REGS], const struct linop_s* model_op, int N, const long ksp_dims[N], const _Complex float* data, const long pat_dims[N], const _Complex float* pattern);
 
 extern void opt_reg_configure(int N, const long img_dims[__VLA(N)], struct opt_reg_s* ropts, const struct operator_p_s* prox_ops[NUM_REGS], const struct linop_s* trafos[NUM_REGS], unsigned int llr_blk, unsigned int shift_mode, const char* wtype_str, _Bool use_gpu);
 
