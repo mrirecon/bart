81b07593b20fde77c328fed18e76bdf5276f1d4b
Moritz Blumenthal <blumenthal@tugraz.at>
2022-12-13 10:47:27 +0100

    prox for weighted least squares + gpu support

--- src/iter/prox.h
+++ src/iter/prox.h
@@ -6,6 +6,7 @@
 
 struct operator_p_s;
 extern const struct operator_p_s* prox_leastsquares_create(int N, const long dims[__VLA(N)], float lambda, const _Complex float* y);
+extern const struct operator_p_s* prox_weighted_leastsquares_create(int N, const long dims[__VLA(N)], float lambda, const _Complex float* y, unsigned long flags, const _Complex float* W);
 extern const struct operator_p_s* prox_l2norm_create(int N, const long dims[__VLA(N)], float lambda);
 extern const struct operator_p_s* prox_l2ball_create(int N, const long dims[__VLA(N)], float eps, const _Complex float* center);
 extern const struct operator_p_s* prox_zero_create(int N, const long dims[__VLA(N)]);
