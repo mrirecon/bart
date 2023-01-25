0238230d50cfeb1fbd270b6a567c4d9a468d3e32
Moritz Blumenthal <blumenthal@tugraz.at>
2023-01-25 17:33:35 +0100

    add real rand function

--- src/num/rand.h
+++ src/num/rand.h
@@ -7,6 +7,8 @@ extern void md_gaussian_rand(unsigned int D, const long dims[__VLA(D)], _Complex
 extern void md_uniform_rand(unsigned int D, const long dims[__VLA(D)], _Complex float* dst);
 extern void md_rand_one(unsigned int D, const long dims[__VLA(D)], _Complex float* dst, double p);
 
+extern void gaussian_rand_vec(long N, float* dst);
+
 extern void num_rand_init(unsigned int seed);
 
 #include "misc/cppwrap.h"
