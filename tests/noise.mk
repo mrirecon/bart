e1e0b5253fff104946421ff344653c30402fb01d
Christian Holme <holme@tugraz.at>
2023-03-20 16:28:46 +0100

    tests/noise: add test for spike noise

--- tests/noise.mk
+++ tests/noise.mk
@@ -23,7 +23,20 @@ tests/test-noise-real: zeros noise std ones nrmse
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+# In order to use a small tolerance, a rather large array of 100k values is needed.
+# Test idea is simply: add spike noise to an array of zeros, multiply with its own
+# inverse and count the number of ones. Then check that it is close to the expected number.
+tests/test-noise-spike: zeros noise invert fmac vec nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+	$(TOOLDIR)/zeros 2 100 1000 z.ra						;\
+	$(TOOLDIR)/noise -s1 -n1. -S 0.67 z.ra n.ra				;\
+	$(TOOLDIR)/invert n.ra o.ra							;\
+	$(TOOLDIR)/fmac -s3 o.ra n.ra nz.ra						;\
+	$(TOOLDIR)/vec 67000 r.ra							;\
+	$(TOOLDIR)/nrmse -t 0.006 r.ra nz.ra						;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
 
 
-TESTS += tests/test-noise tests/test-noise-real
+TESTS += tests/test-noise tests/test-noise-real tests/test-noise-spike
 
