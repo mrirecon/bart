3ef3cd89e0621c1080e5ac6f091fa6b0b6051274
Martin Uecker <uecker@tugraz.at>
2023-07-07 23:36:35 +0200

    make test conform to the rest

--- tests/nlmeans.mk
+++ tests/nlmeans.mk
@@ -1,12 +1,12 @@
+
 tests/test-nlmeans: phantom noise nlmeans nrmse
-	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)							;\
-	$(TOOLDIR)/phantom phantom 												;\
-	$(TOOLDIR)/noise -r -n 0.005 phantom noisy_phantom 						;\
-	$(TOOLDIR)/nlmeans -H.22 3 noisy_phantom smooth_phantom 				;\
-	$(TOOLDIR)/nrmse -t 0.09 phantom smooth_phantom 						;\
-	rm phantom.{hdr,cfl} noisy_phantom.{hdr,cfl} smooth_phantom.{hdr,cfl}	;\
-	rmdir $(TESTS_TMP)
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
+	$(TOOLDIR)/phantom phantom.ra 						;\
+	$(TOOLDIR)/noise -r -n 0.005 phantom.ra noisy_phantom.ra 		;\
+	$(TOOLDIR)/nlmeans -H.22 3 noisy_phantom.ra smooth_phantom.ra 		;\
+	$(TOOLDIR)/nrmse -t 0.09 phantom.ra smooth_phantom.ra 			;\
+	rm *.ra	; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
-
 TESTS += tests/test-nlmeans
+
