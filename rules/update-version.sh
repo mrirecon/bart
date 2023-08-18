209c6f888f52e43b2b2608eab3acb8d9d763daff
Martin Uecker <uecker@tugraz.at>
2023-08-18 21:11:57 +0200

    make update of version more robust

--- rules/update-version.sh
+++ rules/update-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'VERSION('`./git-version.sh`')' > version.new
-./rules/update-if-changed.sh version.new src/misc/version.inc
-rm -f version.new
+echo 'VERSION('`./git-version.sh`')' > version.new.$$
+./rules/update-if-changed.sh version.new.$$ src/misc/version.inc
+rm -f version.new.$$
 
