3ae7e8123828b409711639643a803a0c79d1c1a8
Blumenthal, Moritz <blumenthal@tugraz.at>
2023-09-14 15:19:07 +0200

    update multiplace for vptr

--- src/num/multiplace.c
+++ src/num/multiplace.c
@@ -11,6 +11,8 @@
 #include "num/gpuops.h"
 #endif
 
+#include "num/vptr.h"
+
 #include "num/multiplace.h"
 
 
@@ -23,9 +25,11 @@ struct multiplace_array_s {
 	void* ptr_ref;
 
 	void* ptr_cpu;
+	void* mpi_cpu;
 
 #ifdef USE_CUDA
 	void* ptr_gpu[MAX_CUDA_DEVICES];
+	void* mpi_gpu[MAX_CUDA_DEVICES];
 #endif
 	bool free;
 };
@@ -45,11 +49,15 @@ static struct multiplace_array_s* multiplace_alloc(int D, const long dimensions[
 	result->dims = *PTR_PASS(dims);
 
 	result->ptr_cpu = NULL;
+	result->mpi_cpu = NULL;
 	result->free = true;
 
 #ifdef USE_CUDA
-	for (int i = 0; i < MAX_CUDA_DEVICES; i++)
+	for (int i = 0; i < MAX_CUDA_DEVICES; i++) {
+
 		result->ptr_gpu[i] = NULL;
+		result->mpi_gpu[i] = NULL;
+	}
 #endif
 
 	return PTR_PASS(result);
@@ -57,16 +65,39 @@ static struct multiplace_array_s* multiplace_alloc(int D, const long dimensions[
 
 
 
-void multiplace_free(const struct multiplace_array_s* ptr)
+void multiplace_free(const struct multiplace_array_s* _ptr)
 {
-	if (NULL == ptr)
+	if (NULL == _ptr)
 		return;
 
-	md_free(ptr->ptr_cpu);
+	struct multiplace_array_s* ptr = (struct multiplace_array_s*)_ptr;
+
+	if (ptr->ptr_ref == ptr->ptr_cpu)
+		ptr->ptr_cpu = NULL;
+	
+	if (ptr->ptr_ref == ptr->mpi_cpu)
+		ptr->mpi_cpu = NULL;
 
 #ifdef USE_CUDA
-	for (int i = 0; i < MAX_CUDA_DEVICES; i++)
+	for (int i = 0; i < MAX_CUDA_DEVICES; i++){
+
+		if (ptr->ptr_ref == ptr->ptr_gpu[i])
+			ptr->ptr_gpu[i] = NULL;
+	
+		if (ptr->ptr_ref == ptr->mpi_gpu[i])
+			ptr->mpi_gpu[i] = NULL;
+	}
+#endif
+
+	md_free(ptr->ptr_cpu);
+	md_free(ptr->mpi_cpu);
+
+#ifdef USE_CUDA
+	for (int i = 0; i < MAX_CUDA_DEVICES; i++){
+
 		md_free(ptr->ptr_gpu[i]);
+		md_free(ptr->mpi_gpu[i]);
+	}
 #endif
 
 	if (ptr->free)
@@ -81,38 +112,57 @@ const void* multiplace_read(struct multiplace_array_s* ptr, const void* ref)
 {
 	if (NULL == ptr)
 		return NULL;
-	
-	//CPU reference by default
-	if (NULL == ref)
-		ref = &ref;
-	
-	if (md_is_sameplace(ref, ptr->ptr_ref))
-		return ptr->ptr_ref;
 
-#ifdef USE_CUDA
-	if (cuda_ondevice(ref)) {
+	if (is_vptr(ref)) {
 
-		#pragma omp critical (bart_multiplace)
-		if (NULL == ptr->ptr_gpu[cuda_get_device()]) {
+		if (is_vptr_cpu(ref)) {
 
-			ptr->ptr_gpu[cuda_get_device()] = md_gpu_move(ptr->N, ptr->dims, ptr->ptr_ref, ptr->size);
-			cuda_sync_device();
+			if (NULL == ptr->mpi_cpu) {
+
+				ptr->mpi_cpu = vptr_alloc_sameplace(ptr->N, ptr->dims, ptr->size, ref);
+				md_copy(ptr->N, ptr->dims, ptr->mpi_cpu, ptr->ptr_ref, ptr->size);
+			}
+
+			return ptr->mpi_cpu;
 		}
 
-		return ptr->ptr_gpu[cuda_get_device()];
-	}
-#else
-	UNUSED(ref);
+#ifdef USE_CUDA
+		if (is_vptr_gpu(ref)) {
+
+			if (NULL == ptr->mpi_gpu[cuda_get_device()]) {
+
+				ptr->mpi_gpu[cuda_get_device()] = vptr_alloc_sameplace(ptr->N, ptr->dims, ptr->size, ref);
+				md_copy(ptr->N, ptr->dims, ptr->mpi_gpu[cuda_get_device()], ptr->ptr_ref, ptr->size);
+			}
+
+			return ptr->mpi_gpu[cuda_get_device()];
+		}
 #endif
-	#pragma omp critical (bart_multiplace)
-	if (NULL == ptr->ptr_cpu) {
+	} else {
 
-		ptr->ptr_cpu = md_alloc(ptr->N, ptr->dims, ptr->size);
+#ifdef USE_CUDA
+		if (cuda_ondevice(ref)) {
+			
+			if (NULL == ptr->ptr_gpu[cuda_get_device()]) {
 
-		md_copy(ptr->N, ptr->dims, ptr->ptr_cpu, ptr->ptr_ref, ptr->size);
+				ptr->ptr_gpu[cuda_get_device()] = md_alloc_gpu(ptr->N, ptr->dims, ptr->size);
+				md_copy(ptr->N, ptr->dims, ptr->ptr_gpu[cuda_get_device()], ptr->ptr_ref, ptr->size);
+			}
+
+			return ptr->ptr_gpu[cuda_get_device()];
+		}
+#endif
+	
+		if (NULL == ptr->ptr_cpu) {
+
+			ptr->ptr_cpu = md_alloc(ptr->N, ptr->dims, ptr->size);
+			md_copy(ptr->N, ptr->dims, ptr->mpi_cpu, ptr->ptr_ref, ptr->size);
+		}
+
+		return ptr->ptr_cpu;
 	}
 
-	return ptr->ptr_cpu;
+	return NULL;
 }
 
 
@@ -125,6 +175,25 @@ struct multiplace_array_s* multiplace_move2(int D, const long dimensions[D], con
 	md_copy2(D, dimensions, MD_STRIDES(D, dimensions, size), tmp, strides, ptr, size);
 
 	result->ptr_ref = tmp;
+
+	if (is_vptr(tmp)) {
+
+		if (is_vptr_cpu(tmp))
+			result->mpi_cpu = tmp;
+
+#ifdef USE_CUDA
+		if (is_vptr_gpu(tmp))
+			result->mpi_gpu[cuda_get_device()] = tmp;
+#endif
+	} else {
+
+#ifdef USE_CUDA
+		if (cuda_ondevice(tmp))
+			result->ptr_gpu[cuda_get_device()] = tmp;
+		else
+#endif
+			result->ptr_cpu = tmp;
+	}
 
 	return result;
 }
@@ -139,13 +208,31 @@ struct multiplace_array_s* multiplace_move_F(int D, const long dimensions[D], si
 {
 	auto result = multiplace_alloc(D, dimensions, size);
 	result->ptr_ref = (void*)ptr;
+
+	if (is_vptr(ptr)) {
+
+		if (is_vptr_cpu(ptr))
+			result->mpi_cpu = (void*)ptr;
+
+#ifdef USE_CUDA
+		if (is_vptr_gpu(ptr))
+			result->mpi_gpu[cuda_get_device()] = (void*)ptr;
+#endif
+	} else {
+
+#ifdef USE_CUDA
+		if (cuda_ondevice(ptr))
+			result->ptr_gpu[cuda_get_device()] = (void*)ptr;
+		else
+#endif
+			result->ptr_cpu = (void*)ptr;
+	}
 	return result;
 }
 
 struct multiplace_array_s* multiplace_move_wrapper(int D, const long dimensions[D], size_t size, const void* ptr)
 {
-	auto result = multiplace_alloc(D, dimensions, size);
-	result->ptr_ref = (void*)ptr;
+	auto result = multiplace_move_F(D, dimensions, size, ptr);
 	result->free = false;
 	return result;
 }
