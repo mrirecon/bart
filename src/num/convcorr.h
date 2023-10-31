240b8e7a301dc360d905983a30285fd6b3bb9281
Martin Uecker <uecker@tugraz.at>
2023-10-31 21:10:41 +0100

    incorrect static

--- src/num/convcorr.h
+++ src/num/convcorr.h
@@ -7,17 +7,17 @@ typedef _Bool zconvcorr_fwd_algo_f(	int N,
 					long odims[__VLA(N)], long ostrs[__VLA(N)], _Complex float* out,
 					long idims[__VLA(N)], long istrs[__VLA(N)], const _Complex float* in,
 					long kdims[__VLA(N)], long kstrs[__VLA(N)], const _Complex float* krn,
-					unsigned long flags, const long dilation[__VLA(N)], const long strides[__VLA(N)], _Bool conv);
+					unsigned long flags, const long dilation[__VLA2(N)], const long strides[__VLA2(N)], _Bool conv);
 typedef _Bool zconvcorr_bwd_in_algo_f(	int N,
 					long odims[__VLA(N)], long ostrs[__VLA(N)], const _Complex float* out,
 					long idims[__VLA(N)], long istrs[__VLA(N)], _Complex float* in,
 					long kdims[__VLA(N)], long kstrs[__VLA(N)], const _Complex float* krn,
-					unsigned long flags, const long dilation[__VLA(N)], const long strides[__VLA(N)], _Bool conv);
+					unsigned long flags, const long dilation[__VLA2(N)], const long strides[__VLA2(N)], _Bool conv);
 typedef _Bool zconvcorr_bwd_krn_algo_f(	int N,
 					long odims[__VLA(N)], long ostrs[__VLA(N)], const _Complex float* out,
 					long idims[__VLA(N)], long istrs[__VLA(N)], const _Complex float* in,
 					long kdims[__VLA(N)], long kstrs[__VLA(N)], _Complex float* krn,
-					unsigned long flags, const long dilation[__VLA(N)], const long strides[__VLA(N)], _Bool conv);
+					unsigned long flags, const long dilation[__VLA2(N)], const long strides[__VLA2(N)], _Bool conv);
 
 zconvcorr_fwd_algo_f zconvcorr_fwd_im2col_cf_cpu;
 zconvcorr_bwd_in_algo_f zconvcorr_bwd_in_im2col_cf_cpu;
@@ -37,11 +37,11 @@ _Bool test_zconvcorr_bwd_in(	int N,
 				long odims[__VLA(N)], long ostrs[__VLA(N)],
 				long idims[__VLA(N)], long istrs[__VLA(N)],
 				long kdims[__VLA(N)], long kstrs[__VLA(N)],
-				unsigned long flags, const long dilation[__VLA(N)], const long strides[__VLA(N)], _Bool conv,
+				unsigned long flags, const long dilation[__VLA2(N)], const long strides[__VLA2(N)], _Bool conv,
 				float max_rmse, _Bool gpu, long min_no_algos);
 _Bool test_zconvcorr_bwd_krn(	int N,
 				long odims[__VLA(N)], long ostrs[__VLA(N)],
 				long idims[__VLA(N)], long istrs[__VLA(N)],
 				long kdims[__VLA(N)], long kstrs[__VLA(N)],
-				unsigned long flags, const long dilation[__VLA(N)], const long strides[__VLA(N)], _Bool conv,
+				unsigned long flags, const long dilation[__VLA2(N)], const long strides[__VLA2(N)], _Bool conv,
 				float max_rmse, _Bool gpu, long min_no_algos);
