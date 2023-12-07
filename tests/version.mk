530988e8e2a4a0c088570290bf0047f9d70f0007
Martin Uecker <uecker@tugraz.at>
2023-12-07 23:26:22 +0100

    fix path in test

--- tests/version.mk
+++ tests/version.mk
@@ -2,7 +2,7 @@
 
 # check if bart can parse its own version
 tests/test-version: version
-	$(TOOLDIR)/version -t `cat $(TOOLDIR)/../version.txt`
+	$(TOOLDIR)/version -t `cat $(ROOTDIR)/version.txt`
 	touch $@
 
 TESTS += tests/test-version
