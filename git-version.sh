429cda181d7a4c5743dde299b34b3364436a1b87
Christian Holme <holme@tugraz.at>
2024-02-27 12:02:04 +0100

    git-version.sh: add support for shallow clones
    In shallow clones, `git describe` cannot help us (as we have no tags to
    describe the version). Therefore, we fall back to `git diff` (with
    `--quiet`) to check if the working directory is clean.
    
    github actions use a shallow clone by default, so `git-version.sh` used
    to incorreclty report `-dirty` there.

--- git-version.sh
+++ git-version.sh
@@ -2,12 +2,18 @@
 
 if test -d ${GIT_DIR:-.git} -o -f .git
 then
-    git describe --abbrev=7 --match "v*" --dirty
+    GDOUT=`git describe --abbrev=7 --match "v*" --dirty 2>&1`
     if [[ $? -eq 0 ]]; then
+	echo ${GDOUT}
 	git describe --abbrev=7 --match "v*" | cut -f1 -d'-' > version.txt
     else
-	var=`cat version.txt`
-	echo ${var}-dirty
+	if git diff --quiet --exit-code
+	then
+	    cat version.txt
+	else
+	    var=`cat version.txt`
+	    echo ${var}-dirty
+	fi
     fi
 else
     cat version.txt
