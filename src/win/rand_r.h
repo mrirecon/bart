554e2163d94aa4a5511d391c954f299e196d6246
Christian Holme <holme@tugraz.at>
2024-03-05 15:19:01 +0100

    wind/rand_r: remove UNUSED and self-include
    as we removed it quite a few commits ago

--- src/win/rand_r.h
+++ src/win/rand_r.h
@@ -10,14 +10,12 @@
 #define RAND_R_WINDOWS
 
 #include <stdlib.h>
-#include "win/rand_r.h"
 #include "misc/misc.h"
 
 int rand_r(unsigned int *seed);
 
-int rand_r(unsigned int* seed)
+int rand_r(unsigned int* /*seed*/)
 {
-	UNUSED(seed);
 	// Note: There is no rand_r implementation on Windows, but rand() is thread safe
 	// TODO: Implement a thread safe rand_r that produces the same random sequence as the UNIX version
 	// UNIX implementation: https://sourceware.org/git/?p=glibc.git;a=blob;f=stdlib/rand_r.c;h=30fafc0fa5f30592470a1796bc129f2b1613ecec;hb=refs/heads/master
