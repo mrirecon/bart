008f8e7f463cc640cd8cf00ad0c16973d110b268
Martin Uecker <uecker@tugraz.at>
2024-03-25 16:54:16 +0100

    indentation

--- src/grecon/italgo.c
+++ src/grecon/italgo.c
@@ -76,158 +76,153 @@ struct iter italgo_config(enum algo_t algo, int nr_penalties, const struct reg_s
 
 	switch (algo) {
 
-		case ALGO_DEFAULT:
+	case ALGO_DEFAULT:
 
-			assert(0);
+		assert(0);
 
-		case ALGO_CG: {
+	case ALGO_CG:
 
-			debug_printf(DP_INFO, "conjugate gradients\n");
+		debug_printf(DP_INFO, "conjugate gradients\n");
 
-			assert((0 == nr_penalties) || ((1 == nr_penalties) && (L2IMG == regs[0].xform)));
+		assert((0 == nr_penalties) || ((1 == nr_penalties) && (L2IMG == regs[0].xform)));
 
-			PTR_ALLOC(struct iter_conjgrad_conf, cgconf);
-			*cgconf = iter_conjgrad_defaults;
-			cgconf->maxiter = maxiter;
-			cgconf->l2lambda = (0 == nr_penalties) ? 0. : regs[0].lambda;
+		PTR_ALLOC(struct iter_conjgrad_conf, cgconf);
+		*cgconf = iter_conjgrad_defaults;
+		cgconf->maxiter = maxiter;
+		cgconf->l2lambda = (0 == nr_penalties) ? 0. : regs[0].lambda;
 
-			PTR_ALLOC(struct iter_call_s, iter2_data);
-			SET_TYPEID(iter_call_s, iter2_data);
+		PTR_ALLOC(struct iter_call_s, iter2_data);
+		SET_TYPEID(iter_call_s, iter2_data);
 
-			iter2_data->fun = iter_conjgrad;
-			iter2_data->_conf = CAST_UP(PTR_PASS(cgconf));
+		iter2_data->fun = iter_conjgrad;
+		iter2_data->_conf = CAST_UP(PTR_PASS(cgconf));
 
-			italgo = iter2_call_iter;
-			iconf = CAST_UP(PTR_PASS(iter2_data));
+		italgo = iter2_call_iter;
+		iconf = CAST_UP(PTR_PASS(iter2_data));
 
-			break;
+		break;
+
+	case ALGO_IST:
+
+		debug_printf(DP_INFO, "IST\n");
+
+		assert(1 == nr_penalties);
+
+		PTR_ALLOC(struct iter_ist_conf, isconf);
+		*isconf = iter_ist_defaults;
+		isconf->maxiter = maxiter;
+		isconf->step = step;
+		isconf->hogwild = hogwild;
+
+		PTR_ALLOC(struct iter_call_s, iter2_ist_data);
+		SET_TYPEID(iter_call_s, iter2_ist_data);
+
+		iter2_ist_data->fun = iter_ist;
+		iter2_ist_data->_conf = CAST_UP(PTR_PASS(isconf));
+
+		italgo = iter2_call_iter;
+		iconf = CAST_UP(PTR_PASS(iter2_ist_data));
+
+		break;
+
+	case ALGO_ADMM:
+
+		debug_printf(DP_INFO, "ADMM\n");
+
+		PTR_ALLOC(struct iter_admm_conf, mmconf);
+		*mmconf = iter_admm_defaults;
+		mmconf->maxiter = maxiter;
+		mmconf->maxitercg = admm.maxitercg;
+		mmconf->rho = admm.rho;
+		mmconf->hogwild = hogwild;
+		mmconf->fast = admm.fast;
+		mmconf->dynamic_rho = admm.dynamic_rho;
+		mmconf->dynamic_tau = admm.dynamic_tau;
+		mmconf->relative_norm = admm.relative_norm;
+		mmconf->ABSTOL = 0.;
+		mmconf->RELTOL = 0.;
+		mmconf->do_warmstart = warm_start;
+
+		italgo = iter2_admm;
+		iconf = CAST_UP(PTR_PASS(mmconf));
+
+		break;
+
+	case ALGO_PRIDU:
+
+		debug_printf(DP_INFO, "Primal Dual\n");
+
+		//assert(2 == nr_penalties);
+
+		PTR_ALLOC(struct iter_chambolle_pock_conf, pdconf);
+		*pdconf = iter_chambolle_pock_defaults;
+
+		pdconf->maxiter = maxiter;
+		pdconf->sigma = sqrtf(step);
+		pdconf->tau = sqrtf(step);
+
+		pdconf->sigma *= scaling;
+		pdconf->tau /= scaling;
+
+		pdconf->theta = 1.;
+		pdconf->decay = (hogwild ? .95 : 1.);
+		pdconf->tol = 1.E-4;
+
+		italgo = iter2_chambolle_pock;
+		iconf = CAST_UP(PTR_PASS(pdconf));
+
+		break;
+
+	case ALGO_FISTA:
+
+		debug_printf(DP_INFO, "FISTA\n");
+
+		assert(1 == nr_penalties);
+
+		PTR_ALLOC(struct iter_fista_conf, fsconf);
+		*fsconf = iter_fista_defaults;
+		fsconf->maxiter = maxiter;
+		fsconf->step = step;
+		fsconf->hogwild = hogwild;
+
+		if (-1. != fista_params[0]) {
+
+			fsconf->p = fista_params[0];
+			fsconf->q = fista_params[1];
+			fsconf->r = fista_params[2];
+
+			debug_printf(DP_INFO, "FISTA: p=%f q=%f r=%f\n",
+					fsconf->p, fsconf->q, fsconf->r);
 		}
 
-		case ALGO_IST: {
+		PTR_ALLOC(struct iter_call_s, iter2_fista_data);
+		SET_TYPEID(iter_call_s, iter2_fista_data);
 
-			debug_printf(DP_INFO, "IST\n");
+		iter2_fista_data->fun = iter_fista;
+		iter2_fista_data->_conf = CAST_UP(PTR_PASS(fsconf));
 
-			assert(1 == nr_penalties);
+		italgo = iter2_call_iter;
+		iconf = CAST_UP(PTR_PASS(iter2_fista_data));
 
-			PTR_ALLOC(struct iter_ist_conf, isconf);
-			*isconf = iter_ist_defaults;
-			isconf->maxiter = maxiter;
-			isconf->step = step;
-			isconf->hogwild = hogwild;
+		break;
 
-			PTR_ALLOC(struct iter_call_s, iter2_ist_data);
-			SET_TYPEID(iter_call_s, iter2_ist_data);
+	case ALGO_NIHT:
 
-			iter2_ist_data->fun = iter_ist;
-			iter2_ist_data->_conf = CAST_UP(PTR_PASS(isconf));
+		debug_printf(DP_INFO, "NIHT\n");
 
-			italgo = iter2_call_iter;
-			iconf = CAST_UP(PTR_PASS(iter2_ist_data));
+		PTR_ALLOC(struct iter_niht_conf, ihconf);
 
-			break;
-		}
+		*ihconf = iter_niht_defaults;
+		ihconf->maxiter = maxiter;
+		ihconf->do_warmstart = warm_start;
 
-		case ALGO_ADMM: {
+		italgo = iter2_niht;
+		iconf = CAST_UP(PTR_PASS(ihconf));
 
-			debug_printf(DP_INFO, "ADMM\n");
+		break;
 
-			PTR_ALLOC(struct iter_admm_conf, mmconf);
-			*mmconf = iter_admm_defaults;
-			mmconf->maxiter = maxiter;
-			mmconf->maxitercg = admm.maxitercg;
-			mmconf->rho = admm.rho;
-			mmconf->hogwild = hogwild;
-			mmconf->fast = admm.fast;
-			mmconf->dynamic_rho = admm.dynamic_rho;
-			mmconf->dynamic_tau = admm.dynamic_tau;
-			mmconf->relative_norm = admm.relative_norm;
-			mmconf->ABSTOL = 0.;
-			mmconf->RELTOL = 0.;
-			mmconf->do_warmstart = warm_start;
-			italgo = iter2_admm;
-			iconf = CAST_UP(PTR_PASS(mmconf));
-
-			break;
-		}
-
-		case ALGO_PRIDU: {
-
-			debug_printf(DP_INFO, "Primal Dual\n");
-
-			//assert(2 == nr_penalties);
-
-			PTR_ALLOC(struct iter_chambolle_pock_conf, pdconf);
-			*pdconf = iter_chambolle_pock_defaults;
-
-			pdconf->maxiter = maxiter;
-			pdconf->sigma = sqrtf(step);
-			pdconf->tau = sqrtf(step);
-
-			pdconf->sigma *= scaling;
-			pdconf->tau /= scaling;
-
-			pdconf->theta = 1.;
-			pdconf->decay = (hogwild ? .95 : 1.);
-			pdconf->tol = 1.E-4;
-
-			italgo = iter2_chambolle_pock;
-			iconf = CAST_UP(PTR_PASS(pdconf));
-
-			break;
-		}
-
-		case ALGO_FISTA: {
-
-			debug_printf(DP_INFO, "FISTA\n");
-
-			assert(1 == nr_penalties);
-
-			PTR_ALLOC(struct iter_fista_conf, fsconf);
-			*fsconf = iter_fista_defaults;
-			fsconf->maxiter = maxiter;
-			fsconf->step = step;
-			fsconf->hogwild = hogwild;
-
-			if (-1. != fista_params[0]) {
-
-				fsconf->p = fista_params[0];
-				fsconf->q = fista_params[1];
-				fsconf->r = fista_params[2];
-
-				debug_printf(DP_INFO, "FISTA: p=%f q=%f r=%f\n",
-						fsconf->p, fsconf->q, fsconf->r);
-			}
-
-			PTR_ALLOC(struct iter_call_s, iter2_fista_data);
-			SET_TYPEID(iter_call_s, iter2_fista_data);
-
-			iter2_fista_data->fun = iter_fista;
-			iter2_fista_data->_conf = CAST_UP(PTR_PASS(fsconf));
-
-			italgo = iter2_call_iter;
-			iconf = CAST_UP(PTR_PASS(iter2_fista_data));
-
-			break;
-		}
-
-		case ALGO_NIHT: {
-
-			debug_printf(DP_INFO, "NIHT\n");
-
-			PTR_ALLOC(struct iter_niht_conf, ihconf);
-
-			*ihconf = iter_niht_defaults;
-			ihconf->maxiter = maxiter;
-			ihconf->do_warmstart = warm_start;
-
-			italgo = iter2_niht;
-			iconf = CAST_UP(PTR_PASS(ihconf));
-
-			break;
-		}
-
-		default:
-			assert(0);
+	default:
+		assert(0);
 	}
 
 	return (struct iter){ italgo, iconf };
