cc3069ab50e31f63866f106e3d4593aeb09197b0
Christian Holme <holme@tugraz.at>
2024-04-05 12:36:18 +0200

    tests: add test for BART_COMPAT_VERSION

--- tests/version.mk
+++ tests/version.mk
@@ -5,4 +5,16 @@ tests/test-version: version
 	$(TOOLDIR)/version -t `cat $(ROOTDIR)/version.txt`
 	touch $@
 
-TESTS += tests/test-version
+# check if BART_DEBUG_LEVEL is usable together with BART_COMPAT_VERSION
+tests/test-version-estdelay-ring: estdelay traj phantom nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)									;\
+	$(TOOLDIR)/traj -D -r -q0.3:0.1:0.2 -c -y5 t.ra									;\
+	$(TOOLDIR)/traj -D -c -r -y5 n.ra										;\
+	$(TOOLDIR)/phantom -s8 -k -t t.ra k.ra										;\
+	$(TOOLDIR)/traj -D -r -q`BART_COMPAT_VERSION=v0.4.00 DEBUG_LEVEL=0 $(TOOLDIR)/estdelay -R n.ra k.ra` -y5 -c t2.ra	;\
+	$(TOOLDIR)/nrmse -t 0.0035 t.ra t2.ra										;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+TESTS += tests/test-version tests/test-version-estdelay-ring
