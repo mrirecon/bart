bd38c9aeea896377fcb1fa50e7c2d9324a3dc76b
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-06-07 17:53:38 +0200

    compress symmetric/hermitian matrices

---
+++ src/num/gpukrnls_triagmat.h
@@ -0,0 +1,10 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void cuda_zrfmac_upper_triagmat(long N, long NC, long NM, long ostr, long istr, long mstr, float* dst, const float* src, const float* mat);
+
+#ifdef __cplusplus
+}
+#endif
