17938228330071984d16b83c01354314937c88a2
Nick Scholand <scholand@tugraz.at>
2024-10-08 12:09:18 +0200

    sim.c: style

--- src/sim.c
+++ src/sim.c
@@ -2,7 +2,7 @@
  * All rights reserved. Use of this source code is governed by
  * a BSD-style license which can be found in the LICENSE file.
  *
- * Author: Nick Scholand
+ * Author: Nick Scholand, Martin Juschitz
  */
 
 
@@ -119,12 +119,12 @@ static void perform_bloch_simulation(int N, struct sim_data* data, long mdims[N]
 int main_sim(int argc, char* argv[argc])
 {
 	const char* out_signal = NULL;
-        const char* out_deriv = NULL;
+	const char* out_deriv = NULL;
 
 	struct arg_s args[] = {
 
 		ARG_OUTFILE(true, &out_signal, "signal: Mxy"),
-                ARG_OUTFILE(false, &out_deriv, "Partial derivatives: dR1, dM0, dR2, dB1"),
+		ARG_OUTFILE(false, &out_deriv, "Partial derivatives: dR1, dM0, dR2, dB1"),
 	};
 
 	struct sim_data data;
@@ -132,12 +132,12 @@ int main_sim(int argc, char* argv[argc])
 	data.voxel = simdata_voxel_defaults;
 	data.pulse = simdata_pulse_defaults;
 	data.pulse.sinc = pulse_sinc_defaults;
-        data.pulse.hs = pulse_hypsec_defaults;
+	data.pulse.hs = pulse_hypsec_defaults;
 	data.grad = simdata_grad_defaults;
-        data.other = simdata_other_defaults;
+	data.other = simdata_other_defaults;
 	data.cest = simdata_cest_defaults;
 
-        float T1[3] = { WATER_T1, WATER_T1, 1 };
+	float T1[3] = { WATER_T1, WATER_T1, 1 };
 	float T2[3] = { WATER_T2, WATER_T2, 1 };
 
 	// Further pool parameters
@@ -147,46 +147,46 @@ int main_sim(int argc, char* argv[argc])
 	float M0pools[4] = { 0., 0., 0., 0. };
 	float kpools[4] = { 0., 0., 0., 0. };
 
-        bool split_dim = false;
+	bool split_dim = false;
 
-        struct opt_s seq_opts[] = {
+	struct opt_s seq_opts[] = {
 
-                /* Sequence Specific Parameters */
-                OPTL_SELECT(0, "BSSFP", enum sim_seq, &(data.seq.seq_type), SEQ_BSSFP, "bSSFP"),
-                OPTL_SELECT(0, "IR-BSSFP", enum sim_seq, &(data.seq.seq_type), SEQ_IRBSSFP, "Inversion-Recovery bSSFP"),
-                OPTL_SELECT(0, "FLASH", enum sim_seq, &(data.seq.seq_type), SEQ_FLASH, "FLASH"),
-                OPTL_SELECT(0, "IR-FLASH", enum sim_seq, &(data.seq.seq_type), SEQ_IRFLASH, "Inversion-Recovery FLASH"),
+		/* Sequence Specific Parameters */
+		OPTL_SELECT(0, "BSSFP", enum sim_seq, &(data.seq.seq_type), SEQ_BSSFP, "bSSFP"),
+		OPTL_SELECT(0, "IR-BSSFP", enum sim_seq, &(data.seq.seq_type), SEQ_IRBSSFP, "Inversion-Recovery bSSFP"),
+		OPTL_SELECT(0, "FLASH", enum sim_seq, &(data.seq.seq_type), SEQ_FLASH, "FLASH"),
+		OPTL_SELECT(0, "IR-FLASH", enum sim_seq, &(data.seq.seq_type), SEQ_IRFLASH, "Inversion-Recovery FLASH"),
 		OPTL_SELECT(0, "CEST", enum sim_seq, &(data.seq.seq_type), SEQ_CEST, "CEST"),
-                OPTL_FLOAT(0, "TR", &(data.seq.tr), "float", "Repetition time [s]"),
-                OPTL_FLOAT(0, "TE", &(data.seq.te), "float", "Echo time [s]"),
-                OPTL_INT(0, "Nspins", &(data.seq.spin_num), "int", "Number of averaged spins"),
-                OPTL_INT(0, "Nrep", &(data.seq.rep_num), "int", "Number of repetitions"),
-                OPTL_SET(0, "pinv", &(data.seq.perfect_inversion), "Use perfect inversions"),
-                OPTL_FLOAT(0, "ipl", &(data.seq.inversion_pulse_length), "float", "Inversion Pulse Length [s]"),
-                OPTL_FLOAT(0, "isp", &(data.seq.inversion_spoiler), "float", "Inversion Spoiler Gradient Length [s]"),
-                OPTL_FLOAT(0, "ppl", &(data.seq.prep_pulse_length), "float", "Preparation Pulse Length [s]"),
-                OPTL_INT(0, "av-spokes", &(data.seq.averaged_spokes), "", "Number of averaged consecutive spokes"),
+		OPTL_FLOAT(0, "TR", &(data.seq.tr), "float", "Repetition time [s]"),
+		OPTL_FLOAT(0, "TE", &(data.seq.te), "float", "Echo time [s]"),
+		OPTL_INT(0, "Nspins", &(data.seq.spin_num), "int", "Number of averaged spins"),
+		OPTL_INT(0, "Nrep", &(data.seq.rep_num), "int", "Number of repetitions"),
+		OPTL_SET(0, "pinv", &(data.seq.perfect_inversion), "Use perfect inversions"),
+		OPTL_FLOAT(0, "ipl", &(data.seq.inversion_pulse_length), "float", "Inversion Pulse Length [s]"),
+		OPTL_FLOAT(0, "isp", &(data.seq.inversion_spoiler), "float", "Inversion Spoiler Gradient Length [s]"),
+		OPTL_FLOAT(0, "ppl", &(data.seq.prep_pulse_length), "float", "Preparation Pulse Length [s]"),
+		OPTL_INT(0, "av-spokes", &(data.seq.averaged_spokes), "", "Number of averaged consecutive spokes"),
 
-                /* Pulse Specific Parameters */
-                OPTL_FLOAT(0, "Trf", &(data.pulse.rf_end), "float", "Pulse Duration [s]"), /* Assumes to start at t=0 */
-                OPTL_FLOAT(0, "FA", &(data.pulse.sinc.INTERFACE.flipangle), "float", "Flipangle [deg]"),
-                OPTL_FLOAT(0, "BWTP", &(data.pulse.sinc.bwtp), "float", "Bandwidth-Time-Product"),
+		/* Pulse Specific Parameters */
+		OPTL_FLOAT(0, "Trf", &(data.pulse.rf_end), "float", "Pulse Duration [s]"), /* Assumes to start at t=0 */
+		OPTL_FLOAT(0, "FA", &(data.pulse.sinc.INTERFACE.flipangle), "float", "Flipangle [deg]"),
+		OPTL_FLOAT(0, "BWTP", &(data.pulse.sinc.bwtp), "float", "Bandwidth-Time-Product"),
 
-                /* Voxel Specific Parameters */
-                OPTL_FLOAT(0, "off", &(data.voxel.w), "float", "Off-Resonance [rad/s]"),
+		/* Voxel Specific Parameters */
+		OPTL_FLOAT(0, "off", &(data.voxel.w), "float", "Off-Resonance [rad/s]"),
 
-                /* Slice Profile Parameters */
-                OPTL_FLOAT(0, "sl-grad", &(data.grad.sl_gradient_strength), "float", "Strength of Slice Selection Gradient [T/m]"),
-                OPTL_FLOAT(0, "slice-thickness", &(data.seq.slice_thickness), "float", "Thickness of simulated slice [m]."),
+		/* Slice Profile Parameters */
+		OPTL_FLOAT(0, "sl-grad", &(data.grad.sl_gradient_strength), "float", "Strength of Slice Selection Gradient [T/m]"),
+		OPTL_FLOAT(0, "slice-thickness", &(data.seq.slice_thickness), "float", "Thickness of simulated slice [m]."),
 
-        };
-        const int N_seq_opts = ARRAY_SIZE(seq_opts);
+	};
+	const int N_seq_opts = ARRAY_SIZE(seq_opts);
 
-        struct opt_s other_opts[] = {
+	struct opt_s other_opts[] = {
 
-                OPTL_FLOAT(0, "ode-tol", &(data.other.ode_tol), "", "ODE tolerance value [def: 1e-5]"),
+		OPTL_FLOAT(0, "ode-tol", &(data.other.ode_tol), "", "ODE tolerance value [def: 1e-5]"),
 		OPTL_FLOAT(0, "stm-tol", &(data.other.stm_tol), "", "STM tolerance value [def: 1e-6]"),
-                OPTL_FLOAT(0, "sampling-rate", &(data.other.sampling_rate), "", "Sampling rate of RF pulse used for ROT simulation in Hz [def: 1e6 Hz]"),
+		OPTL_FLOAT(0, "sampling-rate", &(data.other.sampling_rate), "", "Sampling rate of RF pulse used for ROT simulation in Hz [def: 1e6 Hz]"),
 	};
 	const int N_other_opts = ARRAY_SIZE(other_opts);
 
@@ -199,8 +199,8 @@ int main_sim(int argc, char* argv[argc])
 		OPTL_FLVEC4(0, "Om", &Ompools, "<2nd pool>:<3rd pool>:<4th pool>:<5th pool>", "Om values for further pools"),
 		OPTL_FLVEC4(0, "M0", &M0pools, "<2nd pool>:<3rd pool>:<4th pool>:<5th pool>", "M0 values for further pools"),
 		OPTL_FLVEC4(0, "k",  &kpools, "<k1>:<k2>:<k3>:<k4>", "k values for further pools"),
-        };
-        const int N_pool_opts = ARRAY_SIZE(pool_opts);
+	};
+	const int N_pool_opts = ARRAY_SIZE(pool_opts);
 
 	struct opt_s cest_opts[] = {
 
@@ -219,16 +219,16 @@ int main_sim(int argc, char* argv[argc])
 
 	const struct opt_s opts[] = {
 
-                OPTL_FLVEC3('1', "T1", &T1, "min:max:N", "range of T1 values"),
+		OPTL_FLVEC3('1', "T1", &T1, "min:max:N", "range of T1 values"),
 		OPTL_FLVEC3('2', "T2", &T2, "min:max:N", "range of T2 values"),
 		OPTL_SELECT(0, "BLOCH", enum sim_model, &(data.seq.model), MODEL_BLOCH, "Bloch Equations (default)"),
 		OPTL_SELECT(0, "BMC", enum sim_model, &(data.seq.model), MODEL_BMC, "Bloch-McConnell Equations"),
-                OPTL_SELECT(0, "ROT", enum sim_type, &(data.seq.type), SIM_ROT, "homogeneously discretized simulation based on rotational matrices"),
-                OPTL_SELECT(0, "ODE", enum sim_type, &(data.seq.type), SIM_ODE, "full ordinary differential equation solver based simulation (default)"),
-                OPTL_SELECT(0, "STM", enum sim_type, &(data.seq.type), SIM_STM, "state-transition matrix based simulation"),
-                OPTL_SET(0, "split-dim", &split_dim, "Split magnetization into x, y, and z component"),
-                OPTL_SUBOPT(0, "seq", "...", "configure sequence parameter", N_seq_opts, seq_opts),
-                OPTL_SUBOPT(0, "other", "...", "configure other parameters", N_other_opts, other_opts),
+		OPTL_SELECT(0, "ROT", enum sim_type, &(data.seq.type), SIM_ROT, "homogeneously discretized simulation based on rotational matrices"),
+		OPTL_SELECT(0, "ODE", enum sim_type, &(data.seq.type), SIM_ODE, "full ordinary differential equation solver based simulation (default)"),
+		OPTL_SELECT(0, "STM", enum sim_type, &(data.seq.type), SIM_STM, "state-transition matrix based simulation"),
+		OPTL_SET(0, "split-dim", &split_dim, "Split magnetization into x, y, and z component"),
+		OPTL_SUBOPT(0, "seq", "...", "configure sequence parameter", N_seq_opts, seq_opts),
+		OPTL_SUBOPT(0, "other", "...", "configure other parameters", N_other_opts, other_opts),
 		OPTL_SUBOPT(0, "pool", "...", "configure parameters for 2nd->5th pool", N_pool_opts, pool_opts),
 		OPTL_SUBOPT(0, "CEST", "...", "configure parameters for CEST", N_cest_opts, cest_opts),
 	};
@@ -240,12 +240,12 @@ int main_sim(int argc, char* argv[argc])
 	if (MODEL_BMC == data.seq.model)
 		assert(1 < data.voxel.P);
 
-        // Define output dimensions for signal
+	// Define output dimensions for signal
 
-        long mdims[DIMS] = { [0 ... DIMS - 1] = 1 };
+	long mdims[DIMS] = { [0 ... DIMS - 1] = 1 };
 
-        if (split_dim)
-                mdims[READ_DIM] = 3; // (x, y, z)
+	if (split_dim)
+		mdims[READ_DIM] = 3; // (x, y, z)
 
 	mdims[TE_DIM] = data.seq.rep_num;
 
@@ -256,21 +256,21 @@ int main_sim(int argc, char* argv[argc])
 	if ((mdims[TE_DIM] < 1) || (mdims[COEFF_DIM] < 1) || (mdims[COEFF2_DIM] < 1))
 		error("invalid parameter range\n");
 
-        // Allocate output file for signal and optional derivatives
+	// Allocate output file for signal and optional derivatives
 
 	complex float* signals = create_cfl(out_signal, DIMS, mdims);
 
-        long ddims[DIMS] = { [0 ... DIMS - 1] = 1 };
+	long ddims[DIMS] = { [0 ... DIMS - 1] = 1 };
 
-        md_copy_dims(DIMS, ddims, mdims);
-        ddims[MAPS_DIM] = (1 == data.voxel.P) ? 4 : 6; // [dR1, dM0, dR2, dB1], dOm, dk
+	md_copy_dims(DIMS, ddims, mdims);
+	ddims[MAPS_DIM] = (1 == data.voxel.P) ? 4 : 6; // [dR1, dM0, dR2, dB1], dOm, dk
 
-        complex float* deriv = NULL;
+	complex float* deriv = NULL;
 
-        if (NULL != out_deriv)
-                deriv = create_cfl(out_deriv, DIMS, ddims);
+	if (NULL != out_deriv)
+		deriv = create_cfl(out_deriv, DIMS, ddims);
 
-        // Temporary dimensions for derivative and magnetization
+	// Temporary dimensions for derivative and magnetization
 
 	long tmdims[DIMS];
 	md_select_dims(DIMS, ~(COEFF_FLAG|COEFF2_FLAG), tmdims, mdims);
@@ -279,17 +279,17 @@ int main_sim(int argc, char* argv[argc])
 	md_select_dims(DIMS, ~(COEFF_FLAG|COEFF2_FLAG), tddims, ddims);
 
 
-        // Allocate temporary magnetization and derivative arrays
+	// Allocate temporary magnetization and derivative arrays
 
 	complex float* tm = md_calloc(DIMS, tmdims, CFL_SIZE);
 	complex float* td = md_calloc(DIMS, tddims, CFL_SIZE);
 
 
-        // Run all simulations and store signal and optional derivatives
+	// Run all simulations and store signal and optional derivatives
 
-        long pos[DIMS] = { 0 };
+	long pos[DIMS] = { 0 };
 
-        // Starting time of simulation
+	// Starting time of simulation
 	double start = timestamp();
 	do {
 		data.voxel.r1[0] = 1. / (T1[0] + (T1[1] - T1[0]) / T1[2] * (float)pos[COEFF_DIM]);
@@ -317,18 +317,18 @@ int main_sim(int argc, char* argv[argc])
 
 	} while (md_next(DIMS, mdims, ~(READ_FLAG|MAPS_FLAG|TE_FLAG|ITER_FLAG), pos));
 
-        // End time of simulation
+	// End time of simulation
 	double end = timestamp();
 
-        debug_printf(DP_INFO, "%f\n", end - start);
+	debug_printf(DP_INFO, "%f\n", end - start);
 
-        md_free(tm);
-        md_free(td);
+	md_free(tm);
+	md_free(td);
 
 	unmap_cfl(DIMS, mdims, signals);
 
-        if (NULL != out_deriv)
-                unmap_cfl(DIMS, ddims, deriv);
+	if (NULL != out_deriv)
+		unmap_cfl(DIMS, ddims, deriv);
 
 	return 0;
 }
