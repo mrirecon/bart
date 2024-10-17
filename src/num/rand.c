63c877b24e365a02cdf926487bab8b291fc41efd
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-10-17 14:25:01 +0200

    vptr support for obsolete rand

--- src/num/rand.c
+++ src/num/rand.c
@@ -389,8 +389,12 @@ void uniform_rand_vec(long N, float* dst)
 
 static void md_gaussian_obsolete_rand(int D, const long dims[D], complex float* dst)
 {
-#ifdef  USE_CUDA
-	if (cuda_ondevice(dst)) {
+	bool buf = is_vptr(dst);
+#ifdef USE_CUDA
+	buf = buf || cuda_ondevice(dst);
+#endif
+
+	if (buf) {
 
 		complex float* tmp = md_alloc(D, dims, sizeof(complex float));
 
@@ -400,7 +404,7 @@ static void md_gaussian_obsolete_rand(int D, const long dims[D], complex float* 
 		md_free(tmp);
 		return;
 	}
-#endif
+
 	long N = md_calc_size(D, dims);
 
 	for (long i = 0; i < N; i++)
@@ -558,8 +562,12 @@ void md_gaussian_rand(int D, const long dims[D], complex float* dst)
 
 static void md_uniform_obsolete_rand(int D, const long dims[D], complex float* dst)
 {
-#ifdef  USE_CUDA
-	if (cuda_ondevice(dst)) {
+	bool buf = is_vptr(dst);
+#ifdef USE_CUDA
+	buf = buf || cuda_ondevice(dst);
+#endif
+
+	if (buf) {
 
 		complex float* tmp = md_alloc(D, dims, sizeof(complex float));
 
@@ -569,7 +577,7 @@ static void md_uniform_obsolete_rand(int D, const long dims[D], complex float* d
 		md_free(tmp);
 		return;
 	}
-#endif
+
 	long N = md_calc_size(D, dims);
 
 	for (long i = 0; i < N; i++)
@@ -614,8 +622,12 @@ void md_uniform_rand(int D, const long dims[D], complex float* dst)
 
 static void md_obsolete_rand_one(int D, const long dims[D], complex float* dst, double p)
 {
-#ifdef  USE_CUDA
-	if (cuda_ondevice(dst)) {
+	bool buf = is_vptr(dst);
+#ifdef USE_CUDA
+	buf = buf || cuda_ondevice(dst);
+#endif
+
+	if (buf) {
 
 		complex float* tmp = md_alloc(D, dims, sizeof(complex float));
 
@@ -625,7 +637,7 @@ static void md_obsolete_rand_one(int D, const long dims[D], complex float* dst, 
 		md_free(tmp);
 		return;
 	}
-#endif
+
 	long N = md_calc_size(D, dims);
 
 	for (long i = 0; i < N; i++)
