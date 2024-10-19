986a5b52e5a6fcdbba4a92b6d1ff73b44f0928eb
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-10-19 19:58:45 +0200

    compare iovecs

--- src/num/iovec.h
+++ src/num/iovec.h
@@ -21,7 +21,7 @@ extern const struct iovec_s* iovec_create(int N, const long dims[__VLA(N)], size
 extern const struct iovec_s* iovec_create2(int N, const long dims[__VLA(N)], const long strs[__VLA(N)], size_t size);
 extern void iovec_free(const struct iovec_s* x);
 extern bool iovec_check(const struct iovec_s* iov, int N, const long dims[__VLA(N)], const long strs[__VLA(N)]);
-
+extern bool iovec_compare(const struct iovec_s* iova, const struct iovec_s* iovb);
 
 // in-place initialization and deconstruction
 extern void iovec_init2(struct iovec_s* n, int N, const long dims[__VLA(N)], const long strs[__VLA(N)], size_t size);
