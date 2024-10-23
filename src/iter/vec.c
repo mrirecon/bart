7a5c7da667ae2b5fa573087f85a9181ece856bdf
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-10-23 16:45:32 +0200

    mpi aware iter ops

--- src/iter/vec.c
+++ src/iter/vec.c
@@ -1,8 +1,9 @@
 
 #include "num/vecops.h"
 #include "num/gpuops.h"
+#include "num/vptr.h"
+#include "num/vptr_iter.h"
 
-#include "misc/misc.h"
 
 #include "vec.h"
 
@@ -15,6 +16,15 @@
 
 const struct vec_iter_s* select_vecops(const float* x)
 {
+	if (is_vptr(x)) {
+
+#ifdef USE_CUDA
+		return cuda_ondevice(x) ? &vptr_iter_ops_gpu : &vptr_iter_ops;
+#else
+		return &vptr_iter_ops;
+#endif
+	}
+
 #ifdef USE_CUDA
 	return cuda_ondevice(x) ? &gpu_iter_ops : &cpu_iter_ops;
 #else
