4bd65c5b128f14b199a4fe10c2369c0f725fbbab
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-10-23 14:13:09 +0200

    remove stacking from t1 fun

--- src/moba/model_T1.c
+++ src/moba/model_T1.c
@@ -37,7 +37,7 @@
 
 
 
-static struct mobamod T1_create_internal(const long dims[DIMS], const complex float* mask, const complex float* TI, const complex float* psf,
+struct mobamod T1_create(const long dims[DIMS], const complex float* mask, const complex float* TI, const complex float* psf,
 				float scaling_M0, float scaling_R1s, const struct noir_model_conf_s* conf, float fov)
 {
 	long data_dims[DIMS];
@@ -102,93 +102,10 @@ static struct mobamod T1_create_internal(const long dims[DIMS], const complex fl
 	const struct nlop_s* c = nlop_chain2_FF(T1, 0, b, 0);
 
 	nlinv.nlop = nlop_permute_inputs_F(c, 2, (const int[2]){ 1, 0 });
-	ret.nlop = nlinv.nlop;
+	ret.nlop = nlop_flatten_inputs_F(nlinv.nlop);
 	ret.linop = nlinv.linop;
 
 	return ret;
 }
 
-
-struct mobamod T1_create(const long dims[DIMS], const complex float* mask, const complex float* TI, const complex float* psf, 
-			float scaling_M0, float scaling_R1s, const struct noir_model_conf_s* conf, float fov)
-{
-	unsigned long bat_flags = TIME_FLAG | TIME2_FLAG;
-	int bat_idx = TIME_DIM;
-	
-	if (1 < dims[TIME2_DIM]) {
-
-		if (   ((0 == MD_IS_SET(conf->ptrn_flags, TIME_DIM)) != (0 == MD_IS_SET(conf->ptrn_flags, TIME2_DIM)))
-		    || ((0 == MD_IS_SET(conf->cnstcoil_flags, TIME_DIM)) != (0 == MD_IS_SET(conf->cnstcoil_flags, TIME2_DIM))) ) {
-
-			bat_flags = MD_CLEAR(bat_flags, TIME_DIM);
-			bat_idx = TIME2_DIM;
-	    	}  
-	}
-
-	long bat_dims[DIMS];
-	long dims_slc[DIMS];
-
-	md_select_dims(DIMS,  bat_flags, bat_dims, dims);
-	md_select_dims(DIMS, ~bat_flags, dims_slc, dims);
-
-	long psf_dims[DIMS];
-	md_select_dims(DIMS, conf->ptrn_flags & ~COEFF_FLAG, psf_dims, dims);
-
-	long psf_dims_slc[DIMS];
-	md_select_dims(DIMS, ~bat_flags, psf_dims_slc, psf_dims);
-
-	long TI_dims[DIMS];
-	md_select_dims(DIMS, TE_FLAG|TIME_FLAG|TIME2_FLAG, TI_dims, dims);
-
-	long TI_dims_slc[DIMS];
-	md_select_dims(DIMS, ~bat_flags, TI_dims_slc, TI_dims);
-
-	int N = md_calc_size(DIMS, bat_dims);
-	
-	const struct linop_s* lop = NULL;
-	const struct nlop_s* nlops[N];
-
-	long pos[DIMS];
-	md_singleton_strides(DIMS, pos);
-
-	int i = 0;
-
-	do {
-		complex float* psf_tmp = md_alloc(DIMS, psf_dims_slc, CFL_SIZE);
-
-		md_slice(DIMS, bat_flags, pos, psf_dims, psf_tmp, psf, CFL_SIZE);
-
-		complex float* TI_tmp = md_alloc(DIMS, psf_dims_slc, CFL_SIZE);
-
-		md_slice(DIMS, bat_flags, pos, TI_dims, TI_tmp, TI, CFL_SIZE);
-
-		struct mobamod T1 = T1_create_internal(dims_slc, mask, TI_tmp, psf_tmp, scaling_M0, scaling_R1s, conf, fov);	
-
-		nlops[i++] = T1.nlop;	
-
-		if (NULL == lop) {
-
-			lop = linop_clone(T1.linop);
-
-		} else {
-
-			if (!MD_IS_SET(conf->cnstcoil_flags, bat_idx))
-				lop = linop_stack_FF(bat_idx, bat_idx, lop, linop_clone(T1.linop));
-		}
-
-		md_free(TI_tmp);
-		md_free(psf_tmp);
-
-	} while (md_next(DIMS, bat_dims, bat_flags, pos));
-
-	struct mobamod result = {
-
-		.nlop = (1 == N) ? (struct nlop_s*)nlops[0] : (struct nlop_s*)nlop_stack_container_create_F(N, nlops, 2, (int [2]){ bat_idx, (MD_IS_SET(conf->cnstcoil_flags, bat_idx) ? -1 : bat_idx) }, 1, (int[1]){ bat_idx }),
-		.linop = lop,
-	};
-
-	result.nlop = nlop_flatten_F(result.nlop);
-
-	return result;
-}
 
