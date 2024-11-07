33428977aa7e49e7663c56f652b708934c6125d3
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-11-07 15:06:10 +0100

    prox for scaling

--- src/iter/prox3.h
+++ src/iter/prox3.h
@@ -8,6 +8,9 @@ struct operator_p_s;
 extern const struct operator_p_s* prox_convex_conjugate(const struct operator_p_s* prox);
 extern const struct operator_p_s* prox_convex_conjugate_F(const struct operator_p_s* prox);
 
+extern const struct operator_p_s* prox_scale(const struct operator_p_s* prox, float scale);
+extern const struct operator_p_s* prox_scale_F(const struct operator_p_s* prox, float scale);
+
 #include "misc/cppwrap.h"
 
 #endif
