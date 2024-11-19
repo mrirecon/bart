41c72005832c1b84643df2051711f00905c60ebb
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-11-19 20:44:08 +0100

    nn weights move vptr

--- src/nn/weights.h
+++ src/nn/weights.h
@@ -25,17 +25,18 @@ nn_weights_t load_nn_weights(const char *name);
 
 extern void nn_weights_copy(nn_weights_t dst, nn_weights_t src);
 
-void dump_nn_weights(const char *name, nn_weights_t weights);
-void move_gpu_nn_weights(nn_weights_t weights);
-_Bool nn_weights_on_gpu(nn_weights_t weights);
-void nn_weights_free(nn_weights_t weights);
+extern void dump_nn_weights(const char *name, nn_weights_t weights);
+extern void move_gpu_nn_weights(nn_weights_t weights);
+extern void move_vptr_nn_weights(nn_weights_t weights);
+extern _Bool nn_weights_on_gpu(nn_weights_t weights);
+extern void nn_weights_free(nn_weights_t weights);
 
-void nn_init(nn_t op, nn_weights_t weights);
+extern void nn_init(nn_t op, nn_weights_t weights);
 
-const struct nn_s* nn_get_wo_weights(nn_t op, nn_weights_t weights, _Bool copy);
-const struct nn_s* nn_get_wo_weights_F(nn_t op, nn_weights_t weights, _Bool copy);
+extern const struct nn_s* nn_get_wo_weights(nn_t op, nn_weights_t weights, _Bool copy);
+extern const struct nn_s* nn_get_wo_weights_F(nn_t op, nn_weights_t weights, _Bool copy);
 
-const struct nlop_s* nn_get_nlop_wo_weights(nn_t op, nn_weights_t weights, _Bool copy);
-const struct nlop_s* nn_get_nlop_wo_weights_F(nn_t op, nn_weights_t weights, _Bool copy);
+extern const struct nlop_s* nn_get_nlop_wo_weights(nn_t op, nn_weights_t weights, _Bool copy);
+extern const struct nlop_s* nn_get_nlop_wo_weights_F(nn_t op, nn_weights_t weights, _Bool copy);
 
 #endif
