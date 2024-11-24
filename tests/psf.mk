bd4b09a1ae4699f4b214f268fb9350c825aad226
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-11-24 23:09:14 +0100

    tests for psf

---
+++ tests/psf.mk
@@ -0,0 +1,11 @@
+
+tests/test-psf: traj psf nrmse
+	set -e ; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
+	$(TOOLDIR)/traj -r trj.ra						;\
+	$(TOOLDIR)/psf --oversampled trj.ra psf1.ra				;\
+	$(TOOLDIR)/psf --oversampled-decomposed trj.ra psf2.ra			;\
+	$(TOOLDIR)/nrmse -t 1.5e-5 psf1.ra psf2.ra				;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+TESTS += tests/test-psf
