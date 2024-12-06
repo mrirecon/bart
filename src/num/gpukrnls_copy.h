6f160770f7a52616b030ad05af6d3cd8027ce51c
Moritz Blumenthal <blumenthal@tugraz.at>
2024-12-06 22:24:04 +0100

    tool to compress with binary mask

--- src/num/gpukrnls_copy.h
+++ src/num/gpukrnls_copy.h
@@ -4,6 +4,10 @@ extern "C" {
 
 extern void cuda_copy_ND(int D, const long dims[], const long ostrs[], void* dst, const long istrs[], const void* src, unsigned long size);
 
+extern void cuda_decompress(long stride, long N, long dcstrs, void* dst, long istrs, const long* index, const void* src, unsigned long size);
+extern void cuda_compress(long stride, long N, void* dst, long istrs, const long* index, long dcstrs, const void* src, unsigned long size);
+
+
 #ifdef __cplusplus
 }
 #endif
