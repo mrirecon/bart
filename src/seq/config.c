cf9090934be2363d2321525bc395b63e21a97dcb
Daniel Mackner <daniel.mackner@tugraz.at>
2025-07-01 13:15:25 +0200

    basic sequence config

---
+++ src/seq/config.c
@@ -0,0 +1,54 @@
+/* Copyright 2025. Institute of Biomedical Imaging. TU Graz.
+ * All rights reserved. Use of this source code is governed by
+ * a BSD-style license which can be found in the LICENSE file.
+ */
+
+#include "misc/mri.h"
+
+#include "config.h"
+
+const struct seq_config seq_config_defaults = {
+
+	.phys = {
+		.tr = 3110.,
+		.te = 1900.,
+		.dwell = 4.0,
+		.os = 2.,
+		.contrast = CONTRAST_RF_RANDOM,
+		.rf_duration = 620.,
+		.flip_angle = 6.,
+		.bwtp = 3.8,
+	},
+
+	.geom = {
+		.fov = 256,
+		.slice_thickness = 6.,
+		.shift = { [0 ... MAX_SLICES - 1] = { 0., 0., 0. } },
+		.baseres = 256,
+		.mb_factor = 1,
+		.sms_distance = 20.,
+	},
+
+	.enc = {
+		.pe_mode = PEMODE_RATION_APPROX_GA,
+		.tiny = 1,
+		.aligned_flags = 0,
+	},
+
+	.magn = {
+		.mag_prep = PREP_OFF,
+		.ti = 0.,
+	},
+
+	.sys = {
+		.gamma = 42.575575,
+		.grad.inv_slew_rate = 7.848885540911,
+		.grad.max_amplitude = 24.,
+		.coil_control_lead = 100.,
+		.min_duration_ro_rf = 213.,
+	},
+
+	.order = { [0 ... DIMS - 1] = 1 },
+	.loop_dims = { [0 ... DIMS - 1] = 1 },
+};
+
---
+++ src/seq/config.h
@@ -0,0 +1,121 @@
+
+#ifndef __SEQ_CONFIG_H
+#define __SEQ_CONFIG_H
+
+#include "misc/cppwrap.h"
+#include "misc/mri.h"
+
+#include "seq/gradient.h"
+
+#define MAX_RF_PULSES 32
+#define MAX_SLICES 64
+#define MAX_GRAD_POINTS 8192
+#define GRAD_RASTER_TIME 10
+
+enum block {
+
+	BLOCK_UNDEFINED,
+	BLOCK_PRE,
+	BLOCK_KERNEL_CHECK,
+	BLOCK_KERNEL_PREPARE,
+	BLOCK_KERNEL_NOISE,
+	BLOCK_KERNEL_DUMMY ,
+	BLOCK_KERNEL_IMAGE,
+	BLOCK_POST 
+};
+
+enum flash_contrast {
+
+	CONTRAST_RF_RANDOM = 1,
+	CONTRAST_RF_SPOILED
+};
+
+enum mag_prep {
+
+	PREP_OFF,
+	PREP_IR_NON,
+};
+
+
+enum pe_mode {
+
+	PEMODE_RATION_APPROX_GA,
+	PEMODE_RATION_APPROX_GAAL
+};
+
+
+
+struct seq_phys {
+
+	double tr; // us
+	double te; // us
+
+	double dwell; // us (FIXME calc from br and bandwidth?)
+	double os;
+
+	enum flash_contrast contrast;
+	double rf_duration; // us
+	double flip_angle; // deg
+	double bwtp;
+};
+
+struct seq_geom {
+
+	double fov; // mm
+	double slice_thickness; // mm
+	double shift[MAX_SLICES][3]; // [ro, pe, slice] in mm
+
+	int baseres; // 1
+
+	int mb_factor;
+	double sms_distance; // mm
+};
+
+struct seq_enc {
+
+	enum pe_mode pe_mode;
+	int tiny;
+	unsigned long aligned_flags;
+};
+
+struct seq_magn {
+
+	enum mag_prep mag_prep;
+	double ti; // us
+};
+
+struct seq_sys {
+
+	double gamma; // MHz/T
+	struct grad_limits grad; // inv_slew_rate in us / (mT/m), max_amplitude in mT/m
+	double coil_control_lead; // us
+	double min_duration_ro_rf; // us
+};
+
+
+
+struct seq_config {
+
+	struct seq_phys phys;
+	struct seq_geom geom;
+	struct seq_enc enc;
+	struct seq_magn magn;
+	struct seq_sys sys; 
+
+	int order[DIMS];
+	long loop_dims[DIMS];
+};
+
+struct seq_state {
+
+	enum block mode;
+	long chrono_slice;
+	long pos[DIMS];
+};
+
+
+extern const struct seq_config seq_config_defaults;
+
+#include "misc/cppwrap.h"
+
+#endif
