e1f157ad03b010d9de4b187762eacac5b5b7a517
Blumenthal, Moritz <blumenthal@tugraz.at>
2025-07-10 07:18:43 +0200

    trajcor read gradient delays from file and optionally correct transversal

--- src/trajcor.c
+++ src/trajcor.c
@@ -35,6 +35,9 @@ int main_trajcor(int argc, char* argv[argc])
 	const char* outputtraj_file = NULL;
 
 	float gdelays[3] = { 0., 0., 0. };
+	bool transverse = false;
+
+	const char* gdelays_file = NULL;
 
 	struct arg_s args[] = {
 		ARG_INFILE(true, &inputtraj_file, "traj"),
@@ -43,6 +46,8 @@ int main_trajcor(int argc, char* argv[argc])
 
 	const struct opt_s opts[] = {
 		OPT_FLVEC3('q', &gdelays, "delays", "gradient delays: y, x, xy"),
+		OPT_INFILE('V', &gdelays_file, "file", "custom_gdelays"),
+		OPT_SET('O', &transverse, "correct transverse gradient error for radial tajectories"),
 	};
 
 	cmdline(&argc, argv, ARRAY_SIZE(args), args, help_str, ARRAY_SIZE(opts), opts);
@@ -61,14 +66,59 @@ int main_trajcor(int argc, char* argv[argc])
 
 	complex float* outtraj = create_cfl(outputtraj_file, N, dimstraj);
 
+	long gdmat_dims[DIMS];
+	md_singleton_dims(N, gdmat_dims);
+	gdmat_dims[0] = 3; // 3x3 matrix for gradient delays
+	gdmat_dims[0] = 3; // 3x3 matrix for gradient delays
 
-	// to be consistend with traj tool this is flipped
-	// as x is the second entry in the traj and y is the first
-	complex float mat[3][3] = {
-		{ gdelays[1], gdelays[2], 0. },
-		{ gdelays[2], gdelays[0], 0. },
-		{ 0., 0., 0. },
-	};
+	long gdims[DIMS];
+	complex float* delays = NULL;
+
+	if (NULL != gdelays_file) {
+
+		delays = load_cfl(gdelays_file, DIMS, gdims);
+
+		assert((3 == gdims[0] || (6 == gdims[0])));
+		assert(md_check_compat(DIMS - 1, ~0UL, dimstraj + 1, gdims + 1));
+
+	} else {
+
+		md_singleton_dims(DIMS, gdims);
+		gdims[0] = 6;
+
+		delays = anon_cfl(NULL, DIMS, gdims);
+		md_clear(DIMS, gdims, delays, CFL_SIZE);
+
+		for (int i = 0; i < 3; i++)
+			delays[i] = gdelays[i];
+	}
+
+	long mat_dims[DIMS];
+	md_copy_dims(N, mat_dims, gdims);
+	mat_dims[0] = 3;
+	mat_dims[1] = 3;
+	complex float* mat = md_alloc_sameplace(DIMS, mat_dims, CFL_SIZE, traj);
+	md_clear(DIMS, mat_dims, mat, CFL_SIZE);
+
+	long slc_dims[DIMS];
+	md_select_dims(DIMS, ~3UL, slc_dims, gdims);
+
+	long mat_strs[DIMS];
+	long gstrs[DIMS];
+	md_calc_strides(DIMS, mat_strs, mat_dims, CFL_SIZE);
+	md_calc_strides(DIMS, gstrs, gdims, CFL_SIZE);
+
+	// explicite copy due to swapped definition y:x:xy
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 0 + 0), gstrs, delays + 1, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 1 + 1), gstrs, delays + 0, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 0 + 1), gstrs, delays + 2, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 1 + 0), gstrs, delays + 2, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 2 + 2), gstrs, delays + 3, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 0 + 2), gstrs, delays + 4, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 2 + 0), gstrs, delays + 4, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 1 + 2), gstrs, delays + 5, CFL_SIZE);
+	md_copy2(DIMS, slc_dims, mat_strs, mat + (3 * 2 + 1), gstrs, delays + 5, CFL_SIZE);
+	unmap_cfl(DIMS, gdims, delays);
 
 
 	long dir_dims[N];
@@ -80,17 +130,25 @@ int main_trajcor(int argc, char* argv[argc])
 	long tdir_dims[N];
 	md_transpose_dims(N, 0, 1, tdir_dims, dir_dims);
 
-	long mat_dims[N];
-	md_singleton_dims(N, mat_dims);
-	md_max_dims(N, MD_BIT(0) | MD_BIT(1), mat_dims, dir_dims, tdir_dims);
-
 	complex float* offset = md_alloc_sameplace(N, dir_dims, CFL_SIZE, traj);
-	md_ztenmul(N, dir_dims, offset, mat_dims, &(mat[0][0]), tdir_dims, dir);
+	md_ztenmul(N, dir_dims, offset, mat_dims, mat, tdir_dims, dir);
+
+	if (!transverse) {
+
+		// project offset onto direction of spoke
+		long ddims[N];
+		md_select_dims(N, ~MD_BIT(0), ddims, dir_dims);
+		complex float* delay = md_alloc_sameplace(DIMS, ddims, CFL_SIZE, dir);
+		md_ztenmul(N, ddims, delay, dir_dims, dir, dir_dims, offset);
+		md_ztenmul(N, dir_dims, offset, dir_dims, dir, ddims, delay);
+		md_free(delay);
+	}
 
 	md_free(dir);
 	md_zadd2(N, dimstraj, MD_STRIDES(N, dimstraj, CFL_SIZE), outtraj, MD_STRIDES(N, dimstraj, CFL_SIZE), traj, MD_STRIDES(N, dir_dims, CFL_SIZE), offset);
 
 	md_free(offset);
+	md_free(mat);
 
 	unmap_cfl(N, dimstraj, traj);
 	unmap_cfl(N, dimstraj, outtraj);
--- tests/trajcor.mk
+++ tests/trajcor.mk
@@ -1,9 +1,9 @@
 
 tests/test-trajcor: traj trajcor nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
-	$(TOOLDIR)/traj -r -y10  -s1 traj.ra				;\
-	$(TOOLDIR)/traj -r -y10  -s1 -O -q1:2:3 traj_gd.ra		;\
-	$(TOOLDIR)/trajcor -q1:2:3 traj.ra traj_cor.ra			;\
+	$(TOOLDIR)/traj -r -y10 -s1 traj.ra				;\
+	$(TOOLDIR)/traj -r -y10 -s1 -O -q1:2:3 traj_gd.ra		;\
+	$(TOOLDIR)/trajcor -q1:2:3 -O traj.ra traj_cor.ra		;\
 	$(TOOLDIR)/nrmse -t1.e-6 traj_gd.ra traj_cor.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
@@ -13,9 +13,30 @@ tests/test-trajcor2: traj trajcor nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
 	$(TOOLDIR)/traj -r -y10 -t3 -s1 traj.ra				;\
 	$(TOOLDIR)/traj -r -y10 -t3 -s1 -O -q-1:0.5:1 traj_gd.ra	;\
-	$(TOOLDIR)/trajcor -q-1:0.5:1 traj.ra traj_cor.ra		;\
+	$(TOOLDIR)/trajcor -q-1:0.5:1 -O traj.ra traj_cor.ra		;\
 	$(TOOLDIR)/nrmse -t1.e-6 traj_gd.ra traj_cor.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
-TESTS += tests/test-trajcor tests/test-trajcor2
+
+tests/test-trajcor3: traj trajcor nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
+	$(TOOLDIR)/traj -r -y10 -s1 traj.ra				;\
+	$(TOOLDIR)/traj -r -y10 -s1 -q1:2:3 traj_gd.ra			;\
+	$(TOOLDIR)/trajcor -q1:2:3 traj.ra traj_cor.ra			;\
+	$(TOOLDIR)/nrmse -t1.e-6 traj_gd.ra traj_cor.ra			;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+tests/test-trajcor-file: traj trajcor vec nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
+	$(TOOLDIR)/vec -- -1 0.5 1 qf.ra				;\
+	$(TOOLDIR)/traj -r -y10 -t3 -s1 traj.ra				;\
+	$(TOOLDIR)/trajcor -Vqf.ra    traj.ra traj1.ra			;\
+	$(TOOLDIR)/trajcor -q-1:0.5:1 traj.ra traj2.ra			;\
+	$(TOOLDIR)/nrmse -t1.e-6 traj1.ra traj2.ra			;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+TESTS += tests/test-trajcor tests/test-trajcor2 tests/test-trajcor3 tests/test-trajcor-file
