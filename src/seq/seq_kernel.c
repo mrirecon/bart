8337ca578a27f5039cfa934d99a0b2c1ffbd3186
Daniel Mackner <daniel.mackner@tugraz.at>
2025-07-11 12:05:21 +0200

    basic sequence functions

---
+++ src/seq/seq_kernel.c
@@ -0,0 +1,177 @@
+/* Copyright 2025. Institute of Biomedical Imaging. TU Graz.
+ * All rights reserved. Use of this source code is governed by
+ * a BSD-style license which can be found in the LICENSE file.
+ */
+
+#include <complex.h>
+#include <math.h>
+#include <assert.h>
+
+#include "num/multind.h"
+
+#include "misc/mri.h"
+#include "misc/misc.h"
+
+#include "seq/gradient.h"
+#include "seq/misc.h"
+
+#include "seq_kernel.h"
+
+#ifndef CFL_SIZE
+#define CFL_SIZE sizeof(complex float)
+#endif
+
+long block_end(int N, const struct seq_event ev[__VLA(N)])
+{
+	return round_up_GRT(events_end_time(N, ev, 0, 0));
+}
+
+long block_end_flat(int N, const struct seq_event ev[N])
+{
+	return round_up_GRT(events_end_time(N, ev, 1, 1));
+}
+
+
+long block_rdt(int N, const struct seq_event ev[N])
+{
+	return round_up_GRT(events_end_time(N, ev, 1, 0) - block_end_flat(N, ev));
+}
+
+
+/*
+ * Compute 0th moment on a raster. 
+ */
+void seq_compute_moment0(int M, float moments[M][3], double dt, int N, const struct seq_event ev[N])
+{
+	for (int i = 0; i < M; i++) 
+		for (int a = 0; a < 3; a++)
+			moments[i][a] = 0;
+
+	for (int p = 0; p < M; p++) {
+
+		assert((0 <= p) && (p <= M));
+
+		double m[3];
+		moment_sum(m, (p + 0.5)  * dt, N, ev);
+
+		for (int a = 0; a < 3; a++) 
+			moments[p][a] = m[a];
+	}
+}
+
+
+/*
+ * Compute times and phase of adc samples. 
+ */
+void seq_compute_adc_samples(int D, const long adc_dims[D], complex float* adc, int N, const struct seq_event ev[N])
+{
+	md_clear(D, adc_dims, adc, CFL_SIZE);
+	long adc_strs[D];
+	md_calc_strides(D, adc_strs, adc_dims, CFL_SIZE);
+
+	int e = 0;
+	for (int i = 0; i < N; i++) {
+
+		if (SEQ_EVENT_ADC != ev[i].type)
+			continue;
+
+		double dwell = (ev[i].end - ev[i].start) / (ev[i].adc.columns * ev[i].adc.os);
+
+		long pos[DIMS] = {  };
+		pos[TE_DIM] = e;
+
+		do {
+			double ts = ev[i].start + (pos[0] + 0.5) * dwell;
+			MD_ACCESS(D, adc_strs, (pos[MAPS_DIM] = 0, pos), adc) = ts;
+			MD_ACCESS(D, adc_strs, (pos[MAPS_DIM] = 1, pos), adc) = cexpf(1.i * DEG2RAD(ev[i].adc.phase + ev[i].adc.freq * ts));
+
+		} while(md_next(D, adc_dims, READ_FLAG, pos));
+
+		e++;
+	}
+	assert(e == adc_dims[TE_DIM]);
+}
+
+/*
+ * Compute gradients on a raster. This also works
+ * (i.e. yields correct 0 moment) if the abstract
+ * gradients do not start and end on the raster.
+ * We integrate over each interval to obtain the
+ * average gradient.
+ */
+void seq_compute_gradients(int M, double gradients[M][3], double dt, int N, const struct seq_event ev[N])
+{
+	for (int i = 0; i < M; i++) 
+		for (int a = 0; a < 3; a++)
+			gradients[i][a] = 0.;
+
+	for (int i = 0; i < N; i++) {
+
+		if (SEQ_EVENT_GRADIENT != ev[i].type)
+			continue;
+
+		double s = ev[i].start;
+		double e = ev[i].end;
+
+
+		/*            |    /
+                 *            |   /|
+                 *            .../..
+                 *            | /  |
+                 *            |/   |
+                 *            /    |
+                 *       ..../|    |
+                 *  |____|__/_|____|____|
+                 *    0    1    2    3  
+                 */
+
+		assert(0. <= s);
+
+		double om[3];
+
+		for (int a = 0; a < 3; a++)
+			om[a] = 0.;
+
+		for (int p = trunc(s / dt); p <= ceil(e / dt); p++) {
+
+			assert(0 <= p);
+
+			double m0[3];
+			moment(m0, (p + 1.) * dt, &ev[i]);
+
+			for (int a = 0; a < 3; a++) {
+
+				if (p < M)
+					gradients[p][a] += (m0[a] - om[a]) / dt;
+
+				om[a] = m0[a];
+			}
+		}
+	}
+}
+
+
+void seq_gradients_support(int M, double gradients[M][6], int N, const struct seq_event ev[N])
+{
+	for (int i = 0; i < M; i++) 
+		for (int a = 0; a < 6; a++)
+			gradients[i][a] = 0.;
+
+	int g = 0;
+
+	for (int i = 0; i < N; i++) {
+
+		if (SEQ_EVENT_GRADIENT != ev[i].type)
+			continue;
+
+		gradients[g][0] = ev[i].start;
+		gradients[g][1] = ev[i].mid;
+		gradients[g][2] = ev[i].end;
+		gradients[g][3] = ev[i].grad.ampl[0];
+		gradients[g][4] = ev[i].grad.ampl[1];
+		gradients[g][5] = ev[i].grad.ampl[2];
+
+		g++;
+	}
+}
+
---
+++ src/seq/seq_kernel.h
@@ -0,0 +1,25 @@
+#ifndef __SEQ_KERNEL_H
+#define __SEQ_KERNEL_H
+
+#include <complex.h>
+
+#include "misc/cppwrap.h"
+
+#include "seq/seq_event.h"
+
+
+
+long block_end(int N, const struct seq_event ev[__VLA(N)]);
+
+long block_end_flat(int N, const struct seq_event ev[__VLA(N)]);
+
+long block_rdt(int N, const struct seq_event ev[__VLA(N)]);
+
+void seq_compute_moment0(int M, float moments[__VLA(M)][3], double dt, int N, const struct seq_event ev[__VLA(N)]);
+void seq_compute_adc_samples(int D, const long adc_dims[__VLA(D)], _Complex float* adc, int N, const struct seq_event ev[__VLA(N)]);
+void seq_compute_gradients(int M, double gradients[__VLA(M)][3], double dt, int N, const struct seq_event ev[__VLA(N)]);
+void seq_gradients_support(int M, double gradients[__VLA(M)][6], int N, const struct seq_event ev[__VLA(N)]);
+
+#include "misc/cppwrap.h"
+
+#endif // __SEQ_KERNEL_H
