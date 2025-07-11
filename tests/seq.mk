d2834a3f5a852537402a49bc938e66a9de300477
Daniel Mackner <daniel.mackner@tugraz.at>
2025-07-11 07:50:33 +0200

    seq raga tests

--- tests/seq.mk
+++ tests/seq.mk
@@ -1,16 +1,71 @@
 
-tests/test-seq-traj: seq traj transpose slice join nrmse
-	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
-	$(TOOLDIR)/traj -r -D -s1 -x512 -y1 -t200 trj_ref.ra					;\
-	$(TOOLDIR)/seq -t200 --pe_mode 2 --norm-kspace grad.ra mom.ra samples.ra ;\
-	$(TOOLDIR)/transpose 0 1 samples.ra samples_t.ra				;\
-	$(TOOLDIR)/slice 4 3 samples_t.ra samples_t0.ra 				;\
-	$(TOOLDIR)/slice 4 2 samples_t.ra samples_t1.ra 				;\
-	$(TOOLDIR)/slice 4 4 samples_t.ra samples_t2.ra 				;\
-	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra ;\
-	$(TOOLDIR)/nrmse -t 0.000006 trj_ref.ra trj_seq.ra		;\
+tests/test-seq-raga: seq traj transpose slice join nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+	$(TOOLDIR)/traj -x 512 -y 377 -r -A -s 1 --double-base trj_ref.ra 		;\
+	$(TOOLDIR)/seq -r 377 -t377 --pe_mode 0 --norm-kspace grad.ra mom.ra samples.ra ;\
+	$(TOOLDIR)/transpose 1 2 samples.ra   samples_t.ra				;\
+	$(TOOLDIR)/transpose 0 1 samples_t.ra samples_tt.ra				;\
+	$(TOOLDIR)/slice 4 3 samples_tt.ra samples_t0.ra 				;\
+	$(TOOLDIR)/slice 4 2 samples_tt.ra samples_t1.ra 				;\
+	$(TOOLDIR)/slice 4 4 samples_tt.ra samples_t2.ra 				;\
+	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra		;\
+	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
-TESTS +=  tests/test-seq-traj
+TESTS +=  tests/test-seq-raga
+
+
+tests/test-seq-raga-sms: seq traj transpose slice join nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)								;\
+	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 --double-base trj_ref.ra 					;\
+	$(TOOLDIR)/seq -r 377 -t377 --pe_mode 0 --mb_factor=3 -m3 --norm-kspace grad.ra mom.ra samples.ra	;\
+	$(TOOLDIR)/transpose 1 2 samples.ra   samples_t.ra							;\
+	$(TOOLDIR)/transpose 0 1 samples_t.ra samples_tt.ra							;\
+	$(TOOLDIR)/slice 4 3 samples_tt.ra samples_t0.ra 							;\
+	$(TOOLDIR)/slice 4 2 samples_tt.ra samples_t1.ra 							;\
+	$(TOOLDIR)/slice 4 4 samples_tt.ra samples_t2.ra 							;\
+	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra 					;\
+	$(TOOLDIR)/nrmse -t 3e-7 trj_ref.ra trj_seq.ra								;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+TESTS +=  tests/test-seq-raga-sms
+
+
+tests/test-seq-raga-sms-al: seq traj transpose slice join nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)								;\
+	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 -l --double-base trj_ref.ra 					;\
+	$(TOOLDIR)/seq -r 377 -t377 --pe_mode 1 --mb_factor=3 -m3 --norm-kspace grad.ra mom.ra samples.ra	;\
+	$(TOOLDIR)/transpose 1 2 samples.ra   samples_t.ra							;\
+	$(TOOLDIR)/transpose 0 1 samples_t.ra samples_tt.ra							;\
+	$(TOOLDIR)/slice 4 3 samples_tt.ra samples_t0.ra 							;\
+	$(TOOLDIR)/slice 4 2 samples_tt.ra samples_t1.ra 							;\
+	$(TOOLDIR)/slice 4 4 samples_tt.ra samples_t2.ra 							;\
+	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra 					;\
+	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra								;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+TESTS +=  tests/test-seq-raga-sms-al
+
+tests/test-seq-raga-sms-al-frame: seq traj transpose slice join nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)								;\
+	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 -l -t3 --double-base trj_ref.ra 					;\
+	$(TOOLDIR)/seq -r 377 -t1131 --pe_mode 1 --mb_factor=3 -m3 --norm-kspace grad.ra mom.ra samples.ra	;\
+	$(TOOLDIR)/transpose 1 2 samples.ra   samples_t.ra							;\
+	$(TOOLDIR)/transpose 0 1 samples_t.ra samples_tt.ra							;\
+	$(TOOLDIR)/slice 4 3 samples_tt.ra samples_t0.ra 							;\
+	$(TOOLDIR)/slice 4 2 samples_tt.ra samples_t1.ra 							;\
+	$(TOOLDIR)/slice 4 4 samples_tt.ra samples_t2.ra 							;\
+	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra 					;\
+	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra								;\
+	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+TESTS +=  tests/test-seq-raga-sms-al-frame
+
