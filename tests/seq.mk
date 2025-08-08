319d3d636f63df4858fad2f0ed95c28c53b2b5d3
Daniel Mackner <daniel.mackner@tugraz.at>
2025-08-08 15:43:37 +0200

    seqtool: swap X and Y in adc to be consistent with traj

--- tests/seq.mk
+++ tests/seq.mk
@@ -1,13 +1,10 @@
 
-tests/test-seq-raga: seq traj transpose slice join nrmse
-	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
-	$(TOOLDIR)/traj -x 512 -y 377 -r -A -s 1 --double-base trj_ref.ra 		;\
+tests/test-seq-raga: seq traj extract nrmse 
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)				;\
+	$(TOOLDIR)/traj -x 512 -y 377 -r -A -s 1 --double-base trj_ref.ra 	;\
 	$(TOOLDIR)/seq -r 377 -t377 --raga grad.ra mom.ra samples.ra 		;\
-	$(TOOLDIR)/slice 0 3 samples.ra samples_t0.ra 				;\
-	$(TOOLDIR)/slice 0 2 samples.ra samples_t1.ra 				;\
-	$(TOOLDIR)/slice 0 4 samples.ra samples_t2.ra 				;\
-	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra		;\
-	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra					;\
+	$(TOOLDIR)/extract 0 2 5 samples.ra trj_seq.ra				;\
+	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra				;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
@@ -15,15 +12,12 @@ tests/test-seq-raga: seq traj transpose slice join nrmse
 TESTS +=  tests/test-seq-raga
 
 
-tests/test-seq-raga-sms: seq traj transpose slice join nrmse
-	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)								;\
-	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 --double-base trj_ref.ra 					;\
-	$(TOOLDIR)/seq -r 377 -t377 --raga --mb_factor=3 -m3 grad.ra mom.ra samples.ra			;\
-	$(TOOLDIR)/slice 0 3 samples.ra samples_t0.ra 							;\
-	$(TOOLDIR)/slice 0 2 samples.ra samples_t1.ra 							;\
-	$(TOOLDIR)/slice 0 4 samples.ra samples_t2.ra 							;\
-	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra 					;\
-	$(TOOLDIR)/nrmse -t 3e-7 trj_ref.ra trj_seq.ra								;\
+tests/test-seq-raga-sms: seq traj extract nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 --double-base trj_ref.ra 		;\
+	$(TOOLDIR)/seq -r 377 -t377 --raga --mb_factor=3 -m3 grad.ra mom.ra samples.ra	;\
+	$(TOOLDIR)/extract 0 2 5 samples.ra trj_seq.ra					;\
+	$(TOOLDIR)/nrmse -t 3e-7 trj_ref.ra trj_seq.ra					;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
@@ -31,32 +25,25 @@ tests/test-seq-raga-sms: seq traj transpose slice join nrmse
 TESTS +=  tests/test-seq-raga-sms
 
 
-tests/test-seq-raga-sms-al: seq traj transpose slice join nrmse
-	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)								;\
-	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 -l --double-base trj_ref.ra 				;\
-	$(TOOLDIR)/seq -r 377 -t377 --raga_al --mb_factor=3 -m3 grad.ra mom.ra samples.ra			;\
-	$(TOOLDIR)/slice 0 3 samples.ra samples_t0.ra 							;\
-	$(TOOLDIR)/slice 0 2 samples.ra samples_t1.ra 							;\
-	$(TOOLDIR)/slice 0 4 samples.ra samples_t2.ra 							;\
-	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra 					;\
-	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra								;\
+tests/test-seq-raga-sms-al: seq traj extract nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 -l --double-base trj_ref.ra 		;\
+	$(TOOLDIR)/seq -r 377 -t377 --raga_al --mb_factor=3 -m3 grad.ra mom.ra samples.ra	;\
+	$(TOOLDIR)/extract 0 2 5 samples.ra trj_seq.ra						;\
+	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra						;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
 
 TESTS +=  tests/test-seq-raga-sms-al
 
-tests/test-seq-raga-sms-al-frame: seq traj transpose slice join nrmse
-	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)								;\
-	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 -l -t3 --double-base trj_ref.ra 				;\
-	$(TOOLDIR)/seq -r 377 -t1131 --raga_al --mb_factor=3 -m3 grad.ra mom.ra samples.ra			;\
-	$(TOOLDIR)/transpose 1 2 samples.ra   samples_t.ra							;\
-	$(TOOLDIR)/transpose 0 1 samples_t.ra samples_tt.ra							;\
-	$(TOOLDIR)/slice 0 3 samples_tt.ra samples_t0.ra 							;\
-	$(TOOLDIR)/slice 0 2 samples_tt.ra samples_t1.ra 							;\
-	$(TOOLDIR)/slice 0 4 samples_tt.ra samples_t2.ra 							;\
-	$(TOOLDIR)/join 0 samples_t0.ra samples_t1.ra samples_t2.ra trj_seq.ra 					;\
-	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra								;\
+
+tests/test-seq-raga-sms-al-frame: seq traj extract nrmse
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)						;\
+	$(TOOLDIR)/traj -x 512 -y 377 -m3 -r -A -s 1 -l -t3 --double-base trj_ref.ra 		;\
+	$(TOOLDIR)/seq -r 377 -t1131 --raga_al --mb_factor=3 -m3 grad.ra mom.ra samples.ra	;\
+	$(TOOLDIR)/extract 0 2 5 samples.ra trj_seq.ra						;\
+	$(TOOLDIR)/nrmse -t 2e-7 trj_ref.ra trj_seq.ra						;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
