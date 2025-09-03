f96c62bd1147312181f9b68603326080e3508abd
Blumenthal, Moritz <blumenthal@tugraz.at>
2025-09-03 14:46:48 +0200

    fixup stl.mk $(TESTS_OUT)/tetrahedron_cfl never made

--- tests/stl.mk
+++ tests/stl.mk
@@ -1,18 +1,18 @@
 
-$(TESTS_OUT)/tetrahedron_cfl: stl
+$(TESTS_OUT)/tetrahedron_cfl.ra: stl
 	$(TOOLDIR)/stl --model=TET $@
 
-$(TESTS_OUT)/ascii_enc.stl: stl
+$(TESTS_OUT)/ascii_enc.stl:
 	echo -e "solid name\nfacet normal 0 0 1\nouter loop\nvertex 1 0 0\nvertex 0 0 0\nvertex 0 1 0\nendloop\nendfacet\nendsolid name\n" > $@
 
-tests/test-stl-rw-tetrahedron: nrmse stl $(TESTS_OUT)/tetrahedron_cfl $(TESTS_OUT)/ascii_enc.stl
-	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)	;\
-	$(TOOLDIR)/stl --input $(TESTS_OUT)/tetrahedron_cfl tet.stl	;\
-	$(TOOLDIR)/stl --input tet.stl tet_cfl          ;\
-	$(TOOLDIR)/nrmse -t 0 $(TESTS_OUT)/tetrahedron_cfl tet_cfl ;\
-	$(TOOLDIR)/stl --input $(TESTS_OUT)/ascii_enc.stl test_cfl ;\
+tests/test-stl-rw-tetrahedron: nrmse stl $(TESTS_OUT)/tetrahedron_cfl.ra $(TESTS_OUT)/ascii_enc.stl
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
+	$(TOOLDIR)/stl --input $(TESTS_OUT)/tetrahedron_cfl.ra tet.stl	;\
+	$(TOOLDIR)/stl --input tet.stl tet_cfl          		;\
+	$(TOOLDIR)/nrmse -t 0 $(TESTS_OUT)/tetrahedron_cfl.ra tet_cfl	;\
+	$(TOOLDIR)/stl --input $(TESTS_OUT)/ascii_enc.stl test_cfl	;\
 	rm *.cfl *.hdr *.stl ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
-TESTS += tests/test-stl-rw-tetrahedron 
+TESTS += tests/test-stl-rw-tetrahedron
 
