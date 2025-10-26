a98249b5247bad603622cdf5c0cf59e7b0b00851
Philip <philip.schaten@tugraz.at>
2025-10-26 14:05:42 +0100

    tighten test tolerance for cortraj

--- tests/trajcor.mk
+++ tests/trajcor.mk
@@ -1,10 +1,11 @@
+TOL=1e-7
 
 tests/test-trajcor: traj trajcor nrmse
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)			;\
 	$(TOOLDIR)/traj -r -y10 -s1 traj.ra				;\
 	$(TOOLDIR)/traj -r -y10 -s1 -O -q1:2:3 traj_gd.ra		;\
 	$(TOOLDIR)/trajcor -q1:2:3 -O traj.ra traj_cor.ra		;\
-	$(TOOLDIR)/nrmse -t1.e-6 traj_gd.ra traj_cor.ra			;\
+	$(TOOLDIR)/nrmse -t$(TOL) traj_gd.ra traj_cor.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
@@ -14,7 +15,7 @@ tests/test-trajcor2: traj trajcor nrmse
 	$(TOOLDIR)/traj -r -y10 -t3 -s1 traj.ra				;\
 	$(TOOLDIR)/traj -r -y10 -t3 -s1 -O -q-1:0.5:1 traj_gd.ra	;\
 	$(TOOLDIR)/trajcor -q-1:0.5:1 -O traj.ra traj_cor.ra		;\
-	$(TOOLDIR)/nrmse -t1.e-6 traj_gd.ra traj_cor.ra			;\
+	$(TOOLDIR)/nrmse -t$(TOL) traj_gd.ra traj_cor.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
@@ -24,7 +25,7 @@ tests/test-trajcor3: traj trajcor nrmse
 	$(TOOLDIR)/traj -r -y10 -s1 traj.ra				;\
 	$(TOOLDIR)/traj -r -y10 -s1 -q1:2:3 traj_gd.ra			;\
 	$(TOOLDIR)/trajcor -q1:2:3 traj.ra traj_cor.ra			;\
-	$(TOOLDIR)/nrmse -t1.e-6 traj_gd.ra traj_cor.ra			;\
+	$(TOOLDIR)/nrmse -t$(TOL) traj_gd.ra traj_cor.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
@@ -35,7 +36,7 @@ tests/test-trajcor4: traj trajcor nrmse
 	$(TOOLDIR)/traj $$OPTS traj.ra					;\
 	$(TOOLDIR)/traj $$OPTS $$DELAY traj_gd.ra			;\
 	$(TOOLDIR)/trajcor $$DELAY traj.ra traj_cor.ra			;\
-	$(TOOLDIR)/nrmse -t1.e-6 traj_gd.ra traj_cor.ra			;\
+	$(TOOLDIR)/nrmse -t$(TOL) traj_gd.ra traj_cor.ra		;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
@@ -46,7 +47,7 @@ tests/test-trajcor-file: traj trajcor vec nrmse
 	$(TOOLDIR)/traj -r -y10 -t3 -s1 traj.ra				;\
 	$(TOOLDIR)/trajcor -Vqf.ra    traj.ra traj1.ra			;\
 	$(TOOLDIR)/trajcor -q-1:0.5:1 traj.ra traj2.ra			;\
-	$(TOOLDIR)/nrmse -t1.e-6 traj1.ra traj2.ra			;\
+	$(TOOLDIR)/nrmse -t$(TOL) traj1.ra traj2.ra			;\
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
