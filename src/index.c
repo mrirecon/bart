9794b49d58b19e420ad2f5ebe82d3c57667b6394
Blumenthal, Moritz <blumenthal@tugraz.at>
2025-11-19 14:37:45 +0100

    linspace/logspace in index tool

--- src/index.c
+++ src/index.c
@@ -3,6 +3,7 @@
  * a BSD-style license which can be found in the LICENSE file.
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -36,9 +37,37 @@ int main_index(int argc, char* argv[argc])
 		ARG_OUTFILE(true, &out_file, "name"),
 	};
 
-	const struct opt_s opts[] = {};
+	bool inclusive = false;
+	bool logspacing = false;
+	float min = NAN;
+	float max = NAN;
+
+	const struct opt_s opts[] = {
+
+		OPTL_SET(0, "end", &inclusive, "include the endpoint (last value is size)"),
+		OPTL_SET(0, "log", &logspacing, "use logarithmic spacing (but specify min and max)"),
+		OPTL_FLOAT(0, "min", &min, "min", "minimum value (default 0)"),
+		OPTL_FLOAT(0, "max", &max, "max", "maximum value (default size)"),
+	};
 
 	cmdline(&argc, argv, ARRAY_SIZE(args), args, help_str, ARRAY_SIZE(opts), opts);
+
+	if (safe_isnanf(min))
+		min = 0.0f;
+
+	if (safe_isnanf(max))
+		max = (float)(s);
+
+	if (logspacing) {
+
+		if (0 >= min)
+			error("logspacing requires min > 0\n");
+
+		min = logf(min);
+		max = logf(max);
+	}
+
+	float dt = (max - min) / (inclusive ? (float)(s - 1) : (float)(s));
 
 	num_init();
 
@@ -55,7 +84,7 @@ int main_index(int argc, char* argv[argc])
 	complex float* x = create_cfl(out_file, N + 1, dims);
 
 	for (int i = 0; i < s; i++)
-		x[i] = i;
+		x[i] = logspacing ? expf(min + i * dt) : (min + i * dt);
 
 	unmap_cfl(N + 1, dims, x);
 
