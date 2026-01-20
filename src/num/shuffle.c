92f8f587e3a7e29b4d7188a720ca2249889fd3e4
Blumenthal, Moritz <blumenthal@tugraz.at>
2026-01-20 10:27:32 +0100

    remove unnecessary gpu path as optimized copy kernel is available

--- src/num/shuffle.c
+++ src/num/shuffle.c
@@ -1,4 +1,5 @@
 /* Copyright 2013-2015 The Regents of the University of California.
+ * Copyright 2026. TU Graz. Institute of Biomedical Imaging.
  * All rights reserved. Use of this source code is governed by
  * a BSD-style license which can be found in the LICENSE file.
  **/
@@ -96,62 +97,6 @@ void md_decompose2(int N, const long factors[N],
 void md_decompose(int N, const long factors[N], const long odims[N + 1],
 		void* out, const long idims[N], const void* in, size_t size)
 {
-#ifdef USE_CUDA
-
-	int n_factors = bitcount(md_nontriv_dims(N, factors));
-
-	if (cuda_ondevice(out) && (1 < n_factors)) {
-
-		//do decomposition for each dimension independently
-		//=> much less calls to cuda copy strided
-		//FIXME: we should have generic strided copy kernel on GPU! 
-
-		void* tmp = md_alloc_sameplace(N, idims, size, in);
-
-		void* tmp_dst = (1 == n_factors % 2) ? out : tmp;
-		void* tmp_src = (void*)in;
-
-		long factors2[2 * N];
-		long idims2[2 * N];
-		long odims2[2 * N + 1];
-
-		md_copy_dims(N, idims2, idims);
-		md_singleton_dims(N, idims2 + N);
-
-		md_copy_dims(N, odims2, idims);
-		md_singleton_dims(N, odims2 + N);
-
-		md_singleton_dims(2 * N, factors2);
-
-		for (int i = 0; i < N; i++) {
-
-			if (1 < factors[i]) {
-
-				odims2[i] /= factors[i];
-				odims2[2 * N] = factors[i];
-				factors2[i] = factors[i];
-
-				md_decompose(2 * N, factors2, odims2, tmp_dst, idims2, tmp_src, size);
-
-				if (tmp_src == in)
-					tmp_src = (0 == n_factors % 2) ? out : tmp;
-
-				SWAP(tmp_dst, tmp_src);
-
-				factors2[i] = 1;
-				idims2[i] /= factors[i];
-
-				idims2[N + i] = factors[i];
-				odims2[N + i] = factors[i];
-			}
-		}
-
-		md_free(tmp);
-
-		return;
-	}
-#endif
-
 	long ostrs[N + 1];
 	md_calc_strides(N + 1, ostrs, odims, size);
 
@@ -177,63 +122,6 @@ void md_recompose2(int N, const long factors[N],
 void md_recompose(int N, const long factors[N], const long odims[N],
 		void* out, const long idims[N + 1], const void* in, size_t size)
 {
-#ifdef USE_CUDA
-
-	int n_factors = bitcount(md_nontriv_dims(N, factors));
-
-	if (cuda_ondevice(out) && (1 < n_factors)) {
-
-		//do recomposition for each dimension independently
-		//=> much less calls to cuda copy strided
-		//FIXME: we should have generic strided copy kernel on GPU!
-
-		void* tmp = md_alloc_sameplace(N + 1, idims, size, in);
-
-		void* tmp_dst = (1 == n_factors % 2) ? out : tmp;
-		void* tmp_src = (void*)in;
-
-		long factors2[2 * N];
-		long idims2[2 * N + 1];
-		long odims2[2 * N];
-
-		md_copy_dims(N, idims2, idims);
-		md_copy_dims(N, idims2 + N, factors);
-
-		md_copy_dims(N, odims2, idims);
-		md_copy_dims(N, odims2 + N, factors);
-
-		md_singleton_dims(2 * N, factors2);
-
-		for (int i = N - 1; i >= 0; i--) {
-
-			if (1 < factors[i]) {
-
-				idims2[2 * N] = factors[i];
-				odims2[i] *= factors[i];
-				
-				factors2[i] = factors[i];
-				idims2[N + i] = 1;
-				odims2[N + i] = 1;
-
-				md_recompose(2 * N, factors2, odims2, tmp_dst, idims2, tmp_src, size);
-
-				if (tmp_src == in)
-					tmp_src = (0 == n_factors % 2) ? out : tmp;
-
-				SWAP(tmp_dst, tmp_src);
-
-				factors2[i] = 1;
-				idims2[i] *= factors[i];
-			}
-		}
-
-		md_free(tmp);
-
-		return;
-	}
-#endif
-
-
 	long ostrs[N];
 	md_calc_strides(N, ostrs, odims, size);
 
