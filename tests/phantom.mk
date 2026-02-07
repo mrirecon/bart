1e3cf3c647af89e4afba7e571931936f352e1271
Martin Uecker <uecker@tugraz.at>
2026-02-07 12:55:23 +0100

    split stl tests and add FIXME

--- tests/phantom.mk
+++ tests/phantom.mk
@@ -390,24 +390,34 @@ tests/test-phantom-noncart-BRAIN: traj phantom nufft fft nrmse
 	rm *.ra ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
 
+
+# FIXME: broken, because it allows 100% error
 tests/test-phantom-stl: traj phantom nufft fft nrmse stl
 	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
 	$(TOOLDIR)/traj -x 64 -y 64 -z 64 -3 traj.ra					;\
-	$(TOOLDIR)/traj -x 33 -y 33 -z 33 -3 traj3.ra					;\
 	$(TOOLDIR)/stl --model TET tet.stl 						;\
 	$(TOOLDIR)/phantom -s 1 --coil HEAD_3D_64CH -t traj.ra --stl tet.stl kt.ra 	;\
-	$(TOOLDIR)/phantom -s 1 --coil HEAD_2D_8CH -t traj3.ra --stl tet.stl kt3.ra 	;\
 	$(TOOLDIR)/nufft -i traj.ra kt.ra xt.ra						;\
-	$(TOOLDIR)/nufft -i -x 33:33:33 traj3.ra kt3.ra xt3.ra				;\
 	$(TOOLDIR)/phantom -s 1 --coil HEAD_3D_64CH -k -3 -x 64 --stl tet.stl k.ra 	;\
-	$(TOOLDIR)/phantom -s 1 --coil HEAD_2D_8CH -k -3 -x 33 --stl tet.stl k3.ra 	;\
 	$(TOOLDIR)/fft -i 7 k.ra x.ra							;\
-	$(TOOLDIR)/fft -i 7 k3.ra x3.ra							;\
-	$(TOOLDIR)/phantom -s 2 --stl tet.stl -3 -k k.ra				;\
 	$(TOOLDIR)/nrmse -t 1 x.ra xt.ra						;\
+	rm *.ra ; rm *.stl ; cd .. ; rmdir $(TESTS_TMP)
+	touch $@
+
+
+# FIXME: broken, because it allows 100% error
+tests/test-phantom-stl3: traj phantom nufft fft nrmse stl
+	set -e; mkdir $(TESTS_TMP) ; cd $(TESTS_TMP)					;\
+	$(TOOLDIR)/traj -x 33 -y 33 -z 33 -3 traj3.ra					;\
+	$(TOOLDIR)/stl --model TET tet.stl 						;\
+	$(TOOLDIR)/phantom -s 1 --coil HEAD_2D_8CH -t traj3.ra --stl tet.stl kt3.ra 	;\
+	$(TOOLDIR)/nufft -i -x 33:33:33 traj3.ra kt3.ra xt3.ra				;\
+	$(TOOLDIR)/phantom -s 1 --coil HEAD_2D_8CH -k -3 -x 33 --stl tet.stl k3.ra 	;\
+	$(TOOLDIR)/fft -i 7 k3.ra x3.ra							;\
 	$(TOOLDIR)/nrmse -t 1 x3.ra xt3.ra						;\
 	rm *.ra ; rm *.stl ; cd .. ; rmdir $(TESTS_TMP)
 	touch $@
+
 
 
 TESTS += tests/test-phantom-ksp tests/test-phantom-noncart tests/test-phantom-coil tests/test-phantom-ksp-coil
@@ -424,8 +434,9 @@ TESTS += tests/test-phantom-FILE tests/test-phantom-FILE-basis
 TESTS += tests/test-phantom-ellipsoid tests/test-phantom-noncart-ellipsoid tests/test-phantom-noncart-ellipsoid-params
 
 ifneq ($(BUILDTYPE), WASM) # stl fails under wasm
-TESTS_SLOW += tests/test-phantom-stl
+TESTS_SLOW += tests/test-phantom-stl tests/test-phantom-stl3
 endif
 TESTS_SLOW += tests/test-phantom-coil-large tests/test-phantom-ksp-coil-large
 TESTS_SLOW += tests/test-phantom-noncart-BRAIN tests/test-phantom-FILE-coil-large
+
 
